be1ed2e892013be4702bc4b295919c4e80270d12
MontyTRC89 <montyhammet@hotmail.it>
2018-09-18 13:24:04 +0200

    Added tribesman with darts missing file

--- TR5Main/Objects/objects.h
+++ TR5Main/Objects/objects.h
@@ -4,29 +4,33 @@
 
 void __cdecl ClampRotation(PHD_3DPOS *pos, __int16 angle, __int16 rot);
 
-void __cdecl InitialiseWildBoar(__int16 itemNum);
-void __cdecl WildBoarControl(__int16 itemNum);
+// TR1 objects
 
-void __cdecl InitialiseSmallScorpion(__int16 itemNum);
-void __cdecl SmallScorpionControl(__int16 itemNum);
+// TR2 objects
+void __cdecl BarracudaControl(__int16 itemNum);
+void __cdecl SharkControl(__int16 itemNum);
+void __cdecl InitialiseSpinningBlade(__int16 itemNum);
+void __cdecl SpinningBlade(__int16 itemNum);
+void __cdecl InitialiseKillerStatue(__int16 itemNum);
+void __cdecl KillerStatueControl(__int16 itemNum);
+void __cdecl SpringBoardControl(__int16 itemNum);
 
-void __cdecl InitialiseBat(__int16 itemNum);
-void __cdecl BatControl(__int16 itemNum);
+// TR3 objects
+void __cdecl TigerControl(__int16 itemNum);
+void __cdecl InitialiseCobra(__int16 itemNum);
+void __cdecl CobraControl(__int16 itemNum);
+void __cdecl RaptorControl(__int16 itemNum);
+__int32 __cdecl GetWaterSurface(__int32 x, __int32 y, __int32 z, __int16 roomNumber);
+void __cdecl ShootHarpoon(ITEM_INFO* frogman, __int32 x, __int32 y, __int32 z, __int16 speed, __int16 yRot, __int16 roomNumber);
+void __cdecl HarpoonControl(__int16 itemNum);
+void __cdecl ScubaControl(__int16 itemNumber);
+void InitialiseEagle(__int16 itemNum);
+void EagleControl(__int16 itemNum);
+void __cdecl TribemanAxeControl(__int16 itemNum);
+void __cdecl TribesmanShotDart(ITEM_INFO* item);
+void __cdecl TribesmanDartsControl(__int16 itemNum);
+void __cdecl ControlSpikeWall(__int16 itemNum);
 
-void __cdecl InitialiseBaddy(__int16 itemNum);
-void __cdecl BaddyControl(__int16 itemNum);
-
-void __cdecl InitialiseSas(__int16 itemNum);
-void __cdecl SasControl(__int16 itemNum);
-
-void __cdecl InitialiseMummy(__int16 itemNum);
-void __cdecl MummyControl(__int16 itemNum);
-
-void __cdecl InitialiseSkeleton(__int16 itemNum);
-void __cdecl SkeletonControl(__int16 itemNum);
-
-//extern __int16 LaraMotorbike;
-//void __cdecl InitialiseMotorbike(__int16 itemNum);
 __int32 __cdecl GetCollisionAnim(ITEM_INFO* item, PHD_VECTOR* p);
 __int32 __cdecl TestHeight(ITEM_INFO* item, __int32 dz, __int32 dx, PHD_VECTOR* pos);
 __int32 __cdecl DoShift(ITEM_INFO* quad, PHD_VECTOR* pos, PHD_VECTOR* old);
@@ -39,58 +43,33 @@ void __cdecl QuadBikeCollision(__int16 itemNumber, ITEM_INFO* l, COLL_INFO* coll
 __int32 __cdecl QuadBikeControl();
 void __cdecl TriggerExhaustSmoke(__int32 x, __int32 y, __int32 z, __int16 angle, __int32 speed, __int32 moving);
 
-//void __cdecl InitialiseSkeleton(__int16 itemNum);
-//void __cdecl SkeletonControl(__int16 itemNum);
-
+// TR4 object
+void __cdecl InitialiseWildBoar(__int16 itemNum);
+void __cdecl WildBoarControl(__int16 itemNum);
+void __cdecl InitialiseSmallScorpion(__int16 itemNum);
+void __cdecl SmallScorpionControl(__int16 itemNum);
+void __cdecl InitialiseBat(__int16 itemNum);
+void __cdecl BatControl(__int16 itemNum);
+void __cdecl InitialiseBaddy(__int16 itemNum);
+void __cdecl BaddyControl(__int16 itemNum);
+void __cdecl InitialiseSas(__int16 itemNum);
+void __cdecl SasControl(__int16 itemNum);
+void __cdecl InitialiseMummy(__int16 itemNum);
+void __cdecl MummyControl(__int16 itemNum);
+void __cdecl InitialiseSkeleton(__int16 itemNum);
+void __cdecl SkeletonControl(__int16 itemNum);
 void __cdecl FourBladesControl(__int16 itemNum);
-
 void __cdecl BirdBladeControl(__int16 itemNum);
-
 void __cdecl CatwalkBlaldeControl(__int16 itemNum);
-
 void __cdecl PlinthBladeControl(__int16 itemNum);
-
 void __cdecl InitialiseSethBlade(__int16 itemNum);
 void __cdecl SethBladeControl(__int16 itemNum);
-
 void __cdecl ChainControl(__int16 itemNum);
-
 void __cdecl PloughControl(__int16 itemNum);
-
 void __cdecl CogControl(__int16 itemNum);
-
 void __cdecl SpikeballControl(__int16 itemNum);
 
-void __cdecl BarracudaControl(__int16 itemNum);
+//extern __int16 LaraMotorbike;
+//void __cdecl InitialiseMotorbike(__int16 itemNum);
 
-void __cdecl SharkControl(__int16 itemNum);
 
-void __cdecl ControlSpikeWall(__int16 itemNum);
-
-void __cdecl InitialiseSpinningBlade(__int16 item_number);
-void __cdecl SpinningBlade(__int16 item_number);
-
-void __cdecl InitialiseKillerStatue(__int16 item_number);
-void __cdecl KillerStatueControl(__int16 item_number);
-
-void __cdecl SpringBoardControl(__int16 item_number);
-
-void __cdecl TigerControl(__int16 itemNum);
-
-void __cdecl InitialiseCobra(__int16 itemNum);
-void __cdecl CobraControl(__int16 itemNum);
-
-void __cdecl RaptorControl(__int16 itemNum);
-
-__int32 __cdecl GetWaterSurface(__int32 x, __int32 y, __int32 z, __int16 roomNumber);
-void __cdecl ShootHarpoon(ITEM_INFO* frogman, __int32 x, __int32 y, __int32 z, __int16 speed, __int16 yRot, __int16 roomNumber);
-void __cdecl HarpoonControl(__int16 itemNum);
-void __cdecl ScubaControl(__int16 itemNumber);
-
-void InitialiseEagle(__int16 itemNum);
-
-void EagleControl(__int16 itemNum);
-
-void __cdecl TribemanAxeControl(__int16 itemNum);
-void __cdecl TribesmanShotDart(ITEM_INFO* item);
-void __cdecl TribesmanDartsControl(__int16 itemNum);
