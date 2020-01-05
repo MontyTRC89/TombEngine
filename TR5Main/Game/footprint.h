5140369a3cc47f61adf0930eec123684b3714347
Raildex <n@a>
2020-01-05 18:48:16 +0100

    Created Subtractive Blending state Added Footprint struct added Footprint rendering function Added footprint update routine Added AddFootprint Command effect
    TODO:
    Check condition for left/right foot in Command effect

---
+++ TR5Main/Game/footprint.h
@@ -0,0 +1,19 @@
+#include "../Global/types.h"
+#include "math.h"
+#include <queue>
+#pragma once
+
+constexpr size_t MAX_FOOTPRINTS = 10;
+typedef struct footprint_t {
+	PHD_3DPOS pos;
+	int life;
+	int lifeStartFading;
+	byte startOpacity;
+	byte opacity;
+	bool active;
+}FOOTPRINT_STRUCT;
+
+constexpr int FOOT_HEIGHT_OFFSET = 64;
+bool CheckFootOnFloor(ITEM_INFO& const item, int mesh, PHD_3DPOS& outFootprintPosition);
+int getFootprintIndexWithLowestLife();
+void updateFootprints();
