ab6d63fff02396733a3cded3e45ac6bdf755f1ce
Montagna Marco <montyhammet@hotmail.it>
2020-01-07 17:32:49 +0100

    Decompiling ID_STEEL_DOOR; Added missing file;

--- TR5Main/Game/door.h
+++ TR5Main/Game/door.h
@@ -5,9 +5,9 @@
 //#define InitialiseDoor ((void (__cdecl*)(short)) 0x0043DB60)
 #define DrawLiftDoor ((void (__cdecl*)(ITEM_INFO*)) 0x0045AAF0)
 #define DoubleDoorControl ((void (__cdecl*)(short)) 0x00429840)
-#define InitialiseSteelDoor ((void (__cdecl*)(short)) 0x0043F180)
+//#define InitialiseSteelDoor ((void (__cdecl*)(short)) 0x0043F180)
 #define SteelDoorControl ((void (__cdecl*)(short)) 0x00486BE0)
-#define SteelDoorCollision ((void (__cdecl*)(short,ITEM_INFO*,COLL_INFO*)) 0x00487AD0)
+//#define SteelDoorCollision ((void (__cdecl*)(short,ITEM_INFO*,COLL_INFO*)) 0x00487AD0)
 void SequenceDoorControl(short itemNumber);
 void UnderwaterDoorCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll);
 void DoubleDoorCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll);
@@ -18,5 +18,7 @@ void DoorControl(short itemNumber);
 void OpenThatDoor(DOORPOS_DATA* doorPos, DOOR_DATA* dd);
 void ShutThatDoor(DOORPOS_DATA* doorPos, DOOR_DATA* dd);
 void InitialiseDoor(short itemNumber);
+void InitialiseSteelDoor(short itemNumber);
+void SteelDoorCollision(short itemNumber, ITEM_INFO* l, COLL_INFO* coll);
 
 void Inject_Door();
