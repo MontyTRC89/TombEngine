a505c6fde6a33c149de385623de279d03197da8d
Montagna Marco <montyhammet@hotmail.it>
2020-01-28 07:44:46 +0100

    Fixed repo

--- TR5Main/Game/laramisc.h
+++ TR5Main/Game/laramisc.h
@@ -3,10 +3,10 @@
 
 extern COLL_INFO coll;
 
-#define GetLaraDeadlyBounds ((void (__cdecl*)()) 0x004569C0)
 #define phd_SetTrans ((void (__cdecl*)(int, int, int)) 0x0048FA40)
 #define phd_RotBoundingBoxNoPersp ((void (__cdecl*)(short*, short*)) 0x0042E240)
 
+void GetLaraDeadlyBounds();
 void DelAlignLaraToRope(ITEM_INFO* item);
 void InitialiseLaraAnims(ITEM_INFO* item);
 void InitialiseLaraLoad(short itemNumber);
