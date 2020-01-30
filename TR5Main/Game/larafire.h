4fd60a0872dc3a100b0594d811205f936b4efeb7
asasas9500 <asasas9500@gmail.com>
2020-01-30 15:01:36 -0300

    Fix TorchControl(), FireWeapon(), LaraTargetInfo(), DoProperDetection and FlareControl(); Place function markers

--- TR5Main/Game/larafire.h
+++ TR5Main/Game/larafire.h
@@ -5,7 +5,7 @@
 //#define SmashObject ((int (__cdecl*)(short)) 0x00465200)
 //#define FindTargetPoint ((void (__cdecl*)(ITEM_INFO*, GAME_VECTOR*)) 0x004533A0)
 //#define HitTarget ((short  (__cdecl*)(ITEM_INFO*, GAME_VECTOR*, int, int)) 0x00453930)
-#define DoProperDetection ((void  (__cdecl*)(short, int, int, int, int, int)) 0x00453BE0)
+#define DoProperDetection ((void  (__cdecl*)(short, int, int, int, int, int, int)) 0x00453BE0)
 //#define DoFlameTorch ((void  (__cdecl*)()) 0x00433EA0)
 //#define GetAmmo ((int  (__cdecl*)(int)) 0x004546C0)
 #define LaraGetNewTarget ((void (__cdecl*)(WEAPON_INFO*)) 0x00452ED0)
