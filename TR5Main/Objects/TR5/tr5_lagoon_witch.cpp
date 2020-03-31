5a91471af2a969b1b8af517d198ee7b4dddacfee
Montagna Marco <montyhammet@hotmail.it>
2020-03-31 07:44:40 +0200

    Added death states to CHEF and LAGOON_WITCH; Fixed water enemies;

--- TR5Main/Objects/TR5/tr5_lagoon_witch.cpp
+++ TR5Main/Objects/TR5/tr5_lagoon_witch.cpp
@@ -6,9 +6,12 @@
 #include "../../Game/tomb4fx.h"
 #include "../../Game/inventory.h"
 
-#define STATE_LAGOON_WITCH_SWIM		1
-#define STATE_LAGOON_WITCH_STOP		2
-#define STATE_LAGOON_WITCH_ATTACK	3
+#define STATE_LAGOON_WITCH_SWIM			1
+#define STATE_LAGOON_WITCH_STOP			2
+#define STATE_LAGOON_WITCH_ATTACK		3
+#define STATE_LAGOON_WITCH_DEATH		5
+
+#define ANIMATION_LAGOON_WITCH_DEATH	7
 
 extern Inventory* g_Inventory;
 
@@ -35,89 +38,104 @@ void ControlLagoonWitch(short itemNumber)
 	short joint0 = 0;
 	short joint1 = 0;
 	short joint2 = 0;
+	short angle = 0;
 
 	ITEM_INFO* item = &Items[itemNumber];
 	CREATURE_INFO* creature = (CREATURE_INFO*)item->data;
+	OBJECT_INFO* obj = &Objects[item->objectNumber];
 
-	if (g_Inventory->IsObjectPresentInInventory(ID_PUZZLE_ITEM2))
+	if (item->hitPoints <= 0)
 	{
-		item->aiBits = 0;
-		creature->enemy = LaraItem;
-	}
-
-	if (item->aiBits)
-	{
-		GetAITarget(creature);
-	}
-	else if (creature->hurtByLara)
-	{
-		creature->enemy = LaraItem;
-	}
-
-	AI_INFO info;
-	CreatureAIInfo(item, &info);
-
-	//if (creature->enemy != LaraItem)
-	//	phd_atan(lara_item->pos.z_pos - item->pos.z_pos, lara_item->pos.x_pos - item->pos.x_pos);
-	
-	GetCreatureMood(item, &info, VIOLENT);
-	CreatureMood(item, &info, VIOLENT);
-	
-	short angle = CreatureTurn(item, creature->maximumTurn);
-	
-	if (info.ahead)
-	{
-		joint0 = info.angle >> 1;
-		joint2 = info.angle >> 1;
-		joint1 = info.xAngle;
-	}
-
-	creature->maximumTurn = 0;
-
-	switch (item->currentAnimState)
-	{
-	case STATE_LAGOON_WITCH_SWIM:
-		creature->maximumTurn = 728;
-		if (info.distance < SQUARE(1024))
-			item->goalAnimState = STATE_LAGOON_WITCH_STOP;
-		break;
-
-	case STATE_LAGOON_WITCH_STOP:
-		creature->flags = 0;
-		creature->maximumTurn = ANGLE(2);
-		if (info.distance < SQUARE(768))
-			item->goalAnimState = STATE_LAGOON_WITCH_ATTACK;
-		else if (info.distance > SQUARE(1024))
-			item->goalAnimState = STATE_LAGOON_WITCH_SWIM;
-		else
-			item->goalAnimState = STATE_LAGOON_WITCH_STOP;
-		break;
-
-	case STATE_LAGOON_WITCH_ATTACK:
-		creature->maximumTurn = ANGLE(2);
-		if (!creature->flags 
-			&& item->touchBits & 0x3C3C0 
-			&& item->frameNumber > Anims[item->animNumber].frameBase + 29)
+		if (item->currentAnimState != STATE_LAGOON_WITCH_DEATH)
 		{
-			LaraItem->hitPoints -= 100;
-			LaraItem->hitStatus = true;
-			CreatureEffect2(item, &LagoonWitchBite, 10, item->pos.yRot, DoBloodSplat);
-			creature->flags = STATE_LAGOON_WITCH_SWIM;
+			item->hitPoints = 0;
+			item->currentAnimState = STATE_LAGOON_WITCH_DEATH;
+			item->animNumber = obj->animIndex + ANIMATION_LAGOON_WITCH_DEATH;
+			item->frameNumber = Anims[item->animNumber].frameBase;
 		}
-		break;
-
 	}
-
-	if (creature->reachedGoal)
+	else
 	{
-		ITEM_INFO* enemy = creature->enemy;
-		if (enemy)
+		if (g_Inventory->IsObjectPresentInInventory(ID_PUZZLE_ITEM2))
 		{
-			if (enemy->flags & 2)
-				item->itemFlags[3] = (item->TOSSPAD & 0xFF) - 1;
-			item->itemFlags[3]++;
-			creature->reachedGoal = false;
-			creature->enemy = 0;
+			item->aiBits = 0;
+			creature->enemy = LaraItem;
+		}
+
+		if (item->aiBits)
+		{
+			GetAITarget(creature);
+		}
+		else if (creature->hurtByLara)
+		{
+			creature->enemy = LaraItem;
+		}
+
+		AI_INFO info;
+		CreatureAIInfo(item, &info);
+
+		//if (creature->enemy != LaraItem)
+		//	phd_atan(lara_item->pos.z_pos - item->pos.z_pos, lara_item->pos.x_pos - item->pos.x_pos);
+
+		GetCreatureMood(item, &info, VIOLENT);
+		CreatureMood(item, &info, VIOLENT);
+
+		angle = CreatureTurn(item, creature->maximumTurn);
+
+		if (info.ahead)
+		{
+			joint0 = info.angle >> 1;
+			joint2 = info.angle >> 1;
+			joint1 = info.xAngle;
+		}
+
+		creature->maximumTurn = 0;
+
+		switch (item->currentAnimState)
+		{
+		case STATE_LAGOON_WITCH_SWIM:
+			creature->maximumTurn = 728;
+			if (info.distance < SQUARE(1024))
+				item->goalAnimState = STATE_LAGOON_WITCH_STOP;
+			break;
+
+		case STATE_LAGOON_WITCH_STOP:
+			creature->flags = 0;
+			creature->maximumTurn = ANGLE(2);
+			if (info.distance < SQUARE(768))
+				item->goalAnimState = STATE_LAGOON_WITCH_ATTACK;
+			else if (info.distance > SQUARE(1024))
+				item->goalAnimState = STATE_LAGOON_WITCH_SWIM;
+			else
+				item->goalAnimState = STATE_LAGOON_WITCH_STOP;
+			break;
+
+		case STATE_LAGOON_WITCH_ATTACK:
+			creature->maximumTurn = ANGLE(2);
+			if (!creature->flags
+				&& item->touchBits & 0x3C3C0
+				&& item->frameNumber > Anims[item->animNumber].frameBase + 29)
+			{
+				LaraItem->hitPoints -= 100;
+				LaraItem->hitStatus = true;
+				CreatureEffect2(item, &LagoonWitchBite, 10, item->pos.yRot, DoBloodSplat);
+				creature->flags = STATE_LAGOON_WITCH_SWIM;
+			}
+			break;
+
+		}
+
+		if (creature->reachedGoal)
+		{
+			ITEM_INFO* enemy = creature->enemy;
+			if (enemy)
+			{
+				if (enemy->flags & 2)
+					item->itemFlags[3] = (item->TOSSPAD & 0xFF) - 1;
+				item->itemFlags[3]++;
+				creature->reachedGoal = false;
+				creature->enemy = 0;
+			}
 		}
 	}
 
