84273b65777cdc4f810b34b747be5f1cda60ce69
MontyTRC89 <montyhammet@hotmail.it>
2020-04-11 18:32:57 +0200

    Decompiled FixedCamera, BinocularCamera, LaraTorch, ConfirmCameraTargetPos, CalculateCamera

--- TR5Main/Game/flmtorch.h
+++ TR5Main/Game/flmtorch.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "..\Global\global.h"
 
-#define LaraTorch ((void(__cdecl*)(PHD_VECTOR*,PHD_VECTOR*,int,int)) 0x00410550)
-
 void TriggerTorchFlame(char fxObj, char node);
 void DoFlameTorch();
 void GetFlameTorch();
