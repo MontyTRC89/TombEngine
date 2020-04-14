d0a18e49792886aef5930dddb0633ccd211e8316
MontyTRC89 <montyhammet@hotmail.it>
2020-04-14 17:47:49 +0200

    Decompiling math functions 4

--- TR5Main/Game/draw.h
+++ TR5Main/Game/draw.h
@@ -6,16 +6,8 @@
 #define IsRoomOutside ((int (__cdecl*)(int, int, int)) 0x00418E90) 
 #define sub_42B4C0 ((void (__cdecl*)(ITEM_INFO*,short*)) 0x0042B4C0)
 
-#define phd_PushMatrix_I ((void(__cdecl*)(void)) 0x0042BF50)
-#define phd_PushUnitMatrix ((void (__cdecl*)()) 0x0048FA90)
-#define phd_GetVectorAngles ((void(__cdecl*)(int, int, int, short*)) 0x004904B0)
-#define phd_PutPolygons ((void(__cdecl*)(short*)) 0x004B3F00)
-#define phd_PutPolygons_I ((void(__cdecl*)(short*)) 0x0042C3F0)
-#define phd_RotYXZpack ((void(__cdecl*)(int)) 0x0048FEB0)
 #define gar_RotYXZsuperpack ((void(__cdecl*)(short**,int)) 0x0042C310)
 #define gar_RotYXZsuperpack_I ((void(__cdecl*)(short**,short**,int)) 0x0042C290)
-#define phd_ClipBoundingBox ((int(__cdecl*)(short*)) 0x004B7EB0) // int S_GetObjectBounds(frames[0])
-#define phd_SetTrans ((void (__cdecl*)(int, int, int)) 0x0048FA40)
 #define phd_RotBoundingBoxNoPersp ((void (__cdecl*)(short*, short*)) 0x0042E240)
 
 //#define InitInterpolate ((void(__cdecl*)(int,int)) 0x0042BE90)
@@ -35,6 +27,11 @@
 //#define phd_RotY_I ((void(__cdecl*)(short)) 0x0042BFC0)
 //#define phd_RotX_I ((void(__cdecl*)(short)) 0x0042C030)
 //#define phd_RotZ_I ((void(__cdecl*)(short)) 0x0042C0A0)
+//#define phd_SetTrans ((void (__cdecl*)(int, int, int)) 0x0048FA40)
+//#define phd_PushMatrix_I ((void(__cdecl*)(void)) 0x0042BF50)
+//#define phd_PushUnitMatrix ((void (__cdecl*)()) 0x0048FA90)
+//#define phd_GetVectorAngles ((void(__cdecl*)(int, int, int, short*)) 0x004904B0)
+//#define phd_RotYXZpack ((void(__cdecl*)(int)) 0x0048FEB0)
 
 #define mGetAngle ((int(__cdecl*)(int, int, int, int)) 0x0048F290)
 #define phd_GenerateW2V ((void(__cdecl*)(PHD_3DPOS*)) 0x0048F330)
@@ -56,10 +53,10 @@ bool TIME_Init();
 bool TIME_Reset();
 void DrawAnimatingItem(ITEM_INFO* item);
 
-void phd_PushMatrix(void);
+void phd_PushMatrix();
 void InitInterpolate(int frac, int rate);
-void phd_PopMatrix(void);
-void phd_PopMatrix_I(void);
+void phd_PopMatrix();
+void phd_PopMatrix_I();
 void phd_RotYXZ(short ry, short rx, short rz);
 void phd_RotX(short ry);
 void phd_RotY(short ry);
@@ -71,24 +68,17 @@ void phd_TranslateRel(int x, int y, int z);
 void phd_TranslateRel_I(int x, int y, int z);
 void phd_TranslateRel_ID(int x1, int y1, int z1, int x2, int y2, int z2);
 void phd_TranslateAbs(int x, int y, int z);
+void phd_SetTrans(int x, int y, int z);
+void phd_PushMatrix_I();
+void phd_PushUnitMatrix();
+void phd_GetVectorAngles(int x, int y, int z, short* angles);
+void phd_RotYXZpack(int rots);
 
 /* phd function (DX11 version) */
 // TODO: phd_XX function after this lign is used with DX11 one, when it's finished, delete the #define one and delete this sign: "_" at the beginning of the new function. (delete the unused if needed)
 
-void _phd_PushMatrix_I(void);
-void _phd_PushUnitMatrix(void);
-void _phd_RotYXZpack(short ry, short rx, short rz);
 void _gar_RotYXZsuperpack(short** pproc, int skip);
 void _gar_RotYXZsuperpack_I(short** pproc1, short** pproc2, int skip);
-void _phd_ClipBoundingBox(short* frames);
-
-void _phd_DxTranslateRel(int x, int y, int z);
-void _phd_DxTranslateAbs(int x, int y, int z);
-void _phd_DxRotY(short ry);
-void _phd_DxRotX(short rx);
-void _phd_DxRotZ(short rz);
-void _phd_DxRotYXZ(short ry, short rx, short rz);
-void _phd_DxRotYXZpack(int rangle);
 
 extern Renderer11* g_Renderer;
 extern BITE_INFO EnemyBites[9];
