b72fc181a426ac9c579933b44628a69f2ba5ddc3
Wuups <woopstombraider@gmail.com>
2020-05-01 21:19:03 -0500

    work

--- TR5Main/Game/lara.h
+++ TR5Main/Game/lara.h
@@ -283,6 +283,8 @@ void lara_col_all4s(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_all4s(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_duck(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_duck(ITEM_INFO* item, COLL_INFO* coll);
+void lara_as_crouch_roll(ITEM_INFO* item, COLL_INFO* coll);
+void lara_col_crouch_roll(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_ducklr(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_duckr(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_duckl(ITEM_INFO* item, COLL_INFO* coll);
