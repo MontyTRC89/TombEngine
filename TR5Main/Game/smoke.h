b2bd89a263078ab84a2fc2a0904ef162f22d3f51
Nils Gaitzsch <nils.gaitzsch@googlemail.com>
2020-05-28 16:48:36 +0200

    Added new TriggerGunSmokeParticle Function

--- TR5Main/Game/smoke.h
+++ TR5Main/Game/smoke.h
@@ -2,30 +2,40 @@
 #include <d3d11.h>
 #include <SimpleMath.h>
 #include <array>
-struct SmokeParticle {
-	DirectX::SimpleMath::Vector4 sourceColor;
-	DirectX::SimpleMath::Vector4 destinationColor;
-	DirectX::SimpleMath::Vector4 color;
-	DirectX::SimpleMath::Vector3 position;
-	DirectX::SimpleMath::Vector3 velocity;
-	int room;
-	float gravity;
-	float friction;
-	float sourceSize;
-	float destinationSize;
-	float size;
-	float age;
-	float life;
-	float angularVelocity;
-	float angularDrag;
-	float rotation;
-	float terminalVelocity;
-	bool affectedByWind;
-	bool active;
-};
+namespace T5M{
+	namespace Effects {
+		namespace Smoke {
+			struct SmokeParticle {
+				DirectX::SimpleMath::Vector4 sourceColor;
+				DirectX::SimpleMath::Vector4 destinationColor;
+				DirectX::SimpleMath::Vector4 color;
+				DirectX::SimpleMath::Vector3 position;
+				DirectX::SimpleMath::Vector3 velocity;
+				int room;
+				int sprite;
+				float gravity;
+				float friction;
+				float sourceSize;
+				float destinationSize;
+				float size;
+				float age;
+				float life;
+				float angularVelocity;
+				float angularDrag;
+				float rotation;
+				float terminalVelocity;
+				bool affectedByWind;
+				bool active;
+			};
 
-extern std::array<SmokeParticle, 128> SmokeParticles;
+			extern std::array<SmokeParticle, 128> SmokeParticles;
 
-void UpdateSmokeParticles();
-void TriggerFlareSmoke(const DirectX::SimpleMath::Vector3& pos, DirectX::SimpleMath::Vector3& direction,int age,int room);
-SmokeParticle& getFreeSmokeParticle();
+			void UpdateSmokeParticles();
+			void TriggerFlareSmoke(const DirectX::SimpleMath::Vector3& pos, DirectX::SimpleMath::Vector3& direction, int age, int room);
+			SmokeParticle& getFreeSmokeParticle();
+			void TriggerGunSmokeParticles(int x, int y, int z, short xv, short yv, short zv, byte initial, int weaponType, byte count);
+
+		}
+	}
+
+}
