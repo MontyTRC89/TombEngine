b265cbb90c98004a538c04fa0985a77e4800e02f
KrysB4k <krys_bak@onet.pl>
2020-06-07 02:49:38 +0200

    Added GetOrthogonalIntersect()
    GetOrthogonalIntersect() replaces bloated dir switch case in functions like TestLaraVault() and similar

--- TR5Main/Game/collide.h
+++ TR5Main/Game/collide.h
@@ -128,4 +128,5 @@ void CreatureCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll);
 void GetCollisionInfo(COLL_INFO* coll, int xPos, int yPos, int zPos, int roomNumber, int objectHeight);
 void LaraBaddieCollision(ITEM_INFO* item, COLL_INFO* coll);
 void CalcItemToFloorRotation(ITEM_INFO* item, short rotY, int radiusZ, int radiusX); // new function for rotating item along XZ slopes
-Vector2 GetDiagonalIntersect(int xPos, int zPos, int splitType, int radius, short yRot); // finds xPos, zPos that intersects with diagonal on sector
+Vector2 GetDiagonalIntersect(int xPos, int zPos, int splitType, int radius, short yRot); // find xPos, zPos that intersects with diagonal on sector
+Vector2 GetOrthogonalIntersect(int xPos, int zPos, int radius, short yRot); // find xPos, zPos near sector bound, offset by radius;
