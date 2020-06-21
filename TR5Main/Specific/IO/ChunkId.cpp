c15271e0867f56b47c730869c893b07dc68b1f6d
asasas9500 <asasas9500@gmail.com>
2020-06-20 21:02:45 -0300

    Update sol and lua libraries; Fix linker error

--- TR5Main/Specific/IO/ChunkId.cpp
+++ TR5Main/Specific/IO/ChunkId.cpp
@@ -1,7 +1,7 @@
 #include "framework.h"
 #include "ChunkId.h"
 using std::string;
-inline ChunkId::ChunkId(char* bytes, int length) {
+ChunkId::ChunkId(char* bytes, int length) {
 	if (length == 0) {
 		m_chunkBytes = NULL;
 		m_length = 0;
@@ -12,20 +12,20 @@ inline ChunkId::ChunkId(char* bytes, int length) {
 	}
 }
 
-inline ChunkId::~ChunkId() {
+ChunkId::~ChunkId() {
 	if (m_chunkBytes != NULL)
 		delete m_chunkBytes;
 }
 
-inline ChunkId* ChunkId::FromString(const char* str) {
+ChunkId* ChunkId::FromString(const char* str) {
 	return new ChunkId((char*)str, strlen(str));
 }
 
-inline ChunkId* ChunkId::FromString(string* str) {
+ChunkId* ChunkId::FromString(string* str) {
 	return new ChunkId((char*)str->c_str(), str->length());
 }
 
-inline ChunkId* ChunkId::FromStream(BaseStream* stream) {
+ChunkId* ChunkId::FromStream(BaseStream* stream) {
 	int idLength = LEB128::ReadInt32(stream);
 	char* buffer = (char*)malloc(idLength);
 	stream->Read(buffer, idLength);
@@ -34,20 +34,20 @@ inline ChunkId* ChunkId::FromStream(BaseStream* stream) {
 	return chunk;
 }
 
-inline void ChunkId::ToStream(BaseStream* stream) {
+void ChunkId::ToStream(BaseStream* stream) {
 	LEB128::Write(stream, m_length);
 	stream->WriteBytes(m_chunkBytes, m_length);
 }
 
-inline byte* ChunkId::GetBytes() {
+byte* ChunkId::GetBytes() {
 	return m_chunkBytes;
 }
 
-inline int ChunkId::GetLength() {
+int ChunkId::GetLength() {
 	return m_length;
 }
 
-inline bool ChunkId::EqualsTo(ChunkId* other) {
+bool ChunkId::EqualsTo(ChunkId* other) {
 	if (m_length != other->GetLength())
 		return false;
 	return (strncmp((const char*)m_chunkBytes, (const char*)other->GetBytes(), m_length) == 0);
