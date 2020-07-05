538dbb3682fb1a0ac607b4800e4ff352ff945035
MontyTRC89 <montyhammet@hotmail.it>
2020-07-05 06:20:36 +0200

    Refactored again room and eshes geometry loading

--- TR5Main/Specific/newtypes.h
+++ TR5Main/Specific/newtypes.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "framework.h"
 
+struct POLYGON
+{
+	int shape;
+	std::vector<int> indices;
+	std::vector<Vector2> textureCoordinates;
+};
+
 struct BUCKET
 {
 	int texture;
 	byte blendMode;
 	bool animated;
-	std::vector<int> indices;
+	int numQuads;
+	int numTriangles;
+	std::vector<POLYGON> polygons;
 };
