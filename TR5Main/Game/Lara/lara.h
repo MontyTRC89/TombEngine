3b7ff5f20de7041a497acbdb23d23d0661923068
Woops <woopstombraider@gmail.com>
2020-08-10 00:21:45 -0500

    Lara refactor: move crouch/crawl into lara_crawl.cpp

--- TR5Main/Game/Lara/lara.h
+++ TR5Main/Game/Lara/lara.h
@@ -140,22 +140,20 @@ void lara_as_dashdive(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_dash(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_dash(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_crawl2hang(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_crawlb(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_crawlb(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_all4turnr(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_all4turnlr(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_all4turnl(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_crawl(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_crawl(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_all4s(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_all4s(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_duck(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_duck(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_crouch_roll(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_crouch_roll(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_ducklr(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_duckr(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_duckl(ITEM_INFO* item, COLL_INFO* coll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void LaraAboveWater(ITEM_INFO* item, COLL_INFO* coll);
 void LaraSlideEdgeJump(ITEM_INFO* item, COLL_INFO* coll);
