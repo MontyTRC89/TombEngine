8473a3896479af32acc530ebfba668b9eb95957c
Woops <woopstombraider@gmail.com>
2020-08-10 00:34:19 -0500

    Lara refactor: move pickup states to pickup.cpp

--- TR5Main/Game/Lara/lara.cpp
+++ TR5Main/Game/Lara/lara.cpp
@@ -25,6 +25,7 @@
 #include "input.h"
 #include "sound.h"
 #include "setup.h"
+#include "pickup.h"
 
 #include "motorbike.h"
 #include "cannon.h"
@@ -1902,27 +1903,9 @@ void lara_as_switchon(ITEM_INFO* item, COLL_INFO* coll)//1ABEC(<), 1AD20(<) (F)
 	Camera.speed = 6;
 }
 
-void lara_as_pickupflare(ITEM_INFO* item, COLL_INFO* coll)//1AB5C(<), 1AC90(<) (F)
-{
-	Lara.look = false;
-	coll->enableBaddiePush = false;
-	coll->enableSpaz = false;
-	Camera.targetAngle = ANGLE(130.0f);
-	Camera.targetElevation = -ANGLE(15.0f);
-	Camera.targetDistance = SECTOR(1);
-	if (item->frameNumber == g_Level.Anims[item->animNumber].frameEnd - 1)
-		Lara.gunStatus = LG_NO_ARMS;
-}
 
-void lara_as_pickup(ITEM_INFO* item, COLL_INFO* coll)//1AB00(<), 1AC34(<) (F)
-{
-	Lara.look = false;
-	coll->enableBaddiePush = false;
-	coll->enableSpaz = false;
-	Camera.targetAngle = -ANGLE(130.0f);
-	Camera.targetElevation = -ANGLE(15.0f);
-	Camera.targetDistance = SECTOR(1);
-}
+
+
 
 void lara_as_ppready(ITEM_INFO* item, COLL_INFO* coll)//1AABC(<), 1ABF0(<) (F)
 {
