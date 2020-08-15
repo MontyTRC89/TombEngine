334f58bf05507d66d39e68249e6badb61a7366b7
Woops <woopstombraider@gmail.com>
2020-08-15 15:16:08 -0500

    Lara refactor: lara_initialise and move AnimateLara to lara.cpp

---
+++ TR5Main/Game/Lara/lara_initialise.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "lara_struct.h"
+
+void InitialiseLara(int restore);
+void LaraInitialiseMeshes();
+void InitialiseLaraAnims(ITEM_INFO* item);
+void InitialiseLaraLoad(short itemNumber);
--- TR5Main/Game/Lara/laramisc.h
+++ TR5Main/Game/Lara/laramisc.h
@@ -6,11 +6,8 @@ extern COLL_INFO lara_coll;
 
 void GetLaraDeadlyBounds();
 void DelAlignLaraToRope(ITEM_INFO* item);
-void InitialiseLaraAnims(ITEM_INFO* item);
-void InitialiseLaraLoad(short itemNumber);
-void InitialiseLara(int restore);
-void LaraInitialiseMeshes();
-void AnimateLara(ITEM_INFO* item);
+
+
 void LaraCheatyBits();
 void LaraCheatGetStuff();
 void DelsGiveLaraItemsCheat();
