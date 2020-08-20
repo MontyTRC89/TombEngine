b4a08c9cefd44084eb8e2a8e43d4a9edf2dfcb4c
MontyTRC89 <montyhammet@hotmail.it>
2020-08-20 08:13:16 +0200

    Fixed ID_SETHA; Decompiled ID_BIG_BEETLE;

--- TR5Main/Objects/TR4/Entity/tr4_setha.h
+++ TR5Main/Objects/TR4/Entity/tr4_setha.h
@@ -7,3 +7,4 @@ void SethaControl(short itemNumber);
 void TriggerSethaSparks1(int x, int y, int z, short xv, short yv, short zv);
 void TriggerSethaSparks2(short itemNumber, char node, int size);
 void SethaThrowAttack(PHD_3DPOS* pos, short roomNumber, short mesh);
+void SethaAttack(int itemNumber);
