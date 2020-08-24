307c054004510e42e668102ca7346655b5982449
Woops <woopstombraider@gmail.com>
2020-08-24 18:01:12 -0500

    better things

--- TR5Main/Objects/TR5/tr5_objects.cpp
+++ TR5Main/Objects/TR5/tr5_objects.cpp
@@ -1104,6 +1104,17 @@ static void StartTrap(OBJECT_INFO *obj)
 		obj->saveAnim = true;
 	}
 
+	obj = &Objects[ID_BIG_ROLLING_BALL];
+	if (obj->loaded)
+	{
+		obj->collision = ClassicRollingBallCollision;
+		obj->control = ClassicRollingBallControl;
+		obj->initialise = InitialiseClassicRollingBall;
+		obj->savePosition = true;
+		obj->saveFlags = true;
+		obj->saveAnim = true;
+	}
+
 	obj = &Objects[ID_DARTS];
 	if (obj->loaded)
 	{
