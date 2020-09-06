d44a77c63198c73d2c0d0421d1db4c79d7ec0a92
Nils Gaitzsch <nils.gaitzsch@googlemail.com>
2020-09-06 20:34:57 +0200

    Fallback Camera Target

--- TR5Main/Renderer/RenderView/RenderView.cpp
+++ TR5Main/Renderer/RenderView/RenderView.cpp
@@ -47,8 +47,12 @@ namespace T5M::Renderer {
 		RoomNumber = cam->pos.roomNumber;
 		WorldPosition = Vector3(cam->pos.x, cam->pos.y, cam->pos.z);
 		Vector3 target = Vector3(cam->target.x, cam->target.y, cam->target.z);
+		if((target - WorldPosition) == Vector3::Zero){
+			target.y -= 10;
+		}
 		WorldDirection = target - WorldPosition;
 		WorldDirection.Normalize();
+		
 		Vector3 up = -Vector3::UnitY;
 		Matrix upRotation = Matrix::CreateFromYawPitchRoll(0.0f, 0.0f, roll);
 		up = Vector3::Transform(up, upRotation);
