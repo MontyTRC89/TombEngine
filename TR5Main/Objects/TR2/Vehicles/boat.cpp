860a076ad1ed933f5bc9f02065d3d0a64d3f410a
Nils <nils.gaitzsch@googlemail.com>
2020-09-15 08:10:38 +0200

    Added Boat foam, Skidoo snow trail

--- TR5Main/Objects/TR2/Vehicles/boat.cpp
+++ TR5Main/Objects/TR2/Vehicles/boat.cpp
@@ -10,6 +10,7 @@
 #include "input.h"
 #include "sound.h"
 #include <Game\effect2.h>
+#include <Game\particle\SimpleParticle.h>
 
 struct BOAT_INFO
 {
@@ -80,7 +81,8 @@ enum BOAT_STATE
 
 void DoBoatWakeEffect(ITEM_INFO* boat)
 {
-	int c = phd_cos(boat->pos.yRot);
+	T5M::Effects::TriggerSpeedboatFoam(boat);
+	/*int c = phd_cos(boat->pos.yRot);
 	int s = phd_sin(boat->pos.yRot);
 
 	for (int i = 0; i < 3; i++)
@@ -147,7 +149,7 @@ void DoBoatWakeEffect(ITEM_INFO* boat)
 		spark->sSize = spark->size = ((GetRandomControl() & 3) + 16) * 4;
 		spark->dSize = 2 * spark->size;
 		spark->def = Objects[ID_DEFAULT_SPRITES].meshIndex + 17;
-	}
+	}*/
 }
 
 void GetBoatGetOff(ITEM_INFO* boat)
