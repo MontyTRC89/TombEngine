c305d0bdc9427285e1798cc10dc24f9245fc8cda
asasas9500 <asasas9500@gmail.com>
2020-09-27 20:48:01 -0300

    Commit new files

---
+++ TR5Main/Objects/Generic/generic_objects.cpp
@@ -0,0 +1,26 @@
+#include "framework.h"
+#include "generic_objects.h"
+
+/// objects
+#include "generic_bridge.h"
+
+/// necessary import
+#include "setup.h"
+
+static void StartObject()
+{
+	OBJECT_INFO* obj;
+
+	obj = &Objects[ID_BRIDGE];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseBridge;
+		obj->floor = BridgeFloor;
+		obj->ceiling = BridgeCeiling;
+	}
+}
+
+void InitialiseGenericObjects()
+{
+	StartObject();
+}
