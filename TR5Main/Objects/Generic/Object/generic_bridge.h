c252205ca662ca7dc90c347dea24e8ed68844911
asasas9500 <asasas9500@gmail.com>
2020-09-28 21:01:16 -0300

    Add more bridge slots; Allow optional floor and ceiling overriding

--- TR5Main/Objects/Generic/Object/generic_bridge.h
+++ TR5Main/Objects/Generic/Object/generic_bridge.h
@@ -2,6 +2,6 @@
 #include "phd_global.h"
 
 void InitialiseBridge(short itemNumber);
-int BridgeFloor(short itemNumber, int x, int y, int z);
-int BridgeCeiling(short itemNumber, int x, int y, int z);
+std::optional<int> BridgeFloor(short itemNumber, int x, int y, int z);
+std::optional<int> BridgeCeiling(short itemNumber, int x, int y, int z);
 int GetOffset(PHD_3DPOS* pos, int x, int z);
