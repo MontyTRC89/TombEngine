7605313df1c0c84eb01a64c5b93e8d92b2c26e13
asasas9500 <asasas9500@gmail.com>
2020-09-29 09:53:36 -0300

    Move trapdoor to generic objects folder

--- TR5Main/Objects/Generic/generic_objects.cpp
+++ TR5Main/Objects/Generic/generic_objects.cpp
@@ -2,6 +2,7 @@
 #include "generic_objects.h"
 
 /// objects
+#include "generic_trapdoor.h"
 #include "generic_bridge.h"
 
 /// necessary import
@@ -9,9 +10,79 @@
 
 static void StartObject()
 {
+	auto obj = &Objects[ID_TRAPDOOR1];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = TrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
+	obj = &Objects[ID_TRAPDOOR2];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = TrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
+	obj = &Objects[ID_TRAPDOOR3];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = TrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
+	obj = &Objects[ID_FLOOR_TRAPDOOR1];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = FloorTrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
+	obj = &Objects[ID_FLOOR_TRAPDOOR2];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = FloorTrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
+	obj = &Objects[ID_CEILING_TRAPDOOR1];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = CeilingTrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
+	obj = &Objects[ID_CEILING_TRAPDOOR2];
+	if (obj->loaded)
+	{
+		obj->initialise = InitialiseTrapDoor;
+		obj->collision = CeilingTrapDoorCollision;
+		obj->control = TrapDoorControl;
+		obj->saveAnim = true;
+		obj->saveFlags = true;
+	}
+
 	for (int objNumber = ID_BRIDGE1; objNumber <= ID_BRIDGE8; ++objNumber)
 	{
-		auto obj = &Objects[objNumber];
+		obj = &Objects[objNumber];
 		if (obj->loaded)
 		{
 			obj->initialise = InitialiseBridge;
