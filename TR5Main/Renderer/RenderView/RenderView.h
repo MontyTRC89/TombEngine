8af7c0696e998b348e3bb9ba5f9c149eba316839
Nils <nils.gaitzsch@googlemail.com>
2020-10-11 16:47:36 +0200

    Refactor Renderer

--- TR5Main/Renderer/RenderView/RenderView.h
+++ TR5Main/Renderer/RenderView/RenderView.h
@@ -5,12 +5,20 @@
 #include "camera.h"
 #include "Frustum.h"
 #include "ConstantBuffers/CameraMatrixBuffer.h"
+#include "memory\LinearArrayBuffer.h"
+#include "RendererSprites.h"
 namespace T5M::Renderer {
 	struct RendererStatic;
 	struct RendererItem;
 	struct RendererLight;
 	struct RendererEffect;
 	struct RendererRoom;
+	constexpr auto MAX_ROOMS_DRAW = 256;
+	constexpr auto MAX_STATICS_DRAW = 128;
+	constexpr auto MAX_EFFECTS_DRAW = 16;
+	constexpr auto MAX_ITEMS_DRAW = 128;
+	constexpr auto MAX_LIGHTS_DRAW = 48;
+	constexpr auto MAX_SPRITES_DRAW = 512;
 	using DirectX::SimpleMath::Vector3;
 	using DirectX::SimpleMath::Vector2;
 	using DirectX::SimpleMath::Matrix;
@@ -30,11 +38,12 @@ namespace T5M::Renderer {
 	struct RenderView {
 		RenderViewCamera camera;
 		D3D11_VIEWPORT viewport;
-		std::vector<RendererRoom*> roomsToDraw;
-		std::vector<RendererStatic*> staticsToDraw;
-		std::vector<RendererEffect*> effectsToDraw;
-		std::vector<RendererItem*> itemsToDraw;
-		std::vector<RendererLight*> lightsToDraw;
+		T5M::Memory::LinearArrayBuffer<RendererRoom*,MAX_ROOMS_DRAW> roomsToDraw;
+		T5M::Memory::LinearArrayBuffer<RendererStatic*,MAX_STATICS_DRAW> staticsToDraw;
+		T5M::Memory::LinearArrayBuffer<RendererEffect*,MAX_EFFECTS_DRAW> effectsToDraw;
+		T5M::Memory::LinearArrayBuffer<RendererItem*,MAX_ITEMS_DRAW> itemsToDraw;
+		T5M::Memory::LinearArrayBuffer<RendererLight*, MAX_LIGHTS_DRAW> lightsToDraw;
+		T5M::Memory::LinearArrayBuffer<RendererSpriteToDraw, MAX_SPRITES_DRAW> spritesToDraw;
 		RenderView(CAMERA_INFO* cam, float roll, float fov, float nearPlane, float farPlane, int w, int h);
 		RenderView(const Vector3& pos, const Vector3& dir, const Vector3& up, int w, int h, int room, float nearPlane, float farPlane, float fov);
 		void fillConstantBuffer(CCameraMatrixBuffer& bufferToFill);
