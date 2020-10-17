e9c0a0b77daa1d902a71e6a0b70e0d65a7174e46
Woops <woopstombraider@gmail.com>
2020-10-17 01:21:07 -0500

    fix weapons/arms rotations and fix revolver anims

--- TR5Main/Game/control.cpp
+++ TR5Main/Game/control.cpp
@@ -2186,8 +2186,9 @@ int GetTargetOnLOS(GAME_VECTOR *src, GAME_VECTOR *dest, int DrawTarget, int firi
 
 		GetFloor(target.x, target.y, target.z, &target.roomNumber);
 
-		if ((itemNumber >= 0) && (BaddieSlots[itemNumber].itemNum != NO_ITEM))  // BUGFIX: ensure target has AI. No more pistol desync and camera wobble when shooting non-AI movable objects.
-			Lara.target = &g_Level.Items[itemNumber];
+//		if ((itemNumber >= 0) && (BaddieSlots[itemNumber].itemNum != NO_ITEM))  // BUGFIX: ensure target has AI. No more pistol desync and camera wobble when shooting non-AI movable objects.
+//			Lara.target = &g_Level.Items[itemNumber];
+		// this is crashing and it's not really doing anything..
 
 		if (firing)
 		{
