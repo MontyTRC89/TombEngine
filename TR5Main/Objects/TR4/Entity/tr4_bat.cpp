55044709db415749eba7b2fdc68b62f5328b45f6
MontyTRC89 <montyhammet@hotmail.it>
2020-12-01 18:35:21 +0100

    Fixed ID_BAT (but see notes in tr4_bat.cpp)

--- TR5Main/Objects/TR4/Entity/tr4_bat.cpp
+++ TR5Main/Objects/TR4/Entity/tr4_bat.cpp
@@ -25,7 +25,7 @@ constexpr auto BAT_ANIM_FALLING = 3;
 constexpr auto BAT_ANIM_IDLE = 5;
 constexpr auto BAT_ATTACK_RANGE = SQUARE(CLICK(1));
 constexpr auto BAT_TARGETING_RANGE = SQUARE(SECTOR(5));
-constexpr auto BAT_TARGET_YPOS = SQUARE(CLICK(2) / 18);
+constexpr auto BAT_TARGET_YPOS = 896;
 constexpr auto BAT_DAMAGE = 2;
 static BITE_INFO batBite(0, 16, 45, 4);
 
@@ -37,7 +37,9 @@ static bool isBatCollideTarget(ITEM_INFO* item)
 void InitialiseBat(short itemNumber)
 {
 	ITEM_INFO* item = &g_Level.Items[itemNumber];
+
 	InitialiseCreature(itemNumber);
+
 	item->animNumber = Objects[item->objectNumber].animIndex + BAT_ANIM_IDLE;
 	item->frameNumber = g_Level.Anims[item->animNumber].frameBase;
 	item->goalAnimState = BAT_IDLE;
@@ -59,44 +61,31 @@ void BatControl(short itemNumber)
 	bat = GetCreatureInfo(item);
 	angle = 0;
 
-    if (item->hitPoints <= 0)
+    if (item->hitPoints > 0)
     {
-        if (item->pos.yPos >= item->floor)
-        {
-            item->goalAnimState = BAT_DEATH;
-            item->pos.yPos = item->floor;
-            item->gravityStatus = false;
-        }
-        else
-        {
-            item->animNumber = Objects[item->objectNumber].animIndex + BAT_ANIM_FALLING;
-            item->frameNumber = g_Level.Anims[item->animNumber].frameBase;
-            item->goalAnimState = BAT_FALLING;
-            item->currentAnimState = BAT_FALLING;
-            item->speed = 0;
-        }
-    }
-    else
-    {
-        if (item->aiBits & ALL_AIOBJ)
+        if (item->aiBits)
         {
             GetAITarget(bat);
         }
         else
         {
+            bat->enemy = LaraItem;
+
+            // NOTE: it seems weird, bat could target any enemy including dogs for example
+            
             // check if voncroy are in range !
             // take in account that the bat will always target voncroy if he exist and triggered !
             // the bat will ignore lara completly !
-            bestdistance = MAXINT;
+            /*bestdistance = MAXINT;
             bat->enemy = LaraItem;
 
             slots = &BaddieSlots[0];
             for (int i = 0; i < NUM_SLOTS; i++, slots++)
             {
-				if (slots->itemNum != NO_ITEM)
+				if (slots->itemNum != NO_ITEM && slots->itemNum != itemNumber)
 				{
 					target = &g_Level.Items[slots->itemNum];
-					if (target->objectNumber == ID_VON_CROY && target->status != ITEM_INVISIBLE)
+					if (target->objectNumber == ID_VON_CROY)
 					{
 						int x, z;
 						x = target->pos.xPos - item->pos.xPos;
@@ -109,45 +98,56 @@ void BatControl(short itemNumber)
 						}
 					}
 				}
-            }
+            }*/
         }
 
+        // NOTE: chaned from TIMID to VIOLENT, otherwise the bat seems to ignore Lara. 
+        // Personally, i feel fine with bat always VIOLENT, but I will inspect also GetCreatureMood and CreatureMood functions
+        // for bugs.
+
         CreatureAIInfo(item, &info);
-        GetCreatureMood(item, &info, TIMID);
-        // note: this random dont exist in TR4 !
-        // this part set the bat in escape mood, but for too long !
-        // lara can escape and shot it easy ....
-        if (bat->flags && !(GetRandomControl() & 16))
+        GetCreatureMood(item, &info, VIOLENT);
+        
+        if (bat->flags)
             bat->mood = ESCAPE_MOOD;
-        else if (bat->flags && GetRandomControl() & 24) // fine maybe ? the bat react more with "16" but this reaction is too fast !
-            bat->mood = STALK_MOOD;
-        CreatureMood(item, &info, TIMID);
+       
+        CreatureMood(item, &info, VIOLENT);
+        
         angle = CreatureTurn(item, BAT_ANGLE);
 
         switch (item->currentAnimState)
         {
         case BAT_IDLE:
-            if (info.distance < BAT_TARGETING_RANGE || item->hitStatus || bat->hurtByLara)
+            if (info.distance < BAT_TARGETING_RANGE 
+                || item->hitStatus 
+                || bat->hurtByLara)
                 item->goalAnimState = BAT_START;
             break;
+
         case BAT_FLY:
             if (info.distance < BAT_ATTACK_RANGE || !(GetRandomControl() & 0x3F))
                 bat->flags = 0;
 
             if (!bat->flags)
             {
-                if (isBatCollideTarget(item) || bat->enemy != LaraItem)
+                if (item->touchBits
+                    || bat->enemy != LaraItem
+                    && info.distance < BAT_ATTACK_RANGE
+                    && info.ahead
+                    && abs(item->pos.yPos - bat->enemy->pos.yPos) < BAT_TARGET_YPOS)
                 {
-                    if (info.distance < BAT_ATTACK_RANGE
-                        && info.ahead && abs(item->pos.yPos - bat->enemy->pos.yPos) < BAT_TARGET_YPOS)
-                    {
-                        item->goalAnimState = BAT_ATK;
-                    }
+                    item->goalAnimState = BAT_ATK;
                 }
             }
             break;
+
         case BAT_ATK:
-            if (!bat->flags && (isBatCollideTarget(item) || bat->enemy != LaraItem) && info.distance < BAT_ATTACK_RANGE && info.ahead && abs(item->pos.yPos - bat->enemy->pos.yPos) < BAT_TARGET_YPOS)
+            if (!bat->flags 
+                && (item->touchBits 
+                || bat->enemy != LaraItem) 
+                && info.distance < BAT_ATTACK_RANGE
+                && info.ahead && 
+                abs(item->pos.yPos - bat->enemy->pos.yPos) < BAT_TARGET_YPOS)
             {
                 CreatureEffect(item, &batBite, DoBloodSplat);
                 if (bat->enemy == LaraItem)
@@ -163,6 +163,31 @@ void BatControl(short itemNumber)
                 bat->mood = BORED_MOOD;
             }
             break;
+        }
+    }
+    else if (item->currentAnimState == BAT_ATK)
+    {
+        item->animNumber = Objects[item->objectNumber].animIndex + 1;
+        item->frameNumber = g_Level.Anims[item->animNumber].frameBase;
+        item->goalAnimState = BAT_FLY;
+        item->currentAnimState = BAT_FLY;
+    }
+    else
+    {
+        if (item->pos.yPos >= item->floor)
+        {
+            item->goalAnimState = BAT_DEATH;
+            item->pos.yPos = item->floor;
+            item->gravityStatus = false;
+        }
+        else
+        {
+            item->gravityStatus = true;
+            item->animNumber = Objects[item->objectNumber].animIndex + BAT_ANIM_FALLING;
+            item->frameNumber = g_Level.Anims[item->animNumber].frameBase;
+            item->goalAnimState = BAT_FALLING;
+            item->currentAnimState = BAT_FALLING;
+            item->speed = 0;
         }
     }
 
