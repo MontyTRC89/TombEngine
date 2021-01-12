604eac4351ce28b3a88e0727be6f3cf8b6c001f3
KrysB4k <krystianb.96@poczta.onet.pl>
2021-01-12 20:11:03 +0100

    Fix pushable collision bug
    Remove bridge from pushable (stack) on movement start and add bridge from pushable (stack) on movement stop

--- TR5Main/Objects/TR5/Object/tr5_pushableblock.cpp
+++ TR5Main/Objects/TR5/Object/tr5_pushableblock.cpp
@@ -190,6 +190,15 @@ void PushableBlockControl(short itemNumber)
 						belowItem->itemFlags[1] = NO_ITEM;
 				}
 			}
+
+			T5M::Floordata::RemoveBridge(itemNumber);
+
+			stackIndex = item->itemFlags[1];
+			while (stackIndex != NO_ITEM) // remove all attached bridges from stack
+			{
+				T5M::Floordata::RemoveBridge(stackIndex);
+				stackIndex = g_Level.Items[stackIndex].itemFlags[1];
+			}
 		}
 
 		if (LaraItem->frameNumber == g_Level.Anims[LaraItem->animNumber].frameEnd - 1)
@@ -293,6 +302,15 @@ void PushableBlockControl(short itemNumber)
 
 				}
 			}
+
+			T5M::Floordata::RemoveBridge(itemNumber);
+
+			stackIndex = item->itemFlags[1];
+			while (stackIndex != NO_ITEM) // remove all attached bridges from stack
+			{
+				T5M::Floordata::RemoveBridge(stackIndex);
+				stackIndex = g_Level.Items[stackIndex].itemFlags[1];
+			}
 		}
 
 		if (LaraItem->frameNumber == g_Level.Anims[LaraItem->animNumber].frameEnd - 1)
@@ -354,6 +372,15 @@ void PushableBlockControl(short itemNumber)
 			{
 				//AlterFloorHeight(item, -((item->triggerFlags - 64) * 256));
 				AdjustStopperFlag(item, item->itemFlags[0] + 0x8000, 0);
+			}
+
+			T5M::Floordata::AddBridge(itemNumber);
+
+			stackIndex = item->itemFlags[1];
+			while (stackIndex != NO_ITEM) // remove all attached bridges from stack
+			{
+				T5M::Floordata::AddBridge(stackIndex);
+				stackIndex = g_Level.Items[stackIndex].itemFlags[1];
 			}
 
 
