6d0557c6679c31e379ca7c073f86e38943098ed0
Raildex <n@a>
2021-01-21 19:35:57 +0100

    Fixing Jeep wheels (and potentially Motorbike)

--- TR5Main/Objects/TR4/Vehicles/jeep.cpp
+++ TR5Main/Objects/TR4/Vehicles/jeep.cpp
@@ -16,26 +16,7 @@
 #include "setup.h"
 #include "level.h"
 using std::vector;
-typedef struct JEEP_INFO
-{
-	short rot1;
-	short rot2;
-	short rot3;
-	short rot4;
-	int velocity;
-	int revs;
-	short engineRevs;
-	short trackMesh;
-	int jeepTurn;
-	int fallSpeed;
-	short momentumAngle;
-	short extraRotation;
-	short unknown0;
-	int pitch;
-	short flags;
-	short unknown1;
-	short unknown2;
-};
+
 
 #define JF_FALLING					0x40
 #define JF_DEAD						0x80
