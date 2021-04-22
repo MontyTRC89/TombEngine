7b6b275b29b785357d246a378356632c7182a17a
MontyTRC89 <montyhammet@hotmail.it>
2021-04-22 05:58:40 +0200

    More fixes to SAS

--- TR5Main/Objects/TR4/Entity/tr4_sas.h
+++ TR5Main/Objects/TR4/Entity/tr4_sas.h
@@ -6,3 +6,4 @@ void SasControl(short itemNumber);
 void InitialiseSasDying(short itemNumber);
 void SasDyingControl(short itemNumber);
 void SasDragBlokeCollision(short itemNumber, ITEM_INFO* l, COLL_INFO* c);
+void SasFireGrenade(ITEM_INFO* item, short angle1, short angle2);
