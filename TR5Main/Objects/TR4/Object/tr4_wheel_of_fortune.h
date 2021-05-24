639107a9abe5fe5accb6204f2a96d60c087930bb
Trxye <woopstombraider@gmail.com>
2021-05-23 23:02:35 -0500

    trigger_item_in_room(); CheckSenetWinner();

--- TR5Main/Objects/TR4/Object/tr4_wheel_of_fortune.h
+++ TR5Main/Objects/TR4/Object/tr4_wheel_of_fortune.h
@@ -4,3 +4,5 @@ void InitialiseGameStix(short itemNumber);
 void GameStixControl(short itemNumber);
 void _0x0040FAE0(ITEM_INFO* item);
 void SenetPieceExplosionEffect(ITEM_INFO* item, int color, int speed);
+void trigger_item_in_room(short room_number, int object);
+int CheckSenetWinner(short num);
