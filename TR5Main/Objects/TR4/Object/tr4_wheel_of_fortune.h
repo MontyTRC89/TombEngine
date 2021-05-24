0578833c9eed912a8b7d46c0f6ee86a9e1edf44c
Trxye <woopstombraider@gmail.com>
2021-05-23 22:23:10 -0500

    _0x0040FAE0(); SenetPieceExplosionEffect();

--- TR5Main/Objects/TR4/Object/tr4_wheel_of_fortune.h
+++ TR5Main/Objects/TR4/Object/tr4_wheel_of_fortune.h
@@ -2,3 +2,5 @@
 
 void InitialiseGameStix(short itemNumber);
 void GameStixControl(short itemNumber);
+void _0x0040FAE0(ITEM_INFO* item);
+void SenetPieceExplosionEffect(ITEM_INFO* item, int color, int speed);
