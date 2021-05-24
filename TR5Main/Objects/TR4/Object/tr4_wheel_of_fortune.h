5f906a932551c4ee3f62b49da0a226380a48345c
asasas9500 <asasas9500@gmail.com>
2021-05-24 15:35:22 -0300

    Add MakeMove()

--- TR5Main/Objects/TR4/Object/tr4_wheel_of_fortune.h
+++ TR5Main/Objects/TR4/Object/tr4_wheel_of_fortune.h
@@ -1,8 +1,11 @@
 #pragma once
 
+struct ITEM_INFO;
+
 void InitialiseGameStix(short itemNumber);
 void GameStixControl(short itemNumber);
 void _0x0040FAE0(ITEM_INFO* item);
 void SenetPieceExplosionEffect(ITEM_INFO* item, int color, int speed);
 void trigger_item_in_room(short room_number, int object);
 int CheckSenetWinner(short num);
+void MakeMove(int piece, int displacement);
