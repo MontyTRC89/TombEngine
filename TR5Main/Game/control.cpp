41508e1b923aa4bc6ead4d8b7ebb5928f90c4ae0
Trxye <woopstombraider@gmail.com>
2021-05-27 18:02:25 -0500

    fix setting ClockworkBeetleFlag;

--- TR5Main/Game/control.cpp
+++ TR5Main/Game/control.cpp
@@ -1890,6 +1890,7 @@ int GetFloorHeight(FLOOR_INFO *floor, int x, int y, int z)
 				case CLIMB_TYPE:
 				case MONKEY_TYPE:
 				case TRIGTRIGGER_TYPE:
+				case MINER_TYPE:
 					if (!TriggerIndex)
 						TriggerIndex = data - 1;
 					break;
