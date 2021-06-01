70b3635dae30e8d4c435cdc60e6c694a03ab4aca
MontyTRC89 <montyhammet@hotmail.it>
2021-06-01 13:02:05 +0200

    Fixed ID_MUMMY init; Bugged 2 blocks platform;

--- TR5Main/Objects/TR5/Object/tr5_twoblockplatform.h
+++ TR5Main/Objects/TR5/Object/tr5_twoblockplatform.h
@@ -3,6 +3,8 @@
 
 void InitialiseTwoBlocksPlatform(short itemNumber);
 void TwoBlocksPlatformControl(short itemNumber);
-void TwoBlocksPlatformFloor(ITEM_INFO* item, int x, int y, int z, int* height);
-void TwoBlocksPlatformCeiling(ITEM_INFO* item, int x, int y, int z, int* height);
 BOOL IsOnTwoBlocksPlatform(ITEM_INFO* item, int x, int z);
+std::optional<int> TwoBlocksPlatformFloor(short itemNumber, int x, int y, int z);
+std::optional<int> TwoBlocksPlatformCeiling(short itemNumber, int x, int y, int z);
+int TwoBlocksPlatformFloorBorder(short itemNumber);
+int TwoBlocksPlatformCeilingBorder(short itemNumber);
