dc9b86a3751c8988f27fb598d12a7162932c7f37
MontyTRC89 <montyhammet@hotmail.it>
2021-06-20 06:34:52 +0200

    Added Strings.lua

---
+++ TR5Main/Scripting/Scripts/Strings.lua
@@ -0,0 +1,1042 @@
+-- Automatically generated document using TombIDE
+-- Do not add any comments into this document as they are
+-- going to be removed next time the file is regenerated.
+
+local strings = {
+	apply = {
+		"Apply",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	back = {
+		"Back",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	binoculars = {
+		"Binoculars",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	cancel = {
+		"Cancel",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	caustics = {
+		"Underwater Caustics",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	choose_ammo = {
+		"Choose Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	choose_weapon = {
+		"Choose Weapon",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	combine = {
+		"Combine",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	combine_with = {
+		"Combine With",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls = {
+		"Controls",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_action = {
+		"Action",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_dash = {
+		"Dash",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_draw_weapon = {
+		"Draw Weapon",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_duck = {
+		"Duck",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_inventory = {
+		"Inventory",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_jump = {
+		"Jump",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_look = {
+		"Look",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_move_backward = {
+		"Move Backward",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_move_forward = {
+		"Move Forward",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_move_left = {
+		"Move Left",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_move_right = {
+		"Move Right",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_roll = {
+		"Roll",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_step_left = {
+		"Step Left",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_step_right = {
+		"Step Right",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_use_flare = {
+		"Use Flare",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	controls_walk = {
+		"Walk",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	crossbow = {
+		"Crossbow",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	crossbow_ammo1 = {
+		"Crossbow Normal Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	crossbow_ammo2 = {
+		"Crossbow Poison Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	crossbow_ammo3 = {
+		"Crossbow Explosive Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	crossbow_lasersight = {
+		"Crossbow + Lasersight",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	crowbar = {
+		"Crowbar",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	diary = {
+		"Diary",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	disabled = {
+		"Disabled",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	display = {
+		"Display settings",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	display_adapter = {
+		"Display Adapter",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	distance_travelled = {
+		"Distance Travelled",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	enable_sound = {
+		"Enable Sounds",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	enabled = {
+		"Enabled",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	equip = {
+		"Equip",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	examine = {
+		"Examine",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	exit_game = {
+		"Exit game",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	exit_to_title = {
+		"Exit to title",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	flares = {
+		"Flares",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	grenade_launcher = {
+		"Grenade Gun",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	grenade_launcher_ammo1 = {
+		"Grenadegun Normal Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	grenade_launcher_ammo2 = {
+		"Grenadegun Super Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	grenade_launcher_ammo3 = {
+		"Grenadegun Flash Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	harpoon = {
+		"Harpoon",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	harpoon_ammo = {
+		"Harpoon Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	headset = {
+		"Headset",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	hk = {
+		"HK",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	hk_ammo = {
+		"HK Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	hk_burst_mode = {
+		"HK Nurst mode",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	hk_rapid_mode = {
+		"HK Rapid mode",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	hk_silenced = {
+		"HK (Silenced)",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	hk_sniper_mode = {
+		"HK Sniper mode",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	lara_home = {
+		"Lara's home",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	large_medipack = {
+		"Big Medipack",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	lasersight = {
+		"Lasersight",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	music_volume = {
+		"Music Volume",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	new_game = {
+		"New game",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	ok = {
+		"OK",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	output_settings = {
+		"Output Settings",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	passport = {
+		"Passport",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	pistols = {
+		"Pistols",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	pistols_ammo = {
+		"Pistols Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	render_options = {
+		"Render Options",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	revolver = {
+		"Revolver",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	revolver_ammo = {
+		"Revolver Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	revolver_laser = {
+		"Revolver + Laersight",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	rocket_ammo = {
+		"Rocket Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	rocket_launcher = {
+		"Rocket Launcher",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	save_game = {
+		"Save game",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	savegame_timestamp = {
+		"%d Days %d:%d:%d",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	screen_resolution = {
+		"Screen Resolution",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	secrets_found = {
+		"Found Secrets",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	select_level = {
+		"Select Level",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	separe = {
+		"Separe",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	sfx_volume = {
+		"FX Volume",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	shadows = {
+		"Shadows",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	shotgun = {
+		"Shotgun",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	shotgun_ammo1 = {
+		"Shotgun Normal Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	shotgun_ammo2 = {
+		"Shotgun Wideshot Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	small_medipack = {
+		"Small Medipack",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	sound = {
+		"Sound settings",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	special_sound_fx = {
+		"Special Sound FX",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	statistics = {
+		"Statistics",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	time_taken = {
+		"Time Taken",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	timex = {
+		"Timex - TMX",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	torch = {
+		"Torch",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	unused = {
+		"Unused",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	use = {
+		"Use",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	used_ammos = {
+		"Ammo Used",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	used_medipacks = {
+		"Used Medipacks",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	uzi = {
+		"Uzi",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	uzi_ammo = {
+		"Uzi Ammo",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	volumetric_fog = {
+		"Volumetric Fog",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	waiting_for_key = {
+		"Waiting For Key",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	windowed = {
+		"Windowed",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	load_game = {
+		"Load Game",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	},
+	level_andrea1 = {
+		"Our test level",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	}
+}
+
+GameFlow:SetStrings(strings)
+
+local languages = {
+	"English",
+	"Italian",
+	"German",
+	"French",
+	"Dutch",
+	"Spanish",
+	"Japanese",
+	"Russian"
+}
+
+GameFlow:SetLanguageNames(languages)
+
