56a305b546eb8de444cd3c62d935ae219e83b2e2
hispidence <squidshirehimself@gmail.com>
2021-06-30 14:08:12 +0100

    Add CreateEmpty, so that new can be called on the Lua side with no args.
    Change pos and rot from arrays to GameScript[Position/Rotation].
    
    Add getters and setters for the members used so far.
    
    Add Init, which for now is needed to get an item working properly.

--- TR5Main/Scripting/GameScriptItemInfo.h
+++ TR5Main/Scripting/GameScriptItemInfo.h
@@ -2,8 +2,10 @@
 
 namespace sol {
 	class state;
-	template <typename T> class as_table_t;
+	template <typename T> struct as_table_t;
 }
+class GameScriptPosition;
+class GameScriptRotation;
 
 class GameScriptItemInfo
 {
@@ -15,16 +17,43 @@ public:
 	GameScriptItemInfo& operator=(GameScriptItemInfo const& other) = delete;
 	GameScriptItemInfo(GameScriptItemInfo const& other) = delete;
 	static void Register(sol::state *);
+	static std::unique_ptr<GameScriptItemInfo> CreateEmpty();
 	static std::unique_ptr<GameScriptItemInfo> Create(
-		short hp,
-		short currentAnim,
-		short requiredAnimState,
-		sol::as_table_t<std::array<int, 3>> pos,
-		sol::as_table_t<std::array<short, 3>> rot,
-		sol::as_table_t<std::array<short, 8>> itemFlags,
-		short ocb,
-		byte aiBits,
-		short status,
-		bool active,
-		bool hitStatus);
+		GameScriptPosition pos,
+		GameScriptRotation aRot,
+		short aCurrentAnim,
+		short aRequiredAnim,
+		short aHp,
+		short aOcb,
+		sol::as_table_t<std::array<short, 8>> aFlags,
+		byte aAiBits,
+		short aStatus,
+		bool aActive,
+		bool aHitStatus
+		);
+	GameScriptPosition GetPos() const;
+	GameScriptRotation GetRot() const;
+	short GetCurrentAnim() const;
+	short GetRequiredAnim() const;
+	short GetHP() const;
+	short GetOCB() const;
+	sol::as_table_t<std::array<short, 8>>  GetItemFlags() const;
+	byte GetAIBits() const;
+	short GetStatus() const;
+	bool GetHitStatus() const;
+	bool GetActive() const;
+
+	void Init();
+
+	void SetPos(GameScriptPosition const& pos);
+	void SetRot(GameScriptRotation const& rot);
+	void SetCurrentAnim(short anim);
+	void SetRequiredAnim(short anim);
+	void SetHP(short hp);
+	void SetOCB(short ocb);
+	void SetItemFlags(sol::as_table_t<std::array<short, 8>>  const & arr);
+	void SetAIBits(byte bits);
+	void SetStatus(short status);
+	void SetHitStatus(bool hitStatus);
+	void SetActive(bool active);
 };
