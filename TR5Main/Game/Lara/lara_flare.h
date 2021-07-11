54e14ccb3f385fe3d7a369c4f2a3ed8488c673e1
hispidence <squidshirehimself@gmail.com>
2021-07-11 16:55:20 +0100

    Make some functions take a GAME_OBJECT_ID instead of a short. Make AIOBJECT use a GAME_OBJECT_ID instead of a short.

--- TR5Main/Game/Lara/lara_flare.h
+++ TR5Main/Game/Lara/lara_flare.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "items.h"
 
+enum GAME_OBJECT_ID : short;
+
 void FlareControl(short item_number);
 void ready_flare();
 void undraw_flare_meshes();
@@ -8,7 +10,7 @@ void draw_flare_meshes();
 void undraw_flare();
 void draw_flare();
 void set_flare_arm(int frame);
-void CreateFlare(short object, int thrown);
+void CreateFlare(GAME_OBJECT_ID object, int thrown);
 void DrawFlareInAir(ITEM_INFO* item);
 void DoFlareInHand(int flare_age);
 int DoFlareLight(PHD_VECTOR* pos, int flare_age);
