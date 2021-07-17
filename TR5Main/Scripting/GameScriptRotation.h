cb574c13c02b1731a943baf470c5dee501dbb79d
hispidence <squidshirehimself@gmail.com>
2021-07-17 23:51:01 +0100

    Add initial documentation comments for GameScriptPosition and GameScriptRotation.

--- TR5Main/Scripting/GameScriptRotation.h
+++ TR5Main/Scripting/GameScriptRotation.h
@@ -2,6 +2,13 @@
 
 #include "framework.h"
 
+/***
+Represents a rotation as a combination of individual
+angles, in degrees, about each axis.
+All values will be clamped to [-32768, 32767].
+@classmod Rotation
+@pragma nostrip
+*/
 namespace sol {
 	class state;
 }
@@ -9,9 +16,26 @@ struct PHD_3DPOS;
 
 class GameScriptRotation {
 public:
-	int								x;
-	int								y;
-	int								z;
+/// (int) rotation about x axis
+//@mem X
+
+/// (int) rotation about y axis
+//@mem Y
+
+/// (int) rotation about z axis
+//@mem Z
+
+	short								x;
+	short								y;
+	short								z;
+
+/*** 
+@int X rotation about x axis
+@int Y rotation about y axis
+@int Z rotation about z axis
+@return A Rotation object.
+@function Rotation.new
+*/
 
 	GameScriptRotation(int x, int y, int z);
 	GameScriptRotation(PHD_3DPOS const& pos);
