82e73efc2709f0d56bb57920ab345410e7b7df13
hispidence <squidshirehimself@gmail.com>
2021-07-17 23:51:01 +0100

    Add initial documentation comments for GameScriptPosition and GameScriptRotation.

--- TR5Main/Scripting/GameScriptPosition.h
+++ TR5Main/Scripting/GameScriptPosition.h
@@ -2,6 +2,11 @@
 
 #include "framework.h"
 
+/***
+Represents a position in the game world.
+@classmod Position
+@pragma nostrip
+*/
 namespace sol {
 	class state;
 }
@@ -9,10 +14,25 @@ struct PHD_3DPOS;
 
 class GameScriptPosition {
 public:
+/// (int) x coordinate
+//@mem X
+
+/// (int) y coordinate
+//@mem Y
+
+/// (int) z coordinate
+//@mem Z
 	int x;
 	int y;
 	int z;
 
+/*** 
+@int X x coordinate
+@int Y y coordinate
+@int Z z coordinate
+@return A Position object.
+@function Position.new
+*/
 	GameScriptPosition(int x, int y, int z);
 	GameScriptPosition(PHD_3DPOS const& pos);
 	void StoreInPHDPos(PHD_3DPOS& pos) const;
