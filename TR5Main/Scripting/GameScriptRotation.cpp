893c176c0ccebf6c9fda6b6c16307f8d4181dbcc
hispidence <squidshirehimself@gmail.com>
2021-07-20 17:49:14 +0100

    Move documentation from GameScriptRotation.h to ".cpp.

--- TR5Main/Scripting/GameScriptRotation.cpp
+++ TR5Main/Scripting/GameScriptRotation.cpp
@@ -2,17 +2,40 @@
 #include "GameScriptRotation.h"
 #include "phd_global.h"
 
+/***
+Represents a rotation as a combination of individual
+angles, in degrees, about each axis.
+All values will be clamped to [-32768, 32767].
+@classmod Rotation
+@pragma nostrip
+*/
+
 void GameScriptRotation::Register(sol::state* state)
 {
 	state->new_usertype<GameScriptRotation>("Rotation",
 		sol::constructors<GameScriptRotation(int, int, int)>(),
+
+/// (int) rotation about x axis
+//@mem X
 		"X", &GameScriptRotation::x,
+
+/// (int) rotation about x axis
+//@mem Y
 		"Y", &GameScriptRotation::y,
+
+/// (int) rotation about x axis
+//@mem Z
 		"Z", &GameScriptRotation::z
 	);
 }
 
-
+/*** 
+@int X rotation about x axis
+@int Y rotation about y axis
+@int Z rotation about z axis
+@return A Rotation object.
+@function Rotation.new
+*/
 GameScriptRotation::GameScriptRotation(int aX, int aY, int aZ)
 {
 	x = aX;
