7ac16ab2d362c51bc5429fa1db71982a23f2cbff
hispidence <squidshirehimself@gmail.com>
2021-08-03 15:08:43 +0100

    Add TENLog and TENScriptException.

--- TR5Main/Game/debug/debug.h
+++ TR5Main/Game/debug/debug.h
@@ -5,6 +5,7 @@ constexpr bool DebugBuild = true;
 constexpr bool DebugBuild = false;
 #endif
 #include <stdexcept>
+#include <string_view>
 
 inline void assertion(const bool& expr,const char* msg) noexcept {
 	if constexpr (DebugBuild) {
@@ -23,4 +24,25 @@ inline void logE(const T&... x) {
 	if constexpr (DebugBuild) {
 		(std::cerr << ... << x) << std::endl;
 	}
+};
+
+enum class LogLevel
+{
+	Error,
+	Warning,
+	Info
+};
+
+enum class LogConfig
+{
+	Debug,
+	All
+};
+
+void TENLog(std::string_view str, LogLevel level = LogLevel::Info, LogConfig config = LogConfig::Debug);
+
+class TENScriptException : public std::runtime_error
+{
+public:
+	using std::runtime_error::runtime_error;
 };
