9b6a10ecb005735e24bb927308f921b40a640733
hispidence <squidshirehimself@gmail.com>
2021-08-03 15:08:43 +0100

    Add TENLog and TENScriptException.

---
+++ TR5Main/Game/debug/debug.cpp
@@ -0,0 +1,29 @@
+#include "framework.h"
+#include <iostream>
+
+void TENLog(std::string_view str, LogLevel level, LogConfig config)
+{
+	if constexpr (DebugBuild)
+	{
+		if (LogConfig::Debug == config)
+		{
+			return;
+		}
+	}
+
+	switch (level)
+	{
+	case LogLevel::Error:
+		std::cerr << "Error: " << str << "\n";
+		// Monty code goes here
+		break;
+	case LogLevel::Warning:
+		std::cout << "Warning: " << str << "\n";
+		// Monty code goes here
+		break;
+	case LogLevel::Info:
+		std::cout << "Info: " << str << "\n";
+		// Monty code goes here
+		break;
+	}
+}
