408bbf8e7384b1116be4505c47be0b3384994eaf
Lwmte <initiator@nm.ru>
2021-08-05 14:26:23 +0300

    Fix controls lockup if level was changed during flyby with disabled controls

--- TR5Main/Game/control.h
+++ TR5Main/Game/control.h
@@ -180,5 +180,6 @@ int is_object_in_room(short roomNumber, short objectNumber);
 void InterpolateAngle(short angle, short* rotation, short* outAngle, int shift);
 int IsRoomOutside(int x, int y, int z);
 void TestTriggersAtXYZ(int x, int y, int z, short roomNumber, int heavy, int flags);
+void ResetGlobals();
 
 unsigned CALLBACK GameMain(void*);
