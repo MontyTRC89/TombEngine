2d4c091b6472f47eb6bd828c2be11b0b0225a0d5
hispidence <squidshirehimself@gmail.com>
2021-08-07 19:00:45 +0100

    Make MakeReadOnlyTable a member function of LuaHandler so that GameFlow can use it too.

--- TR5Main/Scripting/LuaHandler.h
+++ TR5Main/Scripting/LuaHandler.h
@@ -12,4 +12,28 @@ public:
 
 	void ExecuteScript(const std::string & luaFilename);
 	void ExecuteString(const std::string & command);
+
+	template <typename T>void MakeReadOnlyTable(std::string const& tableName, T const& container)
+	{
+		auto mt = tableName + "Meta";
+		// Put all the data in the metatable	
+		m_lua->set(mt, sol::as_table(container));
+
+		// Make the metatable's __index refer to itself so that requests
+		// to the main table will go through to the metatable (and thus the
+		// container's members)
+		m_lua->safe_script(mt + ".__index = " + mt);
+
+		// Don't allow the table to have new elements put into it
+		m_lua->safe_script(mt + ".__newindex = function() error('" + tableName + " is read-only') end");
+
+		// Protect the metatable
+		m_lua->safe_script(mt + ".__metatable = 'metatable is protected'");
+
+		auto tab = m_lua->create_named_table(tableName);
+		m_lua->safe_script("setmetatable(" + tableName + ", " + mt + ")");
+
+		// point the initial metatable variable away from its contents. this is just for cleanliness
+		m_lua->safe_script(mt + "= nil");
+	}
 };
