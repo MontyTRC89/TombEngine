25d90c9820fae676395acc57c1fc669df071d84b
hispidence <squidshirehimself@gmail.com>
2021-08-11 01:23:07 +0100

    Refactor and add documenation for GameScriptInventoryObject.

--- TR5Main/Scripting/GameScriptInventoryObject.h
+++ TR5Main/Scripting/GameScriptInventoryObject.h
@@ -1,6 +1,20 @@
 #pragma once
 #include <string>
 #include "ItemEnumPair.h"
+#include "GameScriptRotation.h"
+#include "newinv2.h"
+
+static const std::unordered_map<std::string, rotflags> kRotAxes{
+	{"X", rotflags::INV_ROT_X},
+	{"Y", rotflags::INV_ROT_Y},
+	{"Z", rotflags::INV_ROT_Z}
+};
+
+static const std::unordered_map<std::string, item_options> kItemActions{
+	{"USE", item_options::OPT_USE},
+	{"EQUIP", item_options::OPT_EQUIP},
+	{"EXAMINE", item_options::OPT_EXAMINABLE}
+};
 
 namespace sol {
 	class state;
@@ -12,14 +26,14 @@ struct GameScriptInventoryObject
 	inv_objects slot;
 	float yOffset;
 	float scale;
-	float xRot;
-	float yRot;
-	float zRot;
-	short rotationFlags;
+	GameScriptRotation rot;
+	rotflags rotationFlags;
 	int meshBits;
-	__int64 operation;
+	item_options action;
 
-	GameScriptInventoryObject(std::string const & a_name, ItemEnumPair a_slot, float a_yOffset, float a_scale, float a_xRot, float a_yRot, float a_zRot, short a_rotationFlags, int a_meshBits, __int64 a_operation);
+	GameScriptInventoryObject(std::string const & a_name, ItemEnumPair a_slot, float a_yOffset, float a_scale, GameScriptRotation const & a_rot, rotflags a_rotationFlags, int a_meshBits, item_options a_actions);
 
 	static void Register(sol::state* lua);
+
+	void SetAction(item_options a_action);
 };
