MontyTRC89/TombEngine

commit abb7b0c862c80b3ee10d657a452767d8ce0e511f

Added some constants to TestTrigger(); Fixed ID_SETH_BLADE; Fixed many collision bugs with Lara;

TR5Main/Game/collide.cpp
@@ -572,12 +572,12 @@ int ItemPushLara(ITEM_INFO* item, ITEM_INFO* l, COLL_INFO* coll, int spazon, cha
 	BOUNDING_BOX* bounds;
 	short facing;
 
-	c = phd_cos(item->pos.yRot);
+	dx = l->pos.xPos - item->pos.xPos;		// Get Vector from Baddie to Lara
+	dz = l->pos.zPos - item->pos.zPos;
+	c = phd_cos(item->pos.yRot);					// Get Baddies Rotation
 	s = phd_sin(item->pos.yRot);
-	dx = LaraItem->pos.xPos - item->pos.xPos;
-	dz = LaraItem->pos.zPos - item->pos.zPos;
-	rx = c * dx - s * dz;
-	rz = c * dz + s * dx;
+	rx = (c * dx - s * dz);            	// Rotate Lara Vector into Baddie Frame
+	rz = (c * dz + s * dx);
 
 	if (bigpush & 2)
 		bounds = &GlobalCollisionBounds;
@@ -653,14 +653,7 @@ int ItemPushLara(ITEM_INFO* item, ITEM_INFO* l, COLL_INFO* coll, int spazon, cha
 
 	facing = coll->facing;
 	coll->facing = phd_atan(l->pos.zPos - coll->old.z, l->pos.xPos - coll->old.x);
-	if (l == LaraItem)
-	{
-		GetCollisionInfo(coll, l->pos.xPos, l->pos.yPos, l->pos.zPos, l->roomNumber, LARA_HITE);
-	}
-	else
-	{
-		GetObjectCollisionInfo(coll, l->pos.xPos, l->pos.yPos, l->pos.zPos, l->roomNumber, LARA_HITE);
-	}
+	GetCollisionInfo(coll, l->pos.xPos, l->pos.yPos, l->pos.zPos, l->roomNumber, LARA_HITE);
 	coll->facing = facing;
 
 	if (coll->collType == CT_NONE)
@@ -669,7 +662,7 @@ int ItemPushLara(ITEM_INFO* item, ITEM_INFO* l, COLL_INFO* coll, int spazon, cha
 		coll->old.y = l->pos.yPos;
 		coll->old.z = l->pos.zPos;
 
-		//UpdateLaraRoom(l, 0);
+		UpdateLaraRoom(l, -10);
 	}
 	else
 	{
@@ -781,7 +774,7 @@ int TestLaraPosition(OBJECT_COLLISION_BOUNDS* bounds, ITEM_INFO* item, ITEM_INFO
 	// HACK (REMOVED FOR NOW): it seems that a minus sign is required here. I don't know why, but it just works (tm) but we must 
 	// do more tests
 	Matrix matrix = Matrix::CreateFromYawPitchRoll(
-		TO_RAD(item->pos.yRot),
+		TO_RAD(-item->pos.yRot),
 		TO_RAD(item->pos.xRot),
 		TO_RAD(item->pos.zRot)
 	);
@@ -829,9 +822,13 @@ int Move3DPosTo3DPos(PHD_3DPOS* src, PHD_3DPOS* dest, int velocity, short angAdd
 		if (Lara.waterStatus != LW_UNDERWATER)
 		{
 			angle = mGetAngle(dest->xPos, dest->zPos, src->xPos, src->zPos);
-			direction = (GetQuadrant(angle) - GetQuadrant(dest->yRot)) & 3;
-
-			switch (direction)
+			//direction = (GetQuadrant(angle) - GetQuadrant(dest->yRot)) & 3;
+			
+			angle = (angle + 0x2000) / 0x4000;
+			angle = (angle - ((unsigned short)(dest->yRot + 0x2000) / 0x4000));
+			angle &= 3;
+			
+			switch (angle)
 			{
 			case 0:
 				LaraItem->animNumber = LA_SIDESTEP_LEFT;
@@ -2212,19 +2209,26 @@ void GenericSphereBoxCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll)
 	{
 		if (TestBoundsCollide(item, l, coll->radius))
 		{
-			int collided = TestCollision(item, l);
-			if (collided)
+			int collidedBits = TestCollision(item, l);
+			if (collidedBits)
 			{
 				short oldRot = item->pos.yRot;
 
 				item->pos.yRot = 0;
 				GetSpheres(item, CreatureSpheres, SPHERES_SPACE_WORLD, Matrix::Identity);
 				item->pos.yRot = oldRot;
-				
+
+				int deadlyBits = item->itemFlags[0];
 				SPHERE* sphere = &CreatureSpheres[0];
-				while (collided)
+
+				if (item->itemFlags[2] != 0)
 				{
-					if (collided & 1)
+					collidedBits &= ~1;
+				}
+
+				while (collidedBits)
+				{
+					if (collidedBits & 1)
 					{
 						GlobalCollisionBounds.X1 = sphere->x - sphere->r - item->pos.xPos;
 						GlobalCollisionBounds.X2 = sphere->x + sphere->r - item->pos.xPos;
@@ -2237,13 +2241,19 @@ void GenericSphereBoxCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll)
 						int y = l->pos.yPos;
 						int z = l->pos.zPos;
 
-						if (ItemPushLara(item, l, coll, (item->itemFlags[0] & (coll->enableSpaz / 32) & 1), 3) && item->itemFlags[0] & 1)
+						if (ItemPushLara(item, l, coll, ((deadlyBits & 1) & coll->enableSpaz), 3) && (deadlyBits & 1))
 						{
 							l->hitPoints -= item->itemFlags[3];
-							
+
 							int dx = x - l->pos.xPos;
 							int dy = y - l->pos.yPos;
 							int dz = z - l->pos.zPos;
+
+							if (dx || dy || dz)
+							{
+								if (TriggerActive(item))
+									TriggerLaraBlood();
+							}
 
 							if (!coll->enableBaddiePush)
 							{
@@ -2251,16 +2261,11 @@ void GenericSphereBoxCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll)
 								l->pos.yPos += dy;
 								l->pos.zPos += dz;
 							}
-
-							if (dx || dy || dz)
-							{
-								if (TriggerActive(item))
-									TriggerLaraBlood();
-							}
 						}
 					}
 
-					collided /= 2;
+					collidedBits >>= 1;
+					deadlyBits >>= 1;
 					sphere++;
 				}
 			}

TR5Main/Game/control.cpp
@@ -1029,7 +1029,7 @@ void TestTriggers(short *data, int heavy, int HeavyFlags)
 		case TRIGGER_TYPES::SWITCH:
 			value = *(data++) & 0x3FF;
 
-			if (flags & 0x100)
+			if (flags & ONESHOT)
 				g_Level.Items[value].itemFlags[0] = 1;
 
 			if (!SwitchTrigger(value, timer))
@@ -1044,42 +1044,42 @@ void TestTriggers(short *data, int heavy, int HeavyFlags)
 			break;
 
 		case TRIGGER_TYPES::MONKEY:
-			if (LaraItem->currentAnimState >= 75 &&
-				(LaraItem->currentAnimState <= 79 ||
-					LaraItem->currentAnimState == 82 ||
-					LaraItem->currentAnimState == 83))
+			if (LaraItem->currentAnimState >= LS_MONKEYSWING_IDLE &&
+				(LaraItem->currentAnimState <= LS_MONKEYSWING_TURN_180 ||
+					LaraItem->currentAnimState == LS_MONKEYSWING_TURN_LEFT ||
+					LaraItem->currentAnimState == LS_MONKEYSWING_TURN_RIGHT))
 				break;
 			return;
 
 		case TRIGGER_TYPES::TIGHTROPE_T:
-			if (LaraItem->currentAnimState >= 119 &&
-				LaraItem->currentAnimState <= 127 &&
-				LaraItem->currentAnimState != 126)
+			if (LaraItem->currentAnimState >= LS_TIGHTROPE_IDLE &&
+				LaraItem->currentAnimState <= LS_TIGHTROPE_RECOVER_BALANCE &&
+				LaraItem->currentAnimState != LS_DOVESWITCH)
 				break;
 			return;
 
 		case TRIGGER_TYPES::CRAWLDUCK_T:
-			if (LaraItem->currentAnimState == 80 ||
-				LaraItem->currentAnimState == 81 ||
-				LaraItem->currentAnimState == 84 ||
-				LaraItem->currentAnimState == 85 ||
-				LaraItem->currentAnimState == 86 ||
-				LaraItem->currentAnimState == 71 ||
-				LaraItem->currentAnimState == 72 ||
-				LaraItem->currentAnimState == 105 ||
-				LaraItem->currentAnimState == 106)
+			if (LaraItem->currentAnimState == LS_DOVESWITCH ||
+				LaraItem->currentAnimState == LS_CRAWL_IDLE ||
+				LaraItem->currentAnimState == LS_CRAWL_TURN_LEFT ||
+				LaraItem->currentAnimState == LS_CRAWL_TURN_RIGHT ||
+				LaraItem->currentAnimState == LS_CRAWL_BACK ||
+				LaraItem->currentAnimState == LS_CROUCH_IDLE ||
+				LaraItem->currentAnimState == LS_CROUCH_ROLL ||
+				LaraItem->currentAnimState == LS_CROUCH_TURN_LEFT ||
+				LaraItem->currentAnimState == LS_CROUCH_TURN_RIGHT)
 				break;
 			return;
 
 		case TRIGGER_TYPES::CLIMB_T:
-			if (LaraItem->currentAnimState == 10 ||
-				LaraItem->currentAnimState == 56 ||
-				LaraItem->currentAnimState == 57 ||
-				LaraItem->currentAnimState == 58 ||
-				LaraItem->currentAnimState == 59 ||
-				LaraItem->currentAnimState == 60 ||
-				LaraItem->currentAnimState == 61 ||
-				LaraItem->currentAnimState == 75)
+			if (LaraItem->currentAnimState == LS_HANG ||
+				LaraItem->currentAnimState == LS_LADDER_IDLE ||
+				LaraItem->currentAnimState == LS_LADDER_UP ||
+				LaraItem->currentAnimState == LS_LADDER_LEFT ||
+				LaraItem->currentAnimState == LS_LADDER_STOP ||
+				LaraItem->currentAnimState == LS_LADDER_RIGHT ||
+				LaraItem->currentAnimState == LS_LADDER_DOWN ||
+				LaraItem->currentAnimState == LS_MONKEYSWING_IDLE)
 				break;
 			return;
 

TR5Main/Objects/TR4/Trap/tr4_sethblade.cpp
@@ -20,6 +20,7 @@ void SethBladeControl(short itemNum)
 	ITEM_INFO* item = &g_Level.Items[itemNum];
 
 	item->itemFlags[0] = 0;
+
 	if (TriggerActive(item))
 	{
 		if (item->currentAnimState == 2)
@@ -33,7 +34,7 @@ void SethBladeControl(short itemNum)
 				item->goalAnimState = 1;
 				item->itemFlags[2] = 0;
 			}
-			else if (!item->itemFlags[2])
+			else if (item->itemFlags[2] == 0)
 			{
 				if (item->triggerFlags > 0)
 				{
@@ -45,7 +46,7 @@ void SethBladeControl(short itemNum)
 		{
 			short frameNumber = item->frameNumber - g_Level.Anims[item->animNumber].frameBase;
 
-			if (item->frameNumber != g_Level.Anims[item->animNumber].frameBase && frameNumber <= 6)
+			if (frameNumber >= 0 && frameNumber <= 6)
 			{
 				item->itemFlags[0] = -1;
 				item->itemFlags[3] = 1000;

TR5Main/Objects/TR4/tr4_objects.cpp
@@ -936,7 +936,7 @@ static void StartTrap(OBJECT_INFO* obj)
 	{
 		obj->initialise = InitialiseSethBlade;
 		obj->control = SethBladeControl;
-		obj->collision = TrapCollision;
+		obj->collision = GenericSphereBoxCollision;
 		obj->saveAnim = true;
 		obj->saveFlags = true;
 	}
@@ -954,7 +954,7 @@ static void StartTrap(OBJECT_INFO* obj)
 	if (obj->loaded)
 	{
 		obj->control = BirdBladeControl;
-		obj->collision = TrapCollision;
+		obj->collision = GenericSphereBoxCollision;
 		obj->saveAnim = true;
 		obj->saveFlags = true;
 	}

TR5Main/Renderer/Render11Helper.cpp
@@ -854,7 +854,9 @@ namespace T5M::Renderer
 
 		world = Matrix::CreateFromYawPitchRoll(TO_RAD(item->pos.yRot), TO_RAD(item->pos.xRot), TO_RAD(item->pos.zRot)) * world;
 
-		RendererObject &moveable = *m_moveableObjects[item->objectNumber];
+		short objNum = item->objectNumber;
+		if (objNum == ID_LARA) objNum = ID_LARA_SKIN;
+		RendererObject &moveable = *m_moveableObjects[objNum];
 
 		for (int i = 0; i < moveable.ObjectMeshes.size(); i++)
 		{

commit dc34a7081ad7a3e92a296484a48ee05fa91c179e

Rollback regression from abb7b0c862c80b3ee10d657a452767d8ce0e511f

TR5Main/Game/collide.cpp
@@ -573,12 +573,17 @@ int ItemPushLara(ITEM_INFO* item, ITEM_INFO* l, COLL_INFO* coll, int spazon, cha
 	BOUNDING_BOX* bounds;
 	short facing;
 
-	dx = l->pos.xPos - item->pos.xPos;		// Get Vector from Baddie to Lara
-	dz = l->pos.zPos - item->pos.zPos;
-	c = phd_cos(item->pos.yRot);					// Get Baddies Rotation
+	// Get item's rotation
+	c = phd_cos(item->pos.yRot); 
 	s = phd_sin(item->pos.yRot);
-	rx = (c * dx - s * dz);            	// Rotate Lara Vector into Baddie Frame
-	rz = (c * dz + s * dx);
+
+	// Get vector from item to Lara
+	dx = LaraItem->pos.xPos - item->pos.xPos; 
+	dz = LaraItem->pos.zPos - item->pos.zPos;
+
+	// Rotate Lara vector into item frame
+	rx = c * dx - s * dz; 
+	rz = c * dz + s * dx;
 
 	if (bigpush & 2)
 		bounds = &GlobalCollisionBounds;
@@ -654,7 +659,16 @@ int ItemPushLara(ITEM_INFO* item, ITEM_INFO* l, COLL_INFO* coll, int spazon, cha
 
 	facing = coll->facing;
 	coll->facing = phd_atan(l->pos.zPos - coll->old.z, l->pos.xPos - coll->old.x);
-	GetCollisionInfo(coll, l->pos.xPos, l->pos.yPos, l->pos.zPos, l->roomNumber, LARA_HITE);
+
+	if (l == LaraItem)
+	{
+		GetCollisionInfo(coll, l->pos.xPos, l->pos.yPos, l->pos.zPos, l->roomNumber, LARA_HITE);
+	}
+	else
+	{
+		GetObjectCollisionInfo(coll, l->pos.xPos, l->pos.yPos, l->pos.zPos, l->roomNumber, LARA_HITE);
+	}
+
 	coll->facing = facing;
 
 	if (coll->collType == CT_NONE)
@@ -663,8 +677,8 @@ int ItemPushLara(ITEM_INFO* item, ITEM_INFO* l, COLL_INFO* coll, int spazon, cha
 		coll->old.y = l->pos.yPos;
 		coll->old.z = l->pos.zPos;
 
-	//	UpdateLaraRoom(l, -10);
-		/*Causes Lara to jump out of the water if she touches an object on the surface. re: "kayak bug"*/
+		// Commented because causes Lara to jump out of the water if she touches an object on the surface. re: "kayak bug"
+		// UpdateLaraRoom(l, -10);
 	}
 	else
 	{
@@ -773,10 +787,12 @@ int TestLaraPosition(OBJECT_COLLISION_BOUNDS* bounds, ITEM_INFO* item, ITEM_INFO
 
 	Vector3 pos = Vector3(x, y, z);
 
-	// HACK (REMOVED FOR NOW): it seems that a minus sign is required here. I don't know why, but it just works (tm) but we must 
-	// do more tests
+	// HACK (REMOVED FOR NOW): it seems that a minus sign may be required here. 
+	// I don't know why, but it just works (tm) but we must do more tests -- Monty
+	// 16.08.21: Changed back to non-minus sign by request of Krys and ChocolateFan -- Lwmte
+
 	Matrix matrix = Matrix::CreateFromYawPitchRoll(
-		TO_RAD(-item->pos.yRot),
+		TO_RAD(item->pos.yRot),
 		TO_RAD(item->pos.xRot),
 		TO_RAD(item->pos.zRot)
 	);
@@ -824,47 +840,49 @@ int Move3DPosTo3DPos(PHD_3DPOS* src, PHD_3DPOS* dest, int velocity, short angAdd
 		if (Lara.waterStatus != LW_UNDERWATER)
 		{
 			angle = mGetAngle(dest->xPos, dest->zPos, src->xPos, src->zPos);
-			//direction = (GetQuadrant(angle) - GetQuadrant(dest->yRot)) & 3;
+			direction = (GetQuadrant(angle) - GetQuadrant(dest->yRot)) & 3;
 			
-			angle = (angle + 0x2000) / 0x4000;
-			angle = (angle - ((unsigned short)(dest->yRot + 0x2000) / 0x4000));
-			angle &= 3;
+			// 16.08.21: code below was deliberately reintroduced by Monty for whatever reasons.
+			// Identified as regression by ChocolateFan and commented.
+
+			// angle = (angle + 0x2000) / 0x4000;
+			// angle = (angle - ((unsigned short)(dest->yRot + 0x2000) / 0x4000));
+			// angle &= 3;
 			
-			switch (angle)
+			switch (direction)
 			{
-			case 0:
-				LaraItem->animNumber = LA_SIDESTEP_LEFT;
-				LaraItem->frameNumber = GF(LA_SIDESTEP_LEFT, 0);
-				LaraItem->goalAnimState = LS_STEP_LEFT;
-				LaraItem->currentAnimState = LS_STEP_LEFT;
-				Lara.gunStatus = LG_HANDS_BUSY;
-				break;
+				case 0:
+					LaraItem->animNumber = LA_SIDESTEP_LEFT;
+					LaraItem->frameNumber = GF(LA_SIDESTEP_LEFT, 0);
+					LaraItem->goalAnimState = LS_STEP_LEFT;
+					LaraItem->currentAnimState = LS_STEP_LEFT;
+					Lara.gunStatus = LG_HANDS_BUSY;
+					break;
 
-			case 1:
-				LaraItem->animNumber = LA_WALK;
-				LaraItem->frameNumber = GF(LA_WALK, 0);
-				LaraItem->goalAnimState = LS_WALK_FORWARD;
-				LaraItem->currentAnimState = LS_WALK_FORWARD;
-				Lara.gunStatus = LG_HANDS_BUSY;
-				break;
+				case 1:
+					LaraItem->animNumber = LA_WALK;
+					LaraItem->frameNumber = GF(LA_WALK, 0);
+					LaraItem->goalAnimState = LS_WALK_FORWARD;
+					LaraItem->currentAnimState = LS_WALK_FORWARD;
+					Lara.gunStatus = LG_HANDS_BUSY;
+					break;
 
-			case 2:
-				LaraItem->animNumber = LA_WALK;
-				LaraItem->frameNumber = GF(LA_SIDESTEP_RIGHT, 0);
-				LaraItem->goalAnimState = LS_STEP_RIGHT;
-				LaraItem->currentAnimState = LS_STEP_RIGHT;
-				Lara.gunStatus = LG_HANDS_BUSY;
-				break;
+				case 2:
+					LaraItem->animNumber = LA_WALK;
+					LaraItem->frameNumber = GF(LA_SIDESTEP_RIGHT, 0);
+					LaraItem->goalAnimState = LS_STEP_RIGHT;
+					LaraItem->currentAnimState = LS_STEP_RIGHT;
+					Lara.gunStatus = LG_HANDS_BUSY;
+					break;
 
-			case 3:
-			default:
-				LaraItem->animNumber = LA_WALK_BACK;
-				LaraItem->frameNumber = GF(LA_WALK_BACK, 0);
-				LaraItem->goalAnimState = LS_WALK_BACK;
-				LaraItem->currentAnimState = LS_WALK_BACK;
-				Lara.gunStatus = LG_HANDS_BUSY;
-				break;
-
+				case 3:
+				default:
+					LaraItem->animNumber = LA_WALK_BACK;
+					LaraItem->frameNumber = GF(LA_WALK_BACK, 0);
+					LaraItem->goalAnimState = LS_WALK_BACK;
+					LaraItem->currentAnimState = LS_WALK_BACK;
+					Lara.gunStatus = LG_HANDS_BUSY;
+					break;
 			}
 		}
 