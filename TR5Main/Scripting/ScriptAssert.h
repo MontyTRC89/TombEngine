1621a00b3ac2ff33dbdd5e4dc8427fac2c8b3707
hispidence <squidshirehimself@gmail.com>
2021-08-21 00:13:44 +0100

    Rename SetErrorMode to SetScriptErrorMode to avoid VS getting confused by some MS function with the same name. Add GetScriptErrorMode.

--- TR5Main/Scripting/ScriptAssert.h
+++ TR5Main/Scripting/ScriptAssert.h
@@ -9,8 +9,10 @@ enum class ERROR_MODE
 	WARN,
 	TERMINATE
 };
+
+void SetScriptErrorMode(ERROR_MODE mode);
+ERROR_MODE GetScriptErrorMode();
 void ScriptWarn(std::string const& msg);
 
 bool ScriptAssert(bool cond, std::string const& msg, std::optional<ERROR_MODE> forceMode = std::nullopt);
 
-void SetErrorMode(ERROR_MODE mode);
