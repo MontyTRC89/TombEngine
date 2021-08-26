6a10fab856abba86b30834bdc313af5c44f66a58
Lwmte <initiator@nm.ru>
2021-08-26 12:37:46 +0300

    Move and rename DoProperDetection to collide.cpp

--- TR5Main/Game/Lara/lara_one_gun.cpp
+++ TR5Main/Game/Lara/lara_one_gun.cpp
@@ -547,7 +547,7 @@ void ControlGrenade(short itemNumber)
 		short sYrot = item->pos.yRot;
 		item->pos.yRot = item->goalAnimState;
 
-		DoProperDetection(itemNumber, oldX, oldY, oldZ, xv, yv, zv);
+		DoObjectDynamics(itemNumber, oldX, oldY, oldZ, xv, yv, zv);
 
 		item->goalAnimState = item->pos.yRot;
 		item->pos.yRot = sYrot;
