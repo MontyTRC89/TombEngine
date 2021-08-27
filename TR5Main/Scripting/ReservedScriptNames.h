df5353d0c2999e71d4808a32256ca079d9f9e33a
hispidence <squidshirehimself@gmail.com>
2021-08-27 18:11:59 +0100

    Add ReservedScriptNames.h. This holds some static const strings to be used as template arguments.

---
+++ TR5Main/Scripting/ReservedScriptNames.h
@@ -0,0 +1,8 @@
+#pragma once
+
+static constexpr char const ScriptReserved_ItemInfo[]			= "ItemInfo";
+static constexpr char const ScriptReserved_MeshInfo[]			= "MeshInfo";
+static constexpr char const ScriptReserved_CameraInfo[]			= "CameraInfo";
+static constexpr char const ScriptReserved_SinkInfo[]			= "SinkInfo";
+static constexpr char const ScriptReserved_SoundSourceInfo[]	= "SoundSourceInfo";
+static constexpr char const ScriptReserved_AIObject[]			= "AIObject";
