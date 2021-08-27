b8f8bc6caae7b25c74f9ed46c1b3ecaf3a064612
hispidence <squidshirehimself@gmail.com>
2021-08-27 18:21:37 +0100

    Replace Get*ByName with GetByName, AddLuaName* with AddName, and RemoveLuaName* with RemoveName. Replace m_*MapName with m_nameMap. Use a std::variant for the types that m_nameMap elements could be. Modify SetNameCallbacks params to be variadic. These do make it look more complex, but also make it more generic, and mean that specific parameters do not need to be repeated (which will make it easier if/when we change said parameters).

--- TR5Main/Scripting/GameLogicScript.h
+++ TR5Main/Scripting/GameLogicScript.h
@@ -47,17 +47,21 @@ struct LuaVariable
 	bool BoolValue;
 };
 
+using VarMapVal = std::variant< short,
+	std::reference_wrapper<MESH_INFO>,
+	std::reference_wrapper<LEVEL_CAMERA_INFO>,
+	std::reference_wrapper<SINK_INFO>,
+	std::reference_wrapper<SOUND_SOURCE_INFO>,
+	std::reference_wrapper<AI_OBJECT>>;
+
 class GameScript : public LuaHandler
 {
 private:
+
 	LuaVariables										m_globals{};
 	LuaVariables										m_locals{};
+	std::unordered_map<std::string, VarMapVal>			m_nameMap{};
 	std::unordered_map<std::string, short>				m_itemsMapName{};
-	std::unordered_map<std::string, MESH_INFO&>			m_meshesMapName{};
-	std::unordered_map<std::string, LEVEL_CAMERA_INFO&>	m_camerasMapName{};
-	std::unordered_map<std::string, SINK_INFO&>			m_sinksMapName{};
-	std::unordered_map<std::string, SOUND_SOURCE_INFO&>	m_soundSourcesMapName{};
-	std::unordered_map<std::string, AI_OBJECT&>			m_aiObjectsMapName{};
 	std::unordered_set<std::string>						m_levelFuncs{};
 	sol::protected_function								m_onStart{};
 	sol::protected_function								m_onLoad{};
@@ -72,23 +76,6 @@ public:
 
 	void								FreeLevelScripts();
 
-	bool								AddLuaNameItem(std::string const & luaName, short itemNumber);
-	bool								RemoveLuaNameItem(std::string const& luaName);
-
-	bool								AddLuaNameMesh(std::string const & luaName, MESH_INFO &);
-	bool								RemoveLuaNameMesh(std::string const& luaName);
-
-	bool								AddLuaNameCamera(std::string const & luaName, LEVEL_CAMERA_INFO &);
-	bool								RemoveLuaNameCamera(std::string const& luaName);
-
-	bool								AddLuaNameSink(std::string const & luaName, SINK_INFO &);
-	bool								RemoveLuaNameSink(std::string const& luaName);
-
-	bool								AddLuaNameSoundSource(std::string const& luaName, SOUND_SOURCE_INFO&);
-	bool								RemoveLuaNameSoundSource(std::string const& luaName);
-
-	bool								AddLuaNameAIObject(std::string const & luaName, AI_OBJECT &);
-	bool								RemoveLuaNameAIObject(std::string const& luaName);
 
 	bool								SetLevelFunc(sol::table tab, std::string const& luaName, sol::object obj);
 	sol::protected_function				GetLevelFunc(sol::table tab, std::string const& luaName);
@@ -99,12 +86,24 @@ public:
 	void								ExecuteFunction(std::string const & name);
 	void								MakeItemInvisible(short id);
 
-	std::unique_ptr<GameScriptItemInfo>			GetItemByName(std::string const & name);
-	std::unique_ptr<GameScriptMeshInfo>			GetMeshByName(std::string const & name);
-	std::unique_ptr<GameScriptCameraInfo>		GetCameraByName(std::string const & name);
-	std::unique_ptr<GameScriptSinkInfo>			GetSinkByName(std::string const & name);
-	std::unique_ptr<GameScriptSoundSourceInfo>	GetSoundSourceByName(std::string const & name);
-	std::unique_ptr<GameScriptAIObject>			GetAIObjectByName(std::string const & name);
+	template <typename R, char const* S>
+	std::unique_ptr<R> GetByName(std::string const& name)
+	{
+		ScriptAssertF(m_nameMap.find(name) != m_nameMap.end(), "{} name not found: {}", S, name);
+		return std::make_unique<R>(std::get<R::IdentifierType>(m_nameMap.at(name)), false);
+	}
+
+	template <typename Value>
+	bool AddName(std::string const& key, Value&& val)
+	{
+		auto p = std::pair<std::string const&, Value>{ key, val };
+		return m_nameMap.insert(p).second;
+	}
+
+	bool RemoveName(std::string const& key)
+	{
+		return m_nameMap.erase(key);
+	}
 
 	// Variables
 	template <typename T>
