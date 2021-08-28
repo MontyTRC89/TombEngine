fb5bc938f61132a6e3f70c99583945ef67ccafd3
MontyTRC89 <montyhammet@hotmail.it>
2021-08-28 19:12:19 +0200

    Fixed ID_SAS_DRAG_BLOKE; Added namespaces to SAS functions;

--- TR5Main/Objects/TR4/Entity/tr4_sas.h
+++ TR5Main/Objects/TR4/Entity/tr4_sas.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <collide.h>
 
-void InitialiseSas(short itemNumber);
-void SasControl(short itemNumber);
-void InitialiseSasDying(short itemNumber);
-void SasDyingControl(short itemNumber);
-void SasDragBlokeCollision(short itemNumber, ITEM_INFO* l, COLL_INFO* c);
-void SasFireGrenade(ITEM_INFO* item, short angle1, short angle2);
+namespace TEN::Entities::TR4
+{
+	void InitialiseSas(short itemNumber);
+	void SasControl(short itemNumber);
+	void InitialiseInjuredSas(short itemNumber);
+	void InjuredSasControl(short itemNumber);
+	void SasDragBlokeCollision(short itemNumber, ITEM_INFO* l, COLL_INFO* c);
+	void SasFireGrenade(ITEM_INFO* item, short angle1, short angle2);
+}
