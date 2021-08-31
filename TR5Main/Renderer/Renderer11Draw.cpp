54857431be35d750512e9df4e36a5c61c8f150d0
Nils <nils.gaitzsch@googlemail.com>
2021-08-31 18:24:32 +0200

    Fixed Crashing Inventory

--- TR5Main/Renderer/Renderer11Draw.cpp
+++ TR5Main/Renderer/Renderer11Draw.cpp
@@ -77,12 +77,14 @@ namespace TEN::Renderer
         projection = Matrix::CreateOrthographic(ScreenWidth, ScreenHeight, -1024.0f, 1024.0f);
 
         OBJECT_INFO *obj = &Objects[objectNum];
-        RendererObject &moveableObj = *m_moveableObjects[objectNum];
+        auto& moveableObj = m_moveableObjects[objectNum];
+        if(!moveableObj)
+            return;
 
         if (obj->animIndex != -1)
         {
             ANIM_FRAME *frame[] = {&g_Level.Frames[g_Level.Anims[obj->animIndex].framePtr]};
-            updateAnimation(NULL, moveableObj, frame, 0, 0, 0xFFFFFFFF);
+            updateAnimation(NULL, *moveableObj, frame, 0, 0, 0xFFFFFFFF);
         }
 
         Vector3 pos = m_viewportToolkit.Unproject(Vector3(x, y, 1), projection, view, Matrix::Identity);
@@ -115,9 +117,9 @@ namespace TEN::Renderer
         m_cbCameraMatrices.updateData(HudCamera, m_context.Get());
         m_context->VSSetConstantBuffers(0, 1, m_cbCameraMatrices.get());
 
-        for (int n = 0; n < moveableObj.ObjectMeshes.size(); n++)
+        for (int n = 0; n < (*moveableObj).ObjectMeshes.size(); n++)
         {
-            RendererMesh *mesh = moveableObj.ObjectMeshes[n];
+            RendererMesh *mesh = (*moveableObj).ObjectMeshes[n];
 
             /*if (GLOBAL_invMode)
             {
@@ -138,9 +140,9 @@ namespace TEN::Renderer
             world = world * translation;
 
             if (obj->animIndex != -1)
-                m_stItem.World = (moveableObj.AnimationTransforms[n] * world);
+                m_stItem.World = ((*moveableObj).AnimationTransforms[n] * world);
             else
-                m_stItem.World = (moveableObj.BindPoseTransforms[n] * world);
+                m_stItem.World = ((*moveableObj).BindPoseTransforms[n] * world);
             m_stItem.AmbientLight = Vector4(0.5f, 0.5f, 0.5f, 1.0f);
             m_cbItem.updateData(m_stItem, m_context.Get());
             m_context->VSSetConstantBuffers(1, 1, m_cbItem.get());
