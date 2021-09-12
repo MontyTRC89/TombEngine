f8cb69dff978b9cb4ad4a788c8fe1c5aabb00b2a
MontyTRC89 <montyhammet@hotmail.it>
2021-09-12 05:35:56 +0200

    Fixed ID_STARGATE

--- TR5Main/Objects/TR4/Trap/tr4_stargate.h
+++ TR5Main/Objects/TR4/Trap/tr4_stargate.h
@@ -1,6 +1,10 @@
 #pragma once
-#include "items.h"
-#include "collide.h"
 
-void StargateControl(short itemNum);
-void StargateCollision(short itemNum, ITEM_INFO* l, COLL_INFO* c);
+struct ITEM_INFO;
+struct COLL_INFO;
+
+namespace TEN::Entities::TR4
+{
+	void StargateControl(short itemNum);
+	void StargateCollision(short itemNum, ITEM_INFO* l, COLL_INFO* c);
+}
