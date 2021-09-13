b6d55c4ddc104d55f0c42fbca85d85839bf7e3ad
Lwmte <initiator@nm.ru>
2021-09-14 01:22:08 +0300

    Use different sprites for left/right foot

--- TR5Main/Game/effects/footprint.cpp
+++ TR5Main/Game/effects/footprint.cpp
@@ -6,58 +6,79 @@
 #include "effects\footprint.h"
 #include "level.h"
 #include "items.h"
+
 namespace TEN {
-	namespace Effects {
-		namespace Footprints {
-			std::deque<FOOTPRINT_STRUCT> footprints = std::deque<FOOTPRINT_STRUCT>();
+namespace Effects {
+namespace Footprints {
 
-			bool CheckFootOnFloor(ITEM_INFO const & item, int mesh, PHD_3DPOS& outFootprintPosition) {
-				int x = item.pos.xPos;
-				int y = item.pos.yPos;
-				int z = item.pos.zPos;
-				short roomNumber = item.roomNumber;
-				FLOOR_INFO* floor = GetFloor(x, y, z, &roomNumber);
-				if (!(floor->Material == GroundMaterial::Ice || floor->Material == GroundMaterial::Sand || floor->Material == GroundMaterial::Mud)) {
-					return false;
-				}
-				int height = GetFloorHeight(floor, x, y, z);
-				int diff;
-				PHD_VECTOR pos;
-				pos.x = pos.z = 0;
-				pos.y = FOOT_HEIGHT_OFFSET;
-				GetLaraJointPosition(&pos, mesh);
-				outFootprintPosition.xPos = pos.x;
-				outFootprintPosition.zPos = pos.z;
-				outFootprintPosition.yPos = height - 1;
-				outFootprintPosition.yRot = item.pos.yRot;
-				return	abs(pos.y - height) < 32;
-			}
-			void updateFootprints()
+	std::deque<FOOTPRINT_STRUCT> footprints = std::deque<FOOTPRINT_STRUCT>();
+
+	bool CheckFootOnFloor(ITEM_INFO const & item, int mesh, PHD_3DPOS& outFootprintPosition) 
+	{
+		int x = item.pos.xPos;
+		int y = item.pos.yPos;
+		int z = item.pos.zPos;
+		short roomNumber = item.roomNumber;
+
+		FLOOR_INFO* floor = GetFloor(x, y, z, &roomNumber);
+		if (!(floor->Material == GroundMaterial::Ice  || 
+			  floor->Material == GroundMaterial::Sand || 
+			  floor->Material == GroundMaterial::Mud)) 
+		{
+			return false;
+		}
+
+		int height = GetFloorHeight(floor, x, y, z);
+		int diff;
+		PHD_VECTOR pos;
+
+		pos.x = pos.z = 0;
+		pos.y = FOOT_HEIGHT_OFFSET;
+
+		GetLaraJointPosition(&pos, mesh);
+
+		outFootprintPosition.xPos = pos.x;
+		outFootprintPosition.zPos = pos.z;
+		outFootprintPosition.yPos = height - 1;
+		outFootprintPosition.yRot = item.pos.yRot;
+
+		return abs(pos.y - height) < 32;
+	}
+
+	void UpdateFootprints()
+	{
+		if (footprints.size() == 0)
+			return;
+
+		int numInvalidFootprints = 0;
+
+		for (auto i = footprints.begin(); i != footprints.end(); i++) 
+		{
+			FOOTPRINT_STRUCT& footprint = *i;
+			footprint.life--;
+
+			if (footprint.life <= 0) 
 			{
-				if (footprints.size() == 0) {
-					return;
-				}
-				int numInvalidFootprints = 0;
-				for (auto i = footprints.begin(); i != footprints.end(); i++) {
-					FOOTPRINT_STRUCT& footprint = *i;
-					footprint.life--;
-					if (footprint.life <= 0) {
-						numInvalidFootprints++;
-						continue;
-					}
-					if (footprint.life > footprint.lifeStartFading) {
-						footprint.opacity = footprint.startOpacity;
-					}
-					else {
-						float opacity = lerp(0, footprint.startOpacity, fmax(0, fmin(1, footprint.life / (float)footprint.lifeStartFading)));
-						footprint.opacity = opacity;
-					}
-				}
-				for (int i = 0; i < numInvalidFootprints; i++) {
-					footprints.pop_back();
-				}
+				numInvalidFootprints++;
+				continue;
 			}
 
+			if (footprint.life > footprint.lifeStartFading) 
+			{
+				footprint.opacity = footprint.startOpacity;
+			}
+			else 
+			{
+				float opacity = lerp(0, footprint.startOpacity, fmax(0, fmin(1, footprint.life / (float)footprint.lifeStartFading)));
+				footprint.opacity = opacity;
+			}
+		}
+
+		for (int i = 0; i < numInvalidFootprints; i++) 
+		{
+			footprints.pop_back();
 		}
 	}
+}
+}
 }
