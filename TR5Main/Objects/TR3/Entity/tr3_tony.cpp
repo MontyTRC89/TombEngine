7f7bf4357d6c8f6fe2d3466eeda59613c71b5f1e
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2021-09-21 17:18:21 +0100

    Update tr3_tony.cpp
    Tony does not burn lara anymore. Temporarily made him 1 bullet kill for testing purposes (line 54)

--- TR5Main/Objects/TR3/Entity/tr3_tony.cpp
+++ TR5Main/Objects/TR3/Entity/tr3_tony.cpp
@@ -51,7 +51,7 @@ enum TONY_STATE
 
 static BOSS_STRUCT BossData;
 #define TONYBOSS_TURN ANGLE(2.0f)
-#define TONYBOSS_HITS 400 //Tony Harder To Kill, was 100 (6 shotgun shots)
+#define TONYBOSS_HITS 1 //Tony Harder To Kill, was 100 (6 shotgun shots)
 #define MAX_TONY_TRIGGER_RANGE 0x4000
 
 static void TriggerTonyEffect(const TONY_FLAME flame)
@@ -427,7 +427,7 @@ void ControlTonyFireBall(short fxNumber)
 			LaraItem->hitStatus = true;
 			KillEffect(fxNumber);
 			LaraItem->hitPoints -= 200;
-			LaraBurn();
+			//LaraBurn();
 			return;
 		}
 	}
