35b6108ba8baae1bd5622f7632222a0ca01dd1bc
Sezz <sezzary@outlook.com>
2021-09-29 18:35:21 +1000

    lara_as_dash(): cleanup; improve input precedence; more intuitive control logic; finetune lean rate values; add IsStandingWeapon() helper function

--- TR5Main/Game/Lara/lara_helpers.h
+++ TR5Main/Game/Lara/lara_helpers.h
@@ -1,5 +1,6 @@
 #pragma once
 
+bool IsStandingWeapon(LARA_WEAPON_TYPE gunType);
 void SetLaraFallState(ITEM_INFO* item);
 short GetLaraSlideDirection(COLL_INFO* coll);
 void SetLaraSlideState(ITEM_INFO* item, COLL_INFO* coll);
