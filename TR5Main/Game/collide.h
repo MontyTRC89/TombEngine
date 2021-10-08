6220efb3dabb738f0f93b954c6ba0ff8735a41c7
Lwmte <initiator@nm.ru>
2021-10-08 12:57:48 +0300

    Implement unconstrained GetCollisionInfo for water surface movement

--- TR5Main/Game/collide.h
+++ TR5Main/Game/collide.h
@@ -140,8 +140,8 @@ bool Move3DPosTo3DPos(PHD_3DPOS* src, PHD_3DPOS* dest, int velocity, short angAd
 bool MoveLaraPosition(PHD_VECTOR* pos, ITEM_INFO* item, ITEM_INFO* l);
 bool TestBoundsCollide(ITEM_INFO* item, ITEM_INFO* l, int radius);
 void CreatureCollision(short itemNum, ITEM_INFO* l, COLL_INFO* coll);
-void GetCollisionInfo(COLL_INFO* coll, ITEM_INFO* item, PHD_VECTOR offset, bool resetRoom = false);
-void GetCollisionInfo(COLL_INFO* coll, ITEM_INFO* item, bool resetRoom = false);
+void GetCollisionInfo(COLL_INFO* coll, ITEM_INFO* item, PHD_VECTOR offset, bool resetRoom = false, bool unconstrained = false);
+void GetCollisionInfo(COLL_INFO* coll, ITEM_INFO* item, bool resetRoom = false, bool unconstrained = false);
 void DoProjectileDynamics(short itemNumber, int x, int y, int z, int xv, int yv, int zv);
 void DoObjectCollision(ITEM_INFO* item, COLL_INFO* coll);
 bool ItemNearLara(PHD_3DPOS* pos, int radius);
