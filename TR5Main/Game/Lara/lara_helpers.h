f01eeeaed18779550569332076438bf93c4a90f3
Sezz <seb.zych@outlook.com>
2021-10-09 14:39:06 +1100

    lara_as_walk(), lara_col_walk(): cleanup; add lean; add helper functions for step up/down dispatch

--- TR5Main/Game/Lara/lara_helpers.h
+++ TR5Main/Game/Lara/lara_helpers.h
@@ -1,5 +1,9 @@
 #pragma once
 
+bool TestLaraStep(COLL_INFO* coll);
+bool TestLaraStepUp(COLL_INFO* coll);
+bool TestLaraStepDown(COLL_INFO* coll);
+void DoLaraStep(ITEM_INFO* item, COLL_INFO* coll);
 bool IsStandingWeapon(LARA_WEAPON_TYPE gunType);
 void SetLaraFallState(ITEM_INFO* item);
 short GetLaraSlideDirection(COLL_INFO* coll);
