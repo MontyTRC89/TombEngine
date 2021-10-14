e3f16b32c1783b64139956aed47887ff9e3010ef
Sezz <sezzary@outlook.com>
2021-10-14 19:58:29 +1100

    lara_col_roll2(), lara_as_dashdive(): cleanup; add helper function to set fall back state; remove magic number

--- TR5Main/Game/Lara/lara_helpers.h
+++ TR5Main/Game/Lara/lara_helpers.h
@@ -6,5 +6,6 @@ bool TestLaraStepDown(ITEM_INFO* item, COLL_INFO* coll);
 void DoLaraStep(ITEM_INFO* item, COLL_INFO* coll);
 bool IsStandingWeapon(LARA_WEAPON_TYPE gunType);
 void SetLaraFallState(ITEM_INFO* item);
+void SetLaraFallBackState(ITEM_INFO* item);
 short GetLaraSlideDirection(COLL_INFO* coll);
 void SetLaraSlideState(ITEM_INFO* item, COLL_INFO* coll);
