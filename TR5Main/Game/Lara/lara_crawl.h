c2dc9394b7377f124f16ceb13c3f48f8189f20f9
Sezz <seb.zych@outlook.com>
2021-10-15 01:37:41 +1100

    Enforce minor consistency

--- TR5Main/Game/Lara/lara_crawl.h
+++ TR5Main/Game/Lara/lara_crawl.h
@@ -1,25 +1,24 @@
 #pragma once
 #include "lara_struct.h"
 
-/*crouch/duck start*/
 void lara_as_duck(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_duck(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_crouch_roll(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_crouch_roll(ITEM_INFO* item, COLL_INFO* coll);
-/*crouch/duck end*/
-/*-*/
-/*crawl start*/
+
 void lara_as_all4s(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_all4s(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_crawl(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_crawl(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_all4turnl(ITEM_INFO* item, COLL_INFO* coll);
+void lara_col_all4turnl(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_all4turnr(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_all4turnlr(ITEM_INFO* item, COLL_INFO* coll);
+void lara_col_all4turnr(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_crawlb(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_crawlb(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_duckl(ITEM_INFO* item, COLL_INFO* coll);
+void lara_col_duckl(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_duckr(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_ducklr(ITEM_INFO* item, COLL_INFO* coll);
-/*crawl end*/
+void lara_col_duckr(ITEM_INFO* item, COLL_INFO* coll);
+
 void lara_col_crawl2hang(ITEM_INFO* item, COLL_INFO* coll);
