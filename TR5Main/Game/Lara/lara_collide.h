3ea8ed95a82a77627916991c2d1e61c9bfd1aa01
Sezz <seb.zych@outlook.com>
2021-10-26 14:42:10 +1100

    Update crawl probes

--- TR5Main/Game/Lara/lara_collide.h
+++ TR5Main/Game/Lara/lara_collide.h
@@ -4,8 +4,8 @@ struct COLL_INFO;
 
 constexpr auto DEFLECT_STRAIGHT_ANGLE = 5.0f;
 constexpr auto DEFLECT_DIAGONAL_ANGLE = 12.0f;
-constexpr auto DEFLECT_STRAIGHT_ANGLE_CRAWL = 4.0f;
-constexpr auto DEFLECT_DIAGONAL_ANGLE_CRAWL = 9.0f;
+constexpr auto DEFLECT_STRAIGHT_ANGLE_CRAWL = 2.0f;
+constexpr auto DEFLECT_DIAGONAL_ANGLE_CRAWL = 5.0f;
 
 int LaraDeflectEdge(ITEM_INFO* item, COLL_INFO* coll);
 void LaraDeflectEdgeJump(ITEM_INFO* item, COLL_INFO* coll);
