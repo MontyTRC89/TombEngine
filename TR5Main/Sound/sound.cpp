c5918d532972fb6f322d0dad1b3c83d86f879023
Lwmte <initiator@nm.ru>
2021-10-29 12:52:32 +0300

    Fix logging

--- TR5Main/Sound/sound.cpp
+++ TR5Main/Sound/sound.cpp
@@ -345,7 +345,7 @@ void PlaySoundTrack(std::string track, SOUNDTRACK_PLAYTYPE mode, QWORD position)
 			snprintf(fullTrackName, sizeof(fullTrackName), TRACKS_PREFIX, name, "wav");
 			if (!std::filesystem::exists(fullTrackName))
 			{
-				TENLog("No any sound files with name '" + track + "' were found", LogLevel::Error);
+				TENLog("No any soundtrack files with name '" + track + "' were found", LogLevel::Error);
 				return;
 			}
 		}
@@ -353,7 +353,7 @@ void PlaySoundTrack(std::string track, SOUNDTRACK_PLAYTYPE mode, QWORD position)
 
 	auto stream = BASS_StreamCreateFile(false, fullTrackName, 0, 0, flags);
 
-	if (Sound_CheckBASSError("Opening soundtrack %s", true, fullTrackName))
+	if (Sound_CheckBASSError("Opening soundtrack '%s'", false, fullTrackName))
 		return;
 
 	float masterVolume = (float)GlobalMusicVolume / 100.0f;
@@ -393,7 +393,7 @@ void PlaySoundTrack(std::string track, SOUNDTRACK_PLAYTYPE mode, QWORD position)
 	if (position && (BASS_ChannelGetLength(stream, BASS_POS_BYTE) > position))
 		BASS_ChannelSetPosition(stream, position, BASS_POS_BYTE);
 
-	if (Sound_CheckBASSError("Playing soundtrack %s", false, fullTrackName))
+	if (Sound_CheckBASSError("Playing soundtrack '%s'", true, fullTrackName))
 		return;
 
 	BASS_Soundtrack[(int)mode].Channel = stream;
