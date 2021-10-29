7d3cd98a29431d7ef0966a36b10b04fc35e25e0a
MontyTRC89 <montyhammet@hotmail.it>
2021-10-29 06:05:30 +0200

    Added savegame schema

---
+++ SavegameSchema/gen.bat
@@ -0,0 +1,3 @@
+flatc.exe --cpp --strict-json --unknown-json --gen-object-api --force-empty --force-empty-vectors --cpp-std c++17 --scoped-enums ten_itemdata.fbs
+flatc.exe --cpp --strict-json --unknown-json --gen-object-api --force-empty --force-empty-vectors --cpp-std c++17 --scoped-enums ten_savegame.fbs
+pause
