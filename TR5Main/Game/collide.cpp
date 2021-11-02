0c1237857c9935224ea49ed4d7b9bc872dd9b464
Lwmte <initiator@nm.ru>
2021-11-03 02:18:41 +0300

    Store bridge number in COLL_RESULT for future use

--- TR5Main/Game/collide.cpp
+++ TR5Main/Game/collide.cpp
@@ -1232,8 +1232,8 @@ COLL_RESULT GetCollisionResult(FLOOR_INFO* floor, int x, int y, int z)
 	// Split, bridge and slope data
 	result.Position.DiagonalStep = floor->FloorIsDiagonalStep();
 	result.Position.SplitAngle = floor->FloorCollision.SplitAngle;
-	result.Position.Bridge = result.BottomBlock->InsideBridge(x, z, result.Position.Floor + 1, false, false) >= 0;
-	result.Position.Slope = !result.Position.Bridge && ((abs(tilts.first)) > 2 || (abs(tilts.second)) > 2);
+	result.Position.Bridge = result.BottomBlock->InsideBridge(x, z, result.Position.Floor + 1, false, false);
+	result.Position.Slope = (result.Position.Bridge < 0) && ((abs(tilts.first)) > 2 || (abs(tilts.second)) > 2);
 
 	// TODO: check if we need to keep here this slope vs. bridge check from legacy GetTiltType
 	if ((y + CLICK(2)) < (floor->FloorHeight(x, z)))
--- TR5Main/Game/collide.h
+++ TR5Main/Game/collide.h
@@ -39,9 +39,9 @@ struct COLL_POSITION
 {
 	int Floor;
 	int Ceiling;
+	int Bridge;
 	float SplitAngle;
 	bool Slope;
-	bool Bridge;
 	bool DiagonalStep;
 
 	bool HasDiagonalSplit() { return SplitAngle == 45.0f * RADIAN || SplitAngle == 135.0f * RADIAN; }
