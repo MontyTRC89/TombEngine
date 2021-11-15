0e2a59232ad54e961ca719ca949f74ea6650597a
Lwmte <initiator@nm.ru>
2021-11-15 20:04:55 +0300

    Update weather.cpp

--- TR5Main/Game/effects/weather.cpp
+++ TR5Main/Game/effects/weather.cpp
@@ -354,7 +354,7 @@ namespace Environment
 
 				auto coll = GetCollisionResult(xPos, yPos, zPos, outsideRoom);
 
-				if (!(coll.Position.Ceiling + WALL_SIZE < yPos || coll.Block->RoomAbove(xPos, yPos, zPos).value_or(NO_ROOM) != NO_ROOM))
+				if (!(coll.Position.Ceiling - WALL_SIZE < yPos || coll.Block->RoomAbove(xPos, yPos, zPos).value_or(NO_ROOM) != NO_ROOM))
 					continue;
 
 				auto part = WeatherParticle();
