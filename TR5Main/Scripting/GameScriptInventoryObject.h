a83b52380d83a823a14d1f2c1ebb1e560ba587df
Lwmte <initiator@nm.ru>
2021-11-16 15:00:17 +0300

    Tier 3

--- TR5Main/Scripting/GameScriptInventoryObject.h
+++ TR5Main/Scripting/GameScriptInventoryObject.h
@@ -4,10 +4,10 @@
 #include "GameScriptRotation.h"
 #include "newinv2.h"
 
-static const std::unordered_map<std::string, rotflags> kRotAxes{
-	{"X", rotflags::INV_ROT_X},
-	{"Y", rotflags::INV_ROT_Y},
-	{"Z", rotflags::INV_ROT_Z}
+static const std::unordered_map<std::string, RotationFlags> kRotAxes{
+	{"X", RotationFlags::INV_ROT_X},
+	{"Y", RotationFlags::INV_ROT_Y},
+	{"Z", RotationFlags::INV_ROT_Z}
 };
 
 static const std::unordered_map<std::string, ItemOptions> kItemActions{
@@ -23,16 +23,16 @@ namespace sol {
 struct GameScriptInventoryObject
 {
 	std::string name{};
-	inv_objects slot{ INV_OBJECT_PISTOLS };
+	InventoryObjectTypes slot{ INV_OBJECT_PISTOLS };
 	short yOffset{ 0 };
 	float scale{ 1.0f };
 	GameScriptRotation rot{};
-	rotflags rotationFlags{ rotflags::INV_ROT_X };
+	RotationFlags rotationFlags{ RotationFlags::INV_ROT_X };
 	int meshBits{ 0 };
 	ItemOptions action{ ItemOptions::OPT_USE };
 
 	GameScriptInventoryObject() = default;
-	GameScriptInventoryObject(std::string const & a_name, ItemEnumPair a_slot, short a_yOffset, float a_scale, GameScriptRotation const & a_rot, rotflags a_rotationFlags, int a_meshBits, ItemOptions a_actions);
+	GameScriptInventoryObject(std::string const & a_name, ItemEnumPair a_slot, short a_yOffset, float a_scale, GameScriptRotation const & a_rot, RotationFlags a_rotationFlags, int a_meshBits, ItemOptions a_actions);
 
 	static void Register(sol::state* lua);
 
