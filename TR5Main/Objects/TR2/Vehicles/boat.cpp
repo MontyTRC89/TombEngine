972980d136404c595f1bea87268ca5d805723205
Lwmte <initiator@nm.ru>
2021-12-14 15:22:58 +0300

    Update boat.cpp

--- TR5Main/Objects/TR2/Vehicles/boat.cpp
+++ TR5Main/Objects/TR2/Vehicles/boat.cpp
@@ -180,7 +180,7 @@ BoatMountType GetSpeedBoatMountType(ITEM_INFO* laraItem, ITEM_INFO* sBoatItem, C
 
 	BoatMountType mountType = BoatMountType::None;
 
-	if (laraInfo->gunStatus != LG_NO_ARMS)
+	if (laraInfo->gunStatus != LG_HANDS_FREE)
 		return mountType;
 
 	if (!TestBoundsCollide(sBoatItem, laraItem, coll->Setup.Radius))
