e00a92e834cff6a57cd47a2fde51321e6beeaf3a
Lwmte <initiator@nm.ru>
2021-12-15 04:23:23 +0300

    Remove unused declarations

--- TR5Main/Objects/Effects/tr5_electricity.h
+++ TR5Main/Objects/Effects/tr5_electricity.h
@@ -1,5 +1,3 @@
 #pragma once
 
-void TriggerElectricityWiresSparks(int x, int z, byte objNum, byte node, int flags);
-void TriggerLaraElectricitySparks(int flame);
 void ElectricityWiresControl(short itemNumber);
