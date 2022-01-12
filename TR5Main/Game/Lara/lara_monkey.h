e0a288b26aa2e5bc3a383298a29623fa3501affb
Sezz <seb.zych@outlook.com>
2022-01-12 19:33:41 +1100

    Refactor monkey forward state

--- TR5Main/Game/Lara/lara_monkey.h
+++ TR5Main/Game/Lara/lara_monkey.h
@@ -10,8 +10,8 @@ struct COLL_INFO;
 
 void lara_as_monkey_idle(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_monkey_idle(ITEM_INFO* item, COLL_INFO* coll);
-void lara_as_monkeyswing(ITEM_INFO* item, COLL_INFO* coll);
-void lara_col_monkeyswing(ITEM_INFO* item, COLL_INFO* coll);
+void lara_as_monkey_forward(ITEM_INFO* item, COLL_INFO* coll);
+void lara_col_monkey_forward(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_monkeyr(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_monkeyr(ITEM_INFO* item, COLL_INFO* coll);
 void lara_as_monkeyl(ITEM_INFO* item, COLL_INFO* coll);
