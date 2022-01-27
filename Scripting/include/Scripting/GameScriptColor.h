0834bf805f092eeb74c53c377c13552d6bb12226
hispidence <squidshirehimself@gmail.com>
2022-01-27 23:34:54 +0000

    Make new colour class, RGBAColor8Byte (the name is a work-in-progress, I promise), and move some GameScriptColor stuff into it.

--- Scripting/include/Scripting/GameScriptColor.h
+++ Scripting/include/Scripting/GameScriptColor.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Specific/RGBAColor8Byte.h"
 
 typedef DWORD D3DCOLOR;
 
@@ -9,12 +10,8 @@ namespace sol {
 
 class GameScriptColor {
 public:
-	byte r{ 0 };
-	byte g{ 0 };
-	byte b{ 0 };
-	byte a{ 255 };
-
 	GameScriptColor(byte r, byte g, byte b);
+	GameScriptColor(RGBAColor8Byte col);
 	GameScriptColor(byte r, byte g, byte b, byte a);
 	GameScriptColor(Vector3 const &);
 	GameScriptColor(Vector4 const &);
@@ -23,17 +20,20 @@ public:
 	operator Vector3() const;
 	operator Vector4() const;
 	operator D3DCOLOR() const;
+	operator RGBAColor8Byte() const;
 
-	byte								GetR() const;
-	void								SetR(byte v);
-	byte								GetG() const;
-	void								SetG(byte v);
-	byte								GetB() const;
-	void								SetB(byte v);
-	byte								GetA() const;
-	void								SetA(byte v);
+	byte GetR() const;
+	void SetR(byte v);
+	byte GetG() const;
+	void SetG(byte v);
+	byte GetB() const;
+	void SetB(byte v);
+	byte GetA() const;
+	void SetA(byte v);
 
 	std::string ToString() const;
 
 	static void Register(sol::state* state);
+private:
+	RGBAColor8Byte m_color;
 };
