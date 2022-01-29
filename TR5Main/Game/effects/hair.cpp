4a5321ec48e4340f9aad9620a7226886b0dbaf3d
hispidence <squidshirehimself@gmail.com>
2022-01-29 13:03:19 +0000

    Change more headers.

--- TR5Main/Game/effects/hair.cpp
+++ TR5Main/Game/effects/hair.cpp
@@ -8,7 +8,7 @@
 #include "Game/items.h"
 #include "Game/Lara/lara.h"
 #include "Renderer/Renderer11.h"
-#include "Scripting/GameFlowScript.h"
+#include "Scripting/ScriptInterfaceFlow.h"
 #include "Specific/setup.h"
 #include "Specific/level.h"
 
