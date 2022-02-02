b6f842990a7a5745f93c71d74ba4c10f0ed9efdf
hispidence <squidshirehimself@gmail.com>
2022-02-02 20:04:14 +0000

    Entity->Entities.

--- TR5Main/Specific/level.cpp
+++ TR5Main/Specific/level.cpp
@@ -20,7 +20,7 @@
 #include "Scripting/ScriptInterfaceFlow.h"
 #include "Scripting/ScriptInterfaceGame.h"
 #include "Scripting/ScriptInterfaceLevel.h"
-#include "Scripting/Entity/ScriptInterfaceEntity.h"
+#include "Scripting/Entities/ScriptInterfaceEntities.h"
 #include "Sound/sound.h"
 #include "Specific/setup.h"
 
