9bc101a6155ae1b18c7f029130f596ed726f3b79
hispidence <squidshirehimself@gmail.com>
2022-02-02 19:49:57 +0000

    Begin to split GameLogicScript. First up is Entity.

--- Scripting/include/Scripting/ScriptInterfaceState.h
+++ Scripting/include/Scripting/ScriptInterfaceState.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "ScriptInterfaceGame.h"
 #include "ScriptInterfaceFlow.h"
+#include "Entity/ScriptInterfaceEntity.h"
 
 class ScriptInterfaceState
 {
 public:
 	static ScriptInterfaceGame* CreateGame();
 	static ScriptInterfaceFlow* CreateFlow();
+	static ScriptInterfaceEntity* CreateEntities();
 	static void ScriptInterfaceState::Init();
 };
 
---
+++ Scripting/src/Entity/Entity.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <unordered_map>
+#include "LuaHandler.h"
+#include "Scripting/Entity/ScriptInterfaceEntity.h"
+#include "GameScriptItemInfo.h"
+#include "Entity/Static/GameScriptMeshInfo.h"
+#include "GameScriptSinkInfo.h"
+#include "GameScriptAIObject.h"
+#include "GameScriptSoundSourceInfo.h"
+#include "GameScriptCameraInfo.h"
+
+class GameEntities : public ScriptInterfaceEntity, public LuaHandler
+{
+
+public:
+	GameEntities::GameEntities(sol::state* lua);
+
+private:
+	std::unordered_map<std::string, VarMapVal>					m_nameMap{};
+	std::unordered_map<std::string, short>	 					m_itemsMapName{};
+
+
+	void AssignLara() override;
+
+	template <typename R, char const* S>
+	std::unique_ptr<R> GetByName(std::string const& name)
+	{
+		ScriptAssertF(m_nameMap.find(name) != m_nameMap.end(), "{} name not found: {}", S, name);
+		return std::make_unique<R>(std::get<R::IdentifierType>(m_nameMap.at(name)), false);
+	}
+
+	bool AddName(std::string const& key, VarMapVal val) override
+	{
+		auto p = std::pair<std::string const&, VarMapVal>{ key, val };
+		return m_nameMap.insert(p).second;
+	}
+
+	bool RemoveName(std::string const& key)
+	{
+		return m_nameMap.erase(key);
+	}
+
+	void FreeEntities() override
+	{
+		m_nameMap.clear();
+	}
+};
+	
+
