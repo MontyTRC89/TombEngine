32fcfcacf614fd84faecaaf39fbc7624dede92bc
hispidence <squidshirehimself@gmail.com>
2022-02-05 20:47:27 +0000

    GameScriptItemInfo->Moveable.

--- Scripting/src/Objects/Moveable/Moveable.cpp
+++ Scripting/src/Objects/Moveable/Moveable.cpp
@@ -1,6 +1,6 @@
 #include "frameworkandsol.h"
 #include "ScriptAssert.h"
-#include "GameScriptItemInfo.h"
+#include "Moveable.h"
 #include "ScriptUtil.h"
 #include "Game/items.h"
 #include "Objects/objectslist.h"
@@ -20,15 +20,15 @@ pickups, and Lara herself.
 @pragma nostrip
 */
 
-constexpr auto LUA_CLASS_NAME{ "ItemInfo" };
+constexpr auto LUA_CLASS_NAME{ "Moveable" };
 
-static auto index_error = index_error_maker(GameScriptItemInfo, LUA_CLASS_NAME);
-static auto newindex_error = newindex_error_maker(GameScriptItemInfo, LUA_CLASS_NAME);
+static auto index_error = index_error_maker(Moveable, LUA_CLASS_NAME);
+static auto newindex_error = newindex_error_maker(Moveable, LUA_CLASS_NAME);
 
-GameScriptItemInfo::GameScriptItemInfo(short num, bool temp) : m_item{ &g_Level.Items[num] }, m_num{ num }, m_initialised{ false }, m_temporary{ temp }
+Moveable::Moveable(short num, bool temp) : m_item{ &g_Level.Items[num] }, m_num{ num }, m_initialised{ false }, m_temporary{ temp }
 {};
 
-GameScriptItemInfo::GameScriptItemInfo(GameScriptItemInfo&& other) noexcept : 
+Moveable::Moveable(Moveable&& other) noexcept : 
 	m_item { std::exchange(other.m_item, nullptr) },
 	m_num{ std::exchange(other.m_num, NO_ITEM) },
 	m_initialised{ std::exchange(other.m_initialised, false) },
@@ -36,7 +36,7 @@ GameScriptItemInfo::GameScriptItemInfo(GameScriptItemInfo&& other) noexcept :
 {};
 
 // todo.. how to check if item is killed outside of script?
-GameScriptItemInfo::~GameScriptItemInfo() {
+Moveable::~Moveable() {
 	// todo.. see if there's a better default state than -1
 	if (m_temporary && (m_num > NO_ITEM))
 	{
@@ -48,26 +48,26 @@ GameScriptItemInfo::~GameScriptItemInfo() {
 
 /*** If you create items with this you NEED to give a position, room, 
 and object number, and then call InitialiseItem before it will work.
-	@function ItemInfo.new
+	@function Moveable.new
 */
 
 /*** Like above, but the returned variable controls the 
 lifetime of the object (it will be destroyed when the variable goes
 out of scope).
-	@function ItemInfo.newTemporary
+	@function Moveable.newTemporary
 */
-template <bool temp> std::unique_ptr<GameScriptItemInfo> CreateEmpty()
+template <bool temp> std::unique_ptr<Moveable> CreateEmpty()
 {
 	short num = CreateItem();	
 	ITEM_INFO * item = &g_Level.Items[num];
-	return std::make_unique<GameScriptItemInfo>(num, temp);
+	return std::make_unique<Moveable>(num, temp);
 }
 
 /*** For more information on each parameter, see the
 associated getters and setters. If you do not know what to set for these,
 most can just be set to zero (see usage). See also the overload which
 takes no arguments.
-	@function ItemInfo.new
+	@function Moveable.new
 	@tparam ObjID object ID
 	@tparam string name Lua name of the item
 	@tparam Position position position in level
@@ -85,9 +85,9 @@ takes no arguments.
 	@tparam int status status of object
 	@tparam bool active is item active or not?
 	@tparam bool hitStatus hit status of object
-	@return reference to new ItemInfo object
+	@return reference to new Moveable object
 	@usage 
-	local item = ItemInfo.new(
+	local item = Moveable.new(
 		ObjID.PISTOLS_ITEM, -- object id
 		"test", -- name
 		Position.new(18907, 0, 21201),
@@ -111,11 +111,11 @@ takes no arguments.
 /*** Like the above, but the returned variable controls the 
 lifetime of the object (it will be destroyed when the variable goes
 out of scope).
-	@function ItemInfo.newTemporary
+	@function Moveable.newTemporary
 	@param see_above same as above function
 */
 
-template <bool temp> static std::unique_ptr<GameScriptItemInfo> Create(
+template <bool temp> static std::unique_ptr<Moveable> Create(
 	GAME_OBJECT_ID objID,
 	std::string name,
 	GameScriptPosition pos,
@@ -136,7 +136,7 @@ template <bool temp> static std::unique_ptr<GameScriptItemInfo> Create(
 )
 {
 	short num = CreateItem();
-	auto ptr = std::make_unique<GameScriptItemInfo>(num, temp);
+	auto ptr = std::make_unique<Moveable>(num, temp);
 
 	ITEM_INFO* item = &g_Level.Items[num];
 	ptr->SetPos(pos);
@@ -162,9 +162,9 @@ template <bool temp> static std::unique_ptr<GameScriptItemInfo> Create(
 	return ptr;
 }
 
-void GameScriptItemInfo::Register(sol::state* state)
+void Moveable::Register(sol::state* state)
 {
-	state->new_usertype<GameScriptItemInfo>(LUA_CLASS_NAME,
+	state->new_usertype<Moveable>(LUA_CLASS_NAME,
 		"new", sol::overload(Create<false>, CreateEmpty<false>),
 		"newTemporary", sol::overload(Create<true>, CreateEmpty<true>),
 		sol::meta_function::index, index_error,
@@ -172,20 +172,20 @@ void GameScriptItemInfo::Register(sol::state* state)
 
 /// Initialise an item.
 //Use this if you called new with no arguments
-// @function ItemInfo.Init
-		"Init", &GameScriptItemInfo::Init,
+// @function Moveable.Init
+		"Init", &Moveable::Init,
 
 /// Enable the item
-// @function ItemInfo:EnableItem
-		"Enable", &GameScriptItemInfo::EnableItem,
+// @function Moveable:EnableItem
+		"Enable", &Moveable::EnableItem,
 
 /// Disable the item
-// @function ItemInfo:DisableItem
-		"Disable", &GameScriptItemInfo::DisableItem,
+// @function Moveable:DisableItem
+		"Disable", &Moveable::DisableItem,
 
 /// (@{ObjID}) object ID 
 // @mem objectID
-		"objectID", sol::property(&GameScriptItemInfo::GetObjectID, &GameScriptItemInfo::SetObjectID),
+		"objectID", sol::property(&Moveable::GetObjectID, &Moveable::SetObjectID),
 
 /*** (int) current animation state
 
@@ -193,15 +193,15 @@ The state number of the animation the object is currently doing.
 This corresponds to "state" number shown in the animation editor of WadTool.
 @mem currentAnimState
 */
-		"currentAnimState", sol::property(&GameScriptItemInfo::GetCurrentAnimState, &GameScriptItemInfo::SetCurrentAnimState),
+		"currentAnimState", sol::property(&Moveable::GetCurrentAnimState, &Moveable::SetCurrentAnimState),
 
 /// (int) State of required animation
 // @mem requiredAnimState
-		"requiredAnimState", sol::property(&GameScriptItemInfo::GetRequiredAnimState, &GameScriptItemInfo::SetRequiredAnimState),
+		"requiredAnimState", sol::property(&Moveable::GetRequiredAnimState, &Moveable::SetRequiredAnimState),
 
 /// (int) State of goal animation
 // @mem goalAnimState
-		"goalAnimState", sol::property(&GameScriptItemInfo::GetGoalAnimState, &GameScriptItemInfo::SetGoalAnimState),
+		"goalAnimState", sol::property(&Moveable::GetGoalAnimState, &Moveable::SetGoalAnimState),
 
 /*** (int) animation number
 
@@ -209,7 +209,7 @@ The index of the animation the object is currently doing.
 This corresponds to the number shown in the item's animation list in WadTool.
 @mem animNumber
 */
-		"animNumber", sol::property(&GameScriptItemInfo::GetAnimNumber, &GameScriptItemInfo::SetAnimNumber),
+		"animNumber", sol::property(&Moveable::GetAnimNumber, &Moveable::SetAnimNumber),
 
 /*** (int) frame number
 
@@ -218,25 +218,25 @@ The number of frames in an animation can be seen under the heading "End frame" i
 the WadTool animation editor.
 @mem frameNumber
 */
-		"frameNumber", sol::property(&GameScriptItemInfo::GetFrameNumber, &GameScriptItemInfo::SetFrameNumber),
+		"frameNumber", sol::property(&Moveable::GetFrameNumber, &Moveable::SetFrameNumber),
 
 /// (int) HP (hit points/health points) of object 
 //@raise an exception if the object is intelligent and an invalid
 //hp value is given
 // @mem HP
-		"HP", sol::property(&GameScriptItemInfo::GetHP, &GameScriptItemInfo::SetHP),
+		"HP", sol::property(&Moveable::GetHP, &Moveable::SetHP),
 
 /// (int) OCB (object code bit) of object
 // @mem OCB
-		"OCB", sol::property(&GameScriptItemInfo::GetOCB, &GameScriptItemInfo::SetOCB),
+		"OCB", sol::property(&Moveable::GetOCB, &Moveable::SetOCB),
 
 /// (table) item flags of object (table of 8 ints)
 // @mem itemFlags 
-		"itemFlags", sol::property(&GameScriptItemInfo::GetItemFlags, &GameScriptItemInfo::SetItemFlags),
+		"itemFlags", sol::property(&Moveable::GetItemFlags, &Moveable::SetItemFlags),
 
 /// (int) AIBits of object. Will be clamped to [0, 255]
 // @mem AIBits
-		"AIBits", sol::property(&GameScriptItemInfo::GetAIBits, &GameScriptItemInfo::SetAIBits),
+		"AIBits", sol::property(&Moveable::GetAIBits, &Moveable::SetAIBits),
 
 /// (int) status of object.
 // possible values:
@@ -245,37 +245,37 @@ the WadTool animation editor.
 // 2 - deactivated
 // 3 - invisible
 // @mem status
-		"status", sol::property(&GameScriptItemInfo::GetStatus, &GameScriptItemInfo::SetStatus),
+		"status", sol::property(&Moveable::GetStatus, &Moveable::SetStatus),
 
 /// (bool) hit status of object
 // @mem hitStatus
-		"hitStatus", sol::property(&GameScriptItemInfo::GetHitStatus, &GameScriptItemInfo::SetHitStatus),
+		"hitStatus", sol::property(&Moveable::GetHitStatus, &Moveable::SetHitStatus),
 
 /// (bool) whether or not the object is active 
 // @mem active
-		"active", sol::property(&GameScriptItemInfo::GetActive, &GameScriptItemInfo::SetActive),
+		"active", sol::property(&Moveable::GetActive, &Moveable::SetActive),
 
 /// (int) room the item is in 
 // @mem room
-		"room", sol::property(&GameScriptItemInfo::GetRoom, &GameScriptItemInfo::SetRoom),
+		"room", sol::property(&Moveable::GetRoom, &Moveable::SetRoom),
 
 /// (@{Position}) position in level
 // @mem pos
-		"pos", sol::property(&GameScriptItemInfo::GetPos, &GameScriptItemInfo::SetPos),
+		"pos", sol::property(&Moveable::GetPos, &Moveable::SetPos),
 
 /// (@{Rotation}) rotation represented as degree angles about X, Y, and Z axes
 // @mem rot
-		"rot", sol::property(&GameScriptItemInfo::GetRot, &GameScriptItemInfo::SetRot),
+		"rot", sol::property(&Moveable::GetRot, &Moveable::SetRot),
 
 /// (string) unique string identifier.
 // e.g. "door\_back\_room" or "cracked\_greek\_statue"
 // @mem name
-		"name", sol::property(&GameScriptItemInfo::GetName, &GameScriptItemInfo::SetName)
+		"name", sol::property(&Moveable::GetName, &Moveable::SetName)
 		);
 }
 
 
-void GameScriptItemInfo::Init()
+void Moveable::Init()
 {
 	bool cond = IsPointInRoom(m_item->pos, m_item->roomNumber);
 	std::string err{ "Position of item \"{}\" does not match its room ID." };
@@ -292,23 +292,23 @@ void GameScriptItemInfo::Init()
 	m_initialised = true;
 }
 
-GAME_OBJECT_ID GameScriptItemInfo::GetObjectID() const
+GAME_OBJECT_ID Moveable::GetObjectID() const
 {
 	return m_item->objectNumber;
 }
 
-void GameScriptItemInfo::SetObjectID(GAME_OBJECT_ID item) 
+void Moveable::SetObjectID(GAME_OBJECT_ID item) 
 {
 	m_item->objectNumber = item;
 }
 
 
-std::string GameScriptItemInfo::GetName() const
+std::string Moveable::GetName() const
 {
 	return m_item->luaName;
 }
 
-void GameScriptItemInfo::SetName(std::string const & id) 
+void Moveable::SetName(std::string const & id) 
 {
 	ScriptAssert(!id.empty(), "Name cannot be blank", ERROR_MODE::TERMINATE);
 
@@ -324,12 +324,12 @@ void GameScriptItemInfo::SetName(std::string const & id)
 	s_callbackSetName(id, m_num);
 }
 
-GameScriptPosition GameScriptItemInfo::GetPos() const
+GameScriptPosition Moveable::GetPos() const
 {
 	return GameScriptPosition(	m_item->pos	);
 }
 
-void GameScriptItemInfo::SetPos(GameScriptPosition const& pos)
+void Moveable::SetPos(GameScriptPosition const& pos)
 {
 	pos.StoreInPHDPos(m_item->pos);
 }
@@ -338,26 +338,26 @@ void GameScriptItemInfo::SetPos(GameScriptPosition const& pos)
 // to a value written in via SetRot - only that the angle measures
 // will be mathematically equal
 // (e.g. 90 degrees = -270 degrees = 450 degrees)
-GameScriptRotation GameScriptItemInfo::GetRot() const
+GameScriptRotation Moveable::GetRot() const
 {
 	return GameScriptRotation(	int(TO_DEGREES(m_item->pos.xRot)) % 360,
 								int(TO_DEGREES(m_item->pos.yRot)) % 360,
 								int(TO_DEGREES(m_item->pos.zRot)) % 360);
 }
 
-void GameScriptItemInfo::SetRot(GameScriptRotation const& rot)
+void Moveable::SetRot(GameScriptRotation const& rot)
 {
 	m_item->pos.xRot = FROM_DEGREES(rot.x);
 	m_item->pos.yRot = FROM_DEGREES(rot.y);
 	m_item->pos.zRot = FROM_DEGREES(rot.z);
 }
 
-short GameScriptItemInfo::GetHP() const
+short Moveable::GetHP() const
 {
 	return(m_item->hitPoints);
 }
 
-void GameScriptItemInfo::SetHP(short hp)
+void Moveable::SetHP(short hp)
 {
 	if(Objects[m_item->objectNumber].intelligent &&
 		(hp < 0 || hp > Objects[m_item->objectNumber].hitPoints))
@@ -378,86 +378,86 @@ void GameScriptItemInfo::SetHP(short hp)
 	m_item->hitPoints = hp;
 }
 
-short GameScriptItemInfo::GetOCB() const
+short Moveable::GetOCB() const
 {
 	return m_item->triggerFlags;
 }
 
-void GameScriptItemInfo::SetOCB(short ocb)
+void Moveable::SetOCB(short ocb)
 {
 	m_item->triggerFlags = ocb;
 }
 
-byte GameScriptItemInfo::GetAIBits() const
+byte Moveable::GetAIBits() const
 {
 	return m_item->aiBits;
 }
 
-void GameScriptItemInfo::SetAIBits(byte bits)
+void Moveable::SetAIBits(byte bits)
 {
 	m_item->aiBits = bits;
 }
 
-sol::as_table_t<std::array<short, 8>> GameScriptItemInfo::GetItemFlags() const
+sol::as_table_t<std::array<short, 8>> Moveable::GetItemFlags() const
 {	
 	std::array<short, 8> ret{};
 	memcpy(ret.data(), m_item->itemFlags, sizeof(m_item->itemFlags));
 	return ret;
 }
 
-void GameScriptItemInfo::SetItemFlags(sol::as_table_t<std::array<short, 8>> const& arr)
+void Moveable::SetItemFlags(sol::as_table_t<std::array<short, 8>> const& arr)
 {	
 	memcpy(m_item->itemFlags, arr.value().data(), sizeof(m_item->itemFlags));
 }
 
-int GameScriptItemInfo::GetCurrentAnimState() const
+int Moveable::GetCurrentAnimState() const
 {
 	return m_item->currentAnimState;
 }
 
-void GameScriptItemInfo::SetCurrentAnimState(int animState)
+void Moveable::SetCurrentAnimState(int animState)
 {
 	m_item->currentAnimState = animState;
 }
 
-int GameScriptItemInfo::GetRequiredAnimState() const
+int Moveable::GetRequiredAnimState() const
 {
 	return m_item->requiredAnimState;
 }
 
-void GameScriptItemInfo::SetRequiredAnimState(short animState)
+void Moveable::SetRequiredAnimState(int animState)
 {
 	m_item->requiredAnimState = animState;
 }
 
-int GameScriptItemInfo::GetGoalAnimState() const
+int Moveable::GetGoalAnimState() const
 {
 	return m_item->goalAnimState;
 }
 
-void GameScriptItemInfo::SetGoalAnimState(int state)
+void Moveable::SetGoalAnimState(int state)
 {
 	m_item->goalAnimState = state;
 }
 
-int GameScriptItemInfo::GetAnimNumber() const
+int Moveable::GetAnimNumber() const
 {
 	return m_item->animNumber - Objects[m_item->objectNumber].animIndex;
 }
 
-void GameScriptItemInfo::SetAnimNumber(int animNumber)
+void Moveable::SetAnimNumber(int animNumber)
 {
 	//TODO fixme: we need bounds checking with an error message once it's in the level file format
 	m_item->animNumber = animNumber +  Objects[m_item->objectNumber].animIndex;
 }
 
-int GameScriptItemInfo::GetFrameNumber() const
+int Moveable::GetFrameNumber() const
 {
 	return m_item->frameNumber - g_Level.Anims[m_item->animNumber].frameBase;
 }
 
 
-void GameScriptItemInfo::SetFrameNumber(int frameNumber)
+void Moveable::SetFrameNumber(int frameNumber)
 {
 	auto const fBase = g_Level.Anims[m_item->animNumber].frameBase;
 	auto const fEnd = g_Level.Anims[m_item->animNumber].frameEnd;
@@ -475,42 +475,42 @@ void GameScriptItemInfo::SetFrameNumber(int frameNumber)
 }
 
 
-short GameScriptItemInfo::GetStatus() const
+short Moveable::GetStatus() const
 {
 	return m_item->status;
 }
 
-void GameScriptItemInfo::SetStatus(short status)
+void Moveable::SetStatus(short status)
 {
 	m_item->status = status;
 }
 
-bool GameScriptItemInfo::GetActive() const
+bool Moveable::GetActive() const
 {
 	return m_item->active;
 }
 
-void GameScriptItemInfo::SetActive(bool active)
+void Moveable::SetActive(bool active)
 {
 	m_item->active = active;
 }
 
-bool GameScriptItemInfo::GetHitStatus() const
+bool Moveable::GetHitStatus() const
 {
 	return m_item->hitStatus;
 }
 
-void GameScriptItemInfo::SetHitStatus(bool hitStatus)
+void Moveable::SetHitStatus(bool hitStatus)
 {
 	m_item->hitStatus = hitStatus;
 }
 
-short GameScriptItemInfo::GetRoom() const
+short Moveable::GetRoom() const
 {
 	return m_item->roomNumber;
 }
 
-void GameScriptItemInfo::SetRoom(short room)
+void Moveable::SetRoom(short room)
 {	
 	const size_t nRooms = g_Level.Rooms.size();
 	if (room < 0 || static_cast<size_t>(room) >= nRooms)
@@ -526,7 +526,7 @@ void GameScriptItemInfo::SetRoom(short room)
 		ItemNewRoom(m_num, room);
 }
 
-void GameScriptItemInfo::EnableItem()
+void Moveable::EnableItem()
 {
 	if (!m_item->active)
 	{
@@ -558,7 +558,7 @@ void GameScriptItemInfo::EnableItem()
 	}
 }
 
-void GameScriptItemInfo::DisableItem()
+void Moveable::DisableItem()
 {
 	if (m_item->active)
 	{
