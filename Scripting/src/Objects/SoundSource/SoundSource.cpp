a24e4854644d8b2f691c5fef6bf66a519d2b9eba
hispidence <squidshirehimself@gmail.com>
2022-02-06 14:45:27 +0000

    SoundSource modernisation.

--- Scripting/src/Objects/SoundSource/SoundSource.cpp
+++ Scripting/src/Objects/SoundSource/SoundSource.cpp
@@ -1,73 +1,72 @@
 #include "frameworkandsol.h"
 #include "ScriptAssert.h"
-#include "GameScriptSoundSourceInfo.h"
+#include "SoundSource.h"
 #include "GameScriptPosition.h"
 #include "ScriptUtil.h"
+#include "ReservedScriptNames.h"
 /***
-Sound source info
+Sound source
 
-@entityclass SoundSourceInfo
+@tenclass SoundSource
 @pragma nostrip
 */
 
-static constexpr auto LUA_CLASS_NAME{ "SoundSourceInfo" };
+static auto index_error = index_error_maker(SoundSource, ScriptReserved_SoundSource);
+static auto newindex_error = newindex_error_maker(SoundSource, ScriptReserved_SoundSource);
 
-static auto index_error = index_error_maker(GameScriptSoundSourceInfo, LUA_CLASS_NAME);
-static auto newindex_error = newindex_error_maker(GameScriptSoundSourceInfo, LUA_CLASS_NAME);
-
-GameScriptSoundSourceInfo::GameScriptSoundSourceInfo(SOUND_SOURCE_INFO & ref, bool temp) : m_soundSource{ref}, m_temporary{ temp }
+SoundSource::SoundSource(SOUND_SOURCE_INFO & ref, bool temp) : m_soundSource{ref}, m_temporary{ temp }
 {};
 
-GameScriptSoundSourceInfo::~GameScriptSoundSourceInfo() {
+SoundSource::~SoundSource() {
 	if (m_temporary)
 	{
 		s_callbackRemoveName(m_soundSource.luaName);
 	}
 }
 
-void GameScriptSoundSourceInfo::Register(sol::state* state)
+void SoundSource::Register(sol::table & parent)
 {
-	state->new_usertype<GameScriptSoundSourceInfo>(LUA_CLASS_NAME,
+	parent.new_usertype<SoundSource>(ScriptReserved_SoundSource,
 		sol::meta_function::index, index_error,
 		sol::meta_function::new_index, newindex_error,
 
 		/// (@{Position}) position in level
 		// @mem pos
-		"pos", sol::property(&GameScriptSoundSourceInfo::GetPos, &GameScriptSoundSourceInfo::SetPos),
+		"pos", sol::property(&SoundSource::GetPos, &SoundSource::SetPos),
 
 		/// (string) unique string identifier.
 		// e.g. "machine\_sound\_1" or "discordant\_humming"
 		// @mem name
-		"name", sol::property(&GameScriptSoundSourceInfo::GetName, &GameScriptSoundSourceInfo::SetName),
+		"name", sol::property(&SoundSource::GetName, &SoundSource::SetName),
 
 		/// (int) sound ID 
 		// @mem soundID
-		"soundID", sol::property(&GameScriptSoundSourceInfo::GetSoundID, &GameScriptSoundSourceInfo::SetSoundID),
+		"soundID", sol::property(&SoundSource::GetSoundID, &SoundSource::SetSoundID),
 
 		/// (int) flags 
 		// @mem flags
-		"flags", sol::property(&GameScriptSoundSourceInfo::GetFlags, &GameScriptSoundSourceInfo::SetFlags)
+		"flags", sol::property(&SoundSource::GetFlags, &SoundSource::SetFlags)
 	);
 }
 
-GameScriptPosition GameScriptSoundSourceInfo::GetPos() const
+GameScriptPosition SoundSource::GetPos() const
 {
 	return GameScriptPosition{ m_soundSource.x, m_soundSource.y, m_soundSource.z };
 }
 
-void GameScriptSoundSourceInfo::SetPos(GameScriptPosition const& pos)
+void SoundSource::SetPos(GameScriptPosition const& pos)
 {
 	m_soundSource.x = pos.x;
 	m_soundSource.y = pos.y;
 	m_soundSource.z = pos.z;
 }
 
-std::string GameScriptSoundSourceInfo::GetName() const
+std::string SoundSource::GetName() const
 {
 	return m_soundSource.luaName;
 }
 
-void GameScriptSoundSourceInfo::SetName(std::string const & id) 
+void SoundSource::SetName(std::string const & id) 
 {
 	ScriptAssert(!id.empty(), "Name cannot be blank", ERROR_MODE::TERMINATE);
 
@@ -83,22 +82,22 @@ void GameScriptSoundSourceInfo::SetName(std::string const & id)
 	s_callbackSetName(id, m_soundSource);
 }
 
-int GameScriptSoundSourceInfo::GetSoundID() const
+int SoundSource::GetSoundID() const
 {
 	return m_soundSource.soundId;
 }
 
-void GameScriptSoundSourceInfo::SetSoundID(int soundID)
+void SoundSource::SetSoundID(int soundID)
 {	
 	m_soundSource.soundId = soundID;
 }
 
-int GameScriptSoundSourceInfo::GetFlags() const
+int SoundSource::GetFlags() const
 {
 	return m_soundSource.flags;
 }
 
-void GameScriptSoundSourceInfo::SetFlags(int flags)
+void SoundSource::SetFlags(int flags)
 {	
 	m_soundSource.flags = flags;
 }
