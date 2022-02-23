bdc60f2f1eb12075a97f1835fd69c0b40a168563
Sezz <seb.zych@outlook.com>
2022-02-23 14:40:08 +1100

    Refactor minecart.cpp; cleanup

--- TR5Main/Objects/TR3/Vehicles/minecart.h
+++ TR5Main/Objects/TR3/Vehicles/minecart.h
@@ -2,6 +2,6 @@
 #include "Game/items.h"
 #include "Game/collision/collide_room.h"
 
-void InitialiseMineCart(short itemNum);
-void MineCartCollision(short itemNum, ITEM_INFO* laraItem, COLL_INFO* coll);
+void InitialiseMineCart(short itemNumber);
+void MineCartCollision(short itemNumber, ITEM_INFO* laraItem, COLL_INFO* coll);
 int MineCartControl(void);
