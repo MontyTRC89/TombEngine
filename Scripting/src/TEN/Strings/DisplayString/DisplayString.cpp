c1ff9b06d0dd8457c111d6ff57160ee10de4cee2
hispidence <squidshirehimself@gmail.com>
2022-03-10 22:48:00 +0000

    Update DisplayStrings part 3. Move TypeOrNil to ScriptUtil.

--- Scripting/src/TEN/Strings/DisplayString/DisplayString.cpp
+++ Scripting/src/TEN/Strings/DisplayString/DisplayString.cpp
@@ -3,6 +3,7 @@
 #include "DisplayString.h"
 #include "ScriptAssert.h"
 #include "ReservedScriptNames.h"
+#include "ScriptUtil.h"
 
 /*** A string appearing on the screen.
 Can be used for subtitles and "2001, somewhere in Egypt"-style messages.
@@ -39,23 +40,6 @@ DisplayString::DisplayString()
 	m_id = reinterpret_cast<DisplayStringIDType>(this);
 }
 
-// Helper type to allow us to more easily specify "give a value of type X or just give nil" parameters.
-// Sol doesn't (at the time of writing) have any mechanisms to do this kind of optional argument without
-// drawbacks, or at least no mechanisms that I could find.
-//
-// sol::optional doesn't distinguish between nil values and values of the wrong type
-// (so we can't provide the user with an error message to tell them they messed up).
-//
-// std::variant works better, providing an error if the user passes in an arg of the wrong type, but
-// the error isn't too helpful and exposes a lot of C++ code which will not help them fix the error.
-//
-// sol::object lets us check that the user has given the right type, but takes valuable type information
-// away from the function's C++ signature, giving us things like void func(sol::object, sol::object, sol::object),
-// even if the function's actual expected parameter types are (for example) float, sol::table, SomeUserType.
-// 
-// This alias is an effort to avoid the above problems.
-template <typename ... Ts> using TypeOrNil = std::variant<Ts..., sol::nil_t, sol::object>;
-
 /*** Create a DisplayString.
 For use in @{Level-specific.ShowString|ShowString} and @{Level-specific.HideString|HideString}.
 @function DisplayString.new
@@ -72,7 +56,7 @@ If true, the str argument will be the key of a translated string specified in
 strings.lua. __Default: false__.
 @return A new DisplayString object.
 */
-std::unique_ptr<DisplayString> CreateString(std::string const & key, int x, int y, ScriptColor col, TypeOrNil<sol::table> flags, TypeOrNil<bool> maybeTranslated)
+static std::unique_ptr<DisplayString> CreateString(std::string const & key, int x, int y, ScriptColor col, TypeOrNil<sol::table> flags, TypeOrNil<bool> maybeTranslated)
 {
 	auto ptr = std::make_unique<DisplayString>();
 	auto id = ptr->GetID();
@@ -111,18 +95,32 @@ DisplayString::~DisplayString()
 void DisplayString::Register(sol::table & parent)
 {
 	parent.new_usertype<DisplayString>(
-		"DisplayString",
-		"new", &CreateString,
+		ScriptReserved_DisplayString,
+		ScriptReserved_new, &CreateString,
 
-		/// (@{Color}) RBG color
-		// @mem col
-		"col", sol::property(&DisplayString::GetCol, &DisplayString::SetCol),
+		/// Get the display string's color
+		// @function GetColor
+		// @treturn Color a copy of the display string's color
+		ScriptReserved_GetColor, &DisplayString::GetCol,
 
-		/// (string) String key to use. If `translated` is true when @{DisplayString.new}
+		/// Set the display string's color 
+		// @function SetColor
+		// @tparam Color color the new color of the display string 
+		ScriptReserved_SetColor, &DisplayString::SetCol,
+
+		/// Get the string key to use. If `translated` is true when @{DisplayString.new}
 		// is called, this will be the string key for the translation that will be displayed.
 		// If false or omitted, this will be the string that's displayed.
-		// @mem key
-		"key", sol::property(&DisplayString::SetKey, &DisplayString::GetKey),
+		// @treturn String a string
+		ScriptReserved_GetKey, &DisplayString::GetKey, 
+
+		/// Set the string key to use. If `translated` is true when @{DisplayString.new}
+		// is called, this will be the string key for the translation that will be displayed.
+		// If false or omitted, this will be the string that's displayed.
+		// @function SetKey
+		// @tparam String string the new key for the display string 
+		ScriptReserved_SetKey, &DisplayString::SetKey, 
+
 
 		/// Set the position of the string.
 		// Screen-space coordinates are expected.
