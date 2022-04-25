38b93ab79226e5008c084034d9d129cb5d369a63
Sezz <seb.zych@outlook.com>
2022-04-25 22:26:46 +1000

    Wraith cleanup

--- TR5Main/Objects/TR4/Entity/tr4_wraith.h
+++ TR5Main/Objects/TR4/Entity/tr4_wraith.h
@@ -1,13 +1,14 @@
 #pragma once
 
+struct Vector3Int;
 struct ITEM_INFO;
 
 namespace TEN::Entities::TR4
 {
 	void InitialiseWraith(short itemNumber);
 	void WraithControl(short itemNumber);
-	void WraithWallsEffect(int x, int y, int z, short yRot, short objectNumber);
-	void DrawWraith(int x, int y, int z, short xVelocity, short yVelocity, short zVelocity, int objectNumber);
+	void WraithWallsEffect(Vector3Int pos, short yRot, short objectNumber);
+	void DrawWraith(Vector3Int pos, Vector3Int velocity, int objectNumber);
 	void KillWraith(ITEM_INFO* item);
 	void WraithExplosionEffect(ITEM_INFO* item, byte r, byte g, byte b, int velocity);
 }
