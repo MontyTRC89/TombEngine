6961758dd6a13bb76079f0c7b4830fe76b6bdfcc
hispidence <squidshirehimself@gmail.com>
2022-04-30 19:03:25 +0100

    Reset globals, game vars and level vars when a level is started or loaded.

--- Scripting/src/LuaHandler.cpp
+++ Scripting/src/LuaHandler.cpp
@@ -3,7 +3,19 @@
 #include "LuaHandler.h"
 
 LuaHandler::LuaHandler(sol::state* lua) : m_lua{ lua }
-{}
+{
+	ResetGlobals();
+}
+
+void LuaHandler::ResetGlobals()
+{
+	sol::table mt = sol::table{ *m_lua, sol::create };
+	m_globals = sol::table{ *m_lua, sol::create };
+	mt.set(sol::meta_function::new_index, m_globals);
+	mt.set(sol::meta_function::index, m_globals);
+
+	m_lua->set(sol::metatable_key, mt);
+}
 
 void LuaHandler::ExecuteScript(std::string const& luaFilename) {
 	auto result = m_lua->safe_script_file(luaFilename, sol::script_pass_on_error);
