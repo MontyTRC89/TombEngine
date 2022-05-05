a1f99371336440ebaca40df7309d5177a4abc9cc
Sezz <seb.zych@outlook.com>
2022-05-05 19:50:52 +1000

    Update animation bools and fix Settings.lua; use correct block in monkey idle state

--- Scripts/Gameflow.lua
+++ Scripts/Gameflow.lua
@@ -11,7 +11,7 @@ local RotationAxis = Flow.RotationAxis
 local ItemAction = Flow.ItemAction
 
 -- Title level
-Flow.SetIntroImagePath("SCREENS\\MAIN.PNG")
+Flow.SetIntroImagePath("Screens\\Main.png")
 Flow.SetTitleScreenImagePath("Screens\\Title.png")
 Flow.SetFarView(210)
 
