cad20974edcdb26b57f8a15278d753b11e17c9ee
Sezz <seb.zych@outlook.com>
2022-05-06 01:44:15 +1000

    Revert "Update animation bools and fix Settings.lua; use correct block in monkey idle state"
    This reverts commit a1f99371336440ebaca40df7309d5177a4abc9cc.

--- TR5Main/Game/Lara/lara_monkey.cpp
+++ TR5Main/Game/Lara/lara_monkey.cpp
@@ -108,15 +108,19 @@ void lara_as_monkey_idle(ItemInfo* item, CollisionInfo* coll)
 		if (TrInput & IN_LEFT)
 		{
 			item->Animation.TargetState = LS_MONKEY_TURN_LEFT;
-			return;
 		}
 		else if (TrInput & IN_RIGHT)
 		{
 			item->Animation.TargetState = LS_MONKEY_TURN_RIGHT;
-			return;
 		}
-
-		item->Animation.TargetState = LS_MONKEY_IDLE;
+		else if (TrInput & IN_ROLL && g_GameFlow->HasMonkeyTurn180())
+		{
+			item->Animation.TargetState = LS_MONKEY_TURN_180;
+		}
+		else
+		{
+			item->Animation.TargetState = LS_MONKEY_IDLE;
+		}
 		return;
 	}
 
