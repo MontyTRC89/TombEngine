d3a6f8ac41ce04365f9c440b2a77eadde8c28e2c
MontyTRC89 <montyhammet@hotmail.it>
2022-05-08 06:52:04 +0200

    All files under the same project; Renamed TR5Main to TombEngine; Cleaned repository;

--- Scripting/frameworkandsol.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "frameworkbase.h"
-#include <sol.hpp>
-
-#define TEN_OPTIONAL_LUA 1
--- Scripting/src/TEN/Inventory/InventoryHandler.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "frameworkandsol.h"
-
-namespace InventoryHandler
-{
-	void Register(sol::state* state, sol::table& parent);
-};
