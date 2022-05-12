952b3981c00186a8a66eec7128e7b9712acc104c
hispidence <squidshirehimself@gmail.com>
2022-05-12 21:42:31 +0100

    Add a macro to stop complaints about [[likely]].

--- TombEngine/framework.h
+++ TombEngine/framework.h
@@ -18,6 +18,13 @@
 
 #define TEN_OPTIONAL_LUA 1
 
+#if __cplusplus >= 202002L
+#define USE_FEATURE_IF_CPP20(x) x
+#else
+#define USE_FEATURE_IF_CPP20(x)
+#endif
+
+
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
