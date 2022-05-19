bb736526fb244f3a141a0f3b1d07a098485d46bf
Sezz <sezzary@outlook.com>
2022-05-19 22:59:57 +1000

    Fix 462: "Binoculars: movement isn't locked properly"; cleanup

--- TombEngine/Game/Lara/lara_helpers.cpp
+++ TombEngine/Game/Lara/lara_helpers.cpp
@@ -326,7 +326,7 @@ void DoLaraTightropeBalanceRegen(ItemInfo* item)
 	}
 }
 
-void DoLaraFallDamage(ItemInfo* item)
+void DealLaraFallDamage(ItemInfo* item)
 {
 	if (item->Animation.VerticalVelocity >= LARA_DAMAGE_VELOCITY)
 	{
