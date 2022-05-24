dbe89a27864c95f1588cf25fa9904604056b5cdd
Sezz <sezzary@outlook.com>
2022-05-24 15:58:42 +1000

    Fill out trex state and anim enums

--- TombEngine/Objects/TR3/Entity/tr3_trex.cpp
+++ TombEngine/Objects/TR3/Entity/tr3_trex.cpp
@@ -11,23 +11,40 @@
 #include "Specific/level.h"
 #include "Specific/setup.h"
 
-#define LARA_TREX_DEATH 4
+#define LARA_ANIM_TREX_DEATH 4
 
-// TODO
 enum TRexState
 {
-
+	TREX_STATE_NONE = 0,
+	TREX_STATE_IDLE = 1,
+	TREX_STATE_WALK_FORWARD = 2,
+	TREX_STATE_RUN_FORWARD = 3,
+	// 4? Perhaps it was used as a hacky way of storing the index for Lara's trex death animation. @Sezz 2022.05.24
+	TREX_STATE_DEATH = 5,
+	TREX_STATE_ROAR = 6,
+	TREX_STATE_ATTACK = 7,
+	TREX_STATE_KILL = 8,
 };
 
-// TODO
 enum TRexAnim
 {
-
+	TREX_ANIM_IDLE = 0,
+	TREX_ANIM_RUN_FORWARD = 1,
+	TREX_ANIM_WALK_FORWARD = 2,
+	TREX_ANIM_IDLE_TO_WALK_FORWARD = 3,
+	TREX_ANIM_WALK_FORWARD_TO_IDLE = 4,
+	TREX_ANIM_ROAR = 5,
+	TREX_ANIM_ATTACK = 6,
+	TREX_ANIM_IDLE_TO_RUN_START = 7,
+	TREX_ANIM_IDLE_TO_RUN_END = 8,
+	TREX_ANIM_RUN_FORWARD_TO_IDLE = 9,
+	TREX_ANIM_DEATH = 10,
+	TREX_ANIM_KILL = 11
 };
 
 void LaraTRexDeath(ItemInfo* tRexItem, ItemInfo* laraItem)
 {
-	tRexItem->Animation.TargetState = 8;
+	tRexItem->Animation.TargetState = TREX_STATE_KILL;
 
 	if (laraItem->RoomNumber != tRexItem->RoomNumber)
 		ItemNewRoom(Lara.ItemNumber, tRexItem->RoomNumber);
@@ -36,7 +53,7 @@ void LaraTRexDeath(ItemInfo* tRexItem, ItemInfo* laraItem)
 	laraItem->Pose.Orientation= Vector3Shrt(0, tRexItem->Pose.Orientation.y, 0);
 	laraItem->Animation.Airborne = false;
 
-	laraItem->Animation.AnimNumber = Objects[ID_LARA_EXTRA_ANIMS].animIndex + LARA_TREX_DEATH;
+	laraItem->Animation.AnimNumber = Objects[ID_LARA_EXTRA_ANIMS].animIndex + LARA_ANIM_TREX_DEATH;
 	laraItem->Animation.FrameNumber = g_Level.Anims[laraItem->Animation.AnimNumber].frameBase;
 	laraItem->Animation.ActiveState = LS_DEATH;
 	laraItem->Animation.TargetState = LS_DEATH;
@@ -64,10 +81,10 @@ void TRexControl(short itemNumber)
 
 	if (item->HitPoints <= 0)
 	{
-		if (item->Animation.ActiveState == 1)
-			item->Animation.TargetState = 5;
+		if (item->Animation.ActiveState == TREX_STATE_IDLE)
+			item->Animation.TargetState = TREX_STATE_DEATH;
 		else
-			item->Animation.TargetState = 1;
+			item->Animation.TargetState = TREX_STATE_IDLE;
 	}
 	else
 	{
@@ -83,7 +100,7 @@ void TRexControl(short itemNumber)
 		angle = CreatureTurn(item, info->MaxTurn);
 
 		if (item->TouchBits)
-			LaraItem->HitPoints -= (item->Animation.ActiveState == 3) ? 10 : 1;
+			LaraItem->HitPoints -= (item->Animation.ActiveState == TREX_STATE_RUN_FORWARD) ? 10 : 1;
 
 		info->Flags = (info->Mood != MoodType::Escape && !AI.ahead && AI.enemyFacing > -FRONT_ARC && AI.enemyFacing < FRONT_ARC);
 
@@ -96,58 +113,59 @@ void TRexControl(short itemNumber)
 
 		switch (item->Animation.ActiveState)
 		{
-		case 1:
+		case TREX_STATE_IDLE:
 			if (item->Animation.RequiredState)
 				item->Animation.TargetState = item->Animation.RequiredState;
 			else if (AI.distance < pow(1500, 2) && AI.bite)
-				item->Animation.TargetState = 7;
+				item->Animation.TargetState = TREX_STATE_ATTACK;
 			else if (info->Mood == MoodType::Bored || info->Flags)
-				item->Animation.TargetState = 2;
+				item->Animation.TargetState = TREX_STATE_WALK_FORWARD;
 			else
-				item->Animation.TargetState = 3;
+				item->Animation.TargetState = TREX_STATE_RUN_FORWARD;
+
 			break;
 
-		case 2:
+		case TREX_STATE_WALK_FORWARD:
 			info->MaxTurn = ANGLE(2.0f);
 
 			if (info->Mood != MoodType::Bored || !info->Flags)
-				item->Animation.TargetState = 1;
+				item->Animation.TargetState = TREX_STATE_IDLE;
 			else if (AI.ahead && GetRandomControl() < 0x200)
 			{
-				item->Animation.RequiredState = 6;
-				item->Animation.TargetState = 1;
+				item->Animation.RequiredState = TREX_STATE_ROAR;
+				item->Animation.TargetState = TREX_STATE_IDLE;
 			}
 
 			break;
 
-		case 3:
+		case TREX_STATE_RUN_FORWARD:
 			info->MaxTurn = ANGLE(4.0f);
 
 			if (AI.distance < pow(SECTOR(5), 2) && AI.bite)
-				item->Animation.TargetState = 1;
+				item->Animation.TargetState = TREX_STATE_IDLE;
 			else if (info->Flags)
-				item->Animation.TargetState = 1;
+				item->Animation.TargetState = TREX_STATE_IDLE;
 			else if (info->Mood != MoodType::Escape && AI.ahead && GetRandomControl() < 0x200)
 			{
-				item->Animation.RequiredState = 6;
-				item->Animation.TargetState = 1;
+				item->Animation.RequiredState = TREX_STATE_ROAR;
+				item->Animation.TargetState = TREX_STATE_IDLE;
 			}
 			else if (info->Mood == MoodType::Bored)
-				item->Animation.TargetState = 1;
+				item->Animation.TargetState = TREX_STATE_IDLE;
 
 			break;
 
-		case 7:
+		case TREX_STATE_ATTACK:
 			if (item->TouchBits & 0x3000)
 			{
-				item->Animation.TargetState = 8;
+				item->Animation.TargetState = TREX_STATE_KILL;
 
 				LaraItem->HitPoints -= 1500;
 				LaraItem->HitStatus = true;
 				LaraTRexDeath(item, LaraItem);
 			}
 
-			item->Animation.RequiredState = 2;
+			item->Animation.RequiredState = TREX_STATE_WALK_FORWARD;
 			break;
 		}
 	}
