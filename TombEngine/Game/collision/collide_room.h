39c9bc82d0ac167f9f490817b1998dfc41953aa0
Sezz <seb.zych@outlook.com>
2022-06-14 22:10:25 +1000

    Update GetCollision() overloads

--- TombEngine/Game/collision/collide_room.h
+++ TombEngine/Game/collision/collide_room.h
@@ -122,7 +122,7 @@ struct CollisionInfo
 
 [[nodiscard]] bool TestItemRoomCollisionAABB(ItemInfo* item);
 
-CollisionResult GetCollision(ItemInfo* item, float vertical = 0.0f);
+CollisionResult GetCollision(ItemInfo* item);
 CollisionResult GetCollision(ItemInfo* item, short angle, float forward, float vertical = 0.0f, float lateral = 0.0f);
 CollisionResult GetCollision(Vector3Int pos, int roomNumber, short angle, float forward, float vertical = 0.0f, float lateral = 0.0f);
 CollisionResult GetCollision(int x, int y, int z, short roomNumber);
