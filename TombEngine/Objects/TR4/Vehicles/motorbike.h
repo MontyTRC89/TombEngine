e30eba578f525c2d17d42c93b538d0a49d8570a6
Sezz <sezzary@outlook.com>
2022-06-16 23:44:16 +1000

    Remove some instances of Lara global usage in motorbike.cpp; cleanup

--- TombEngine/Objects/TR4/Vehicles/motorbike.h
+++ TombEngine/Objects/TR4/Vehicles/motorbike.h
@@ -2,8 +2,6 @@
 #include "Game/items.h"
 #include "Game/collision/collide_room.h"
 
-
-
 void InitialiseMotorbike(short itemNumber);
-void MotorbikeCollision(short itemNumber, ItemInfo* laraitem, CollisionInfo* coll);
-int MotorbikeControl(void);
+void MotorbikeCollision(short itemNumber, ItemInfo* laraItem, CollisionInfo* coll);
+bool MotorbikeControl(ItemInfo* laraItem, CollisionInfo* coll);
