3779ccfa2b7369143bc8f0e4cc2de52d1f750b9d
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-06-17 17:14:28 +0300

    Fix spark height setting up

--- TombEngine/Game/effects/spark.cpp
+++ TombEngine/Game/effects/spark.cpp
@@ -26,9 +26,6 @@ namespace TEN {
 					s.velocity.y += s.gravity;
 					s.velocity *= s.friction;
 					s.pos += s.velocity;
-					float normalizedLife = s.age / s.life;
-					s.height = lerp(s.width / 0.15625, 0, normalizedLife);
-					s.color = DirectX::SimpleMath::Vector4::Lerp(s.sourceColor, s.destinationColor, normalizedLife);
 				}
 			}
 
@@ -51,7 +48,8 @@ namespace TEN {
 				s.life = GenerateFloat(10, 20);
 				s.friction = 0.98f;
 				s.gravity = 1.2f;
-				s.width = 8;
+				s.width = 8.0f;
+				s.height = 48.0f;
 				s.room = room;
 				s.pos = Vector3(pos->x, pos->y, pos->z);
 				Vector3 v = Vector3(vel->x, vel->y, vel->z);
@@ -73,7 +71,8 @@ namespace TEN {
 					s.life = GenerateFloat(10, 20);
 					s.friction = 0.98f;
 					s.gravity = 1.2f;
-					s.width = 8;
+					s.width = 8.0f;
+					s.height = 64.0f;
 					s.room = pos->roomNumber;
 					s.pos = Vector3(pos->x, pos->y, pos->z);
 					float ang = TO_RAD(angle);
@@ -94,16 +93,16 @@ namespace TEN {
 					SparkParticle& s = GetFreeSparkParticle();
 					s = {};
 					s.age = 0;
-					s.life = GenerateFloat(5, 10);
+					s.life = GenerateFloat(8, 15);
 					s.friction = 0.1f;
 					s.gravity = 0.0f;
-					s.height = 48.0f;
-					s.width = length;
+					s.height = length;
+					s.width = 24.0f;
 					s.room = pos->roomNumber;
 					s.pos = Vector3(pos->x, pos->y, pos->z);
 					float ang = TO_RAD(angle.y);
 					float vAng = -TO_RAD(angle.x);
-					Vector3 v = Vector3(sin(ang + GenerateFloat(-PI / 16, PI / 16)), vAng, cos(ang + GenerateFloat(-PI / 16, PI / 16)));
+					Vector3 v = Vector3(sin(ang), vAng + GenerateFloat(-PI / 16, PI / 16), cos(ang));
 					v.Normalize(v);
 					s.velocity = v * GenerateFloat(32, 64);
 					s.sourceColor = Vector4(1, 0.8, 0.5f, 1) * 3;
