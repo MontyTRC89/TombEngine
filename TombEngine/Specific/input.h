cd7c225b9628b052a91337d1fb936b0f400976a9
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-06-21 03:22:46 +0300

    Demagic InputActions enum

--- TombEngine/Specific/input.h
+++ TombEngine/Specific/input.h
@@ -8,8 +8,6 @@ namespace TEN::Input
 	constexpr int MAX_JOYSTICK_KEYS  = 16;
 	constexpr int MAX_JOYSTICK_AXES  = 6;
 	constexpr int MAX_POV_AXES       = 4;
-	constexpr int LAST_KEYBOARD_KEY  = MAX_KEYBOARD_KEYS - 1;
-	constexpr int LAST_JOYSTICK_KEY  = MAX_KEYBOARD_KEYS + MAX_JOYSTICK_KEYS - 1;
 
 	constexpr int MAX_INPUT_SLOTS = MAX_KEYBOARD_KEYS + MAX_JOYSTICK_KEYS + MAX_POV_AXES + MAX_JOYSTICK_AXES * 2;
 
@@ -46,32 +44,34 @@ namespace TEN::Input
 
 	enum InputActions
 	{
-		IN_NONE = 0,				// 0x00000000
-		IN_FORWARD = (1 << 0),		// 0x00000001
-		IN_BACK = (1 << 1),			// 0x00000002
-		IN_LEFT = (1 << 2),			// 0x00000004
-		IN_RIGHT = (1 << 3),		// 0x00000008
-		IN_JUMP = (1 << 4),			// 0x00000010
-		IN_DRAW = (1 << 5), 		// 0x00000020
-		IN_ACTION = (1 << 6), 		// 0x00000040
-		IN_WALK = (1 << 7), 		// 0x00000080
-		IN_OPTION = (1 << 8),		// 0x00000100
-		IN_LOOK = (1 << 9),			// 0x00000200
-		IN_LSTEP = (1 << 10),		// 0x00000400
-		IN_RSTEP = (1 << 11),		// 0x00000800
-		IN_ROLL = (1 << 12),		// 0x00001000
-		IN_PAUSE = (1 << 13),		// 0x00002000
-		IN_FLARE = (1 << 14),		// 0x00004000
-		IN_SELECT = (1 << 15),		// 0x00008000
-		IN_DESELECT = (1 << 16),	// 0x00010000
-		IN_SAVE = (1 << 17), 		// 0x00020000
-		IN_LOAD = (1 << 18),		// 0x00040000
-		IN_CROUCH = (1 << 19),		// 0x00080000
-		IN_SPRINT = (1 << 20),		// 0x00100000
-		IN_LOOKSWITCH = (1 << 21)	// 0x00200000
+		IN_NONE		  = 0,
+		IN_FORWARD	  = (1 << KEY_FORWARD),
+		IN_BACK		  = (1 << KEY_BACK),
+		IN_LEFT		  = (1 << KEY_LEFT),
+		IN_RIGHT	  = (1 << KEY_RIGHT),
+		IN_CROUCH	  = (1 << KEY_CROUCH),
+		IN_SPRINT	  = (1 << KEY_SPRINT),
+		IN_WALK		  = (1 << KEY_WALK),
+		IN_JUMP		  = (1 << KEY_JUMP),
+		IN_ACTION	  = (1 << KEY_ACTION),
+		IN_DRAW		  = (1 << KEY_DRAW),
+		IN_FLARE	  = (1 << KEY_FLARE),
+		IN_LOOK		  = (1 << KEY_LOOK),
+		IN_ROLL		  = (1 << KEY_ROLL),
+		IN_OPTION	  = (1 << KEY_OPTION),
+		IN_PAUSE	  = (1 << KEY_PAUSE),
+		IN_LSTEP	  = (1 << KEY_LSTEP),
+		IN_RSTEP	  = (1 << KEY_RSTEP),
+
+		// Additional input flags without direct key relation
+
+		IN_SAVE		  = (1 << KEY_COUNT + 0),
+		IN_LOAD		  = (1 << KEY_COUNT + 1),
+		IN_DESELECT   = (1 << KEY_COUNT + 2),
+		IN_LOOKSWITCH = (1 << KEY_COUNT + 3)
 	};
 
-	constexpr int IN_OPTIC_CONTROLS = (IN_FORWARD | IN_BACK | IN_LEFT | IN_RIGHT | IN_ACTION | IN_SELECT | IN_CROUCH | IN_SPRINT);
+	constexpr int IN_OPTIC_CONTROLS = (IN_FORWARD | IN_BACK | IN_LEFT | IN_RIGHT | IN_ACTION | IN_CROUCH | IN_SPRINT);
 	constexpr int IN_WAKE = (IN_FORWARD | IN_BACK | IN_LEFT | IN_RIGHT | IN_LSTEP | IN_RSTEP | IN_WALK | IN_JUMP | IN_SPRINT | IN_ROLL | IN_CROUCH | IN_DRAW | IN_FLARE | IN_ACTION);
 	constexpr int IN_DIRECTION = (IN_FORWARD | IN_BACK | IN_LEFT | IN_RIGHT);
 
