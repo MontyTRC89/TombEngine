b32f27ad28821c17d46007ebc97a22828e6f8141
Sezz <sezzary@outlook.com>
2022-06-24 01:49:41 +1000

    Demagic and simplify UPV garbage

--- TombEngine/Objects/TR3/Vehicles/upv_info.h
+++ TombEngine/Objects/TR3/Vehicles/upv_info.h
@@ -5,9 +5,11 @@ namespace TEN::Entities::Vehicles
 	struct UPVInfo
 	{
 		int Velocity = 0;
+
 		int Rot = 0;
 		int XRot = 0;
 		short FanRot = 0;
+
 		unsigned int HarpoonTimer = 0;
 		bool HarpoonLeft = false;
 
