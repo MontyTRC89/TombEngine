84f93608bb0659a9fe96167568dc0fc9544bf80d
Raildex <14938934+Raildex@users.noreply.github.com>
2022-06-29 16:54:48 +0200

    Cubemap shadow (#558)
    * Cubemap shadow WIP
    * Fade out Point Light Shadow
    * Fixed SpotLight Direction and Angle
    
    Added Fadeout at the edges of Spot Light Cone
    
    * Using Light.Out for Spot Light Projection Matrix
    * Added Experimental Blob Shadows
    * Fixed Crash with too many blob shadows.
    
    Now using the 16 nearest spheres
    
    * Remove long unused UNIT_SHADOW
    * Fix merge
    * Check cast shadows flag for dynamic shadows generation
    * Make shadow spheres count customizable, add "shadows: all" setting
    * Fix headers
    * Fix copypaste error
    * Fix shader compiling
    * Format code, make subfunction for blobs rendering
    
    Co-authored-by: Raildex <n@a>
    Co-authored-by: Lwmte <3331699+Lwmte@users.noreply.github.com>

--- TombEngine/Renderer/Renderer11Frame.cpp
+++ TombEngine/Renderer/Renderer11Frame.cpp
@@ -427,7 +427,7 @@ namespace TEN::Renderer
 
 		int numLights = room.Lights.size();
 
-		shadowLight = NULL;
+		shadowLight = nullptr;
 		RendererLight* brightestLight = NULL;
 		float brightest = 0.0f;
 
@@ -455,7 +455,7 @@ namespace TEN::Renderer
 					continue;
 
 				// If Lara, try to collect shadow casting light
-				if (effect->Effect->objectNumber == ID_LARA)
+				if (light->CastShadows && effect->Effect->objectNumber == ID_LARA)
 				{
 					float attenuation = 1.0f - distance / light->Out;
 					float intensity = std::max(0.0f, attenuation * (light->Color.x + light->Color.y + light->Color.z) / 3.0f);
@@ -612,9 +612,10 @@ namespace TEN::Renderer
 					float intensity = std::max(0.0f, attenuation * (light->Color.x + light->Color.y + light->Color.z) / 3.0f);
 
 					light->LocalIntensity = intensity;
+					light->Distance = distance;
 
 					// If Lara, try to collect shadow casting light
-					if (nativeItem->ObjectNumber == ID_LARA && light->Type == LIGHT_TYPE_POINT)
+					if (light->CastShadows && nativeItem->ObjectNumber == ID_LARA && light->Type == LIGHT_TYPE_POINT)
 					{
 						if (intensity >= brightest)
 						{
@@ -622,8 +623,6 @@ namespace TEN::Renderer
 							brightestLight = light;
 						}
 					}
-
-					light->Distance = distance;
 				}
 				else if (light->Type == LIGHT_TYPE_SPOT)
 				{
@@ -645,7 +644,7 @@ namespace TEN::Renderer
 					light->LocalIntensity = intensity;
 
 					// If Lara, try to collect shadow casting light
-					if (nativeItem->ObjectNumber == ID_LARA)
+					if (light->CastShadows && nativeItem->ObjectNumber == ID_LARA)
 					{
 						if (intensity >= brightest)
 						{
