2ac154f8de1579b920101131d4f42b0f47c0a2cc
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-06-30 13:05:37 +0300

    Revert "Fix incorrect scissor test argument pass"
    This reverts commit 723cbe260d0fa750e897d4388759bd3af6db159f.

--- TombEngine/Renderer/Renderer11.cpp
+++ TombEngine/Renderer/Renderer11.cpp
@@ -444,9 +444,9 @@ namespace TEN::Renderer
 	{
 		D3D11_RECT rects;
 		rects.left = s.left;
+		rects.top = ScreenHeight - s.top;
 		rects.right = s.right;
-		rects.bottom = ScreenHeight - s.top;
-		rects.top = ScreenHeight - s.bottom;
+		rects.bottom = ScreenHeight - s.bottom;
 
 		m_context->RSSetScissorRects(1, &rects);
 	}
