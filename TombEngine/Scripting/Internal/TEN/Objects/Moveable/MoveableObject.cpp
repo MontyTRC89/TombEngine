d9c6aff44952accc881975bf9da680d5c3f721d9
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-07-12 12:04:23 +0300

    Use ScriptAssert to generate error message

--- TombEngine/Scripting/Internal/TEN/Objects/Moveable/MoveableObject.cpp
+++ TombEngine/Scripting/Internal/TEN/Objects/Moveable/MoveableObject.cpp
@@ -975,7 +975,7 @@ bool Moveable::MeshExists(int index) const
 {
 	if (index < 0 || index >= Objects[m_item->ObjectNumber].nmeshes)
 	{
-		TENLog(std::string("Mesh index ") + std::to_string(index) + std::string(" does not exist in moveable '") + m_item->LuaName + "'", LogLevel::Error);
+		ScriptAssertF(false, "Mesh index {} does not exist in moveable '{}'", index, m_item->LuaName);
 		return false;
 	}
 
