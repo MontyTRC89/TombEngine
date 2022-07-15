58b7ffe568b1c5c806207beeef74b9f555a26dc9
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-07-15 11:55:27 +0300

    Naming consistency

--- TombEngine/Renderer/Renderer11Compatibility.cpp
+++ TombEngine/Renderer/Renderer11Compatibility.cpp
@@ -222,7 +222,7 @@ namespace TEN::Renderer
 						vertex->Effects = Vector4(room.effects[index].x, room.effects[index].y, room.effects[index].z, 0);
 
 						const unsigned long long primes[]{ 73856093ULL, 19349663ULL, 83492791ULL };
-						vertex->hash = std::hash<float>{}((vertex->Position.x)* primes[0]) ^ (std::hash<float>{}(vertex->Position.y)* primes[1]) ^ std::hash<float>{}(vertex->Position.z) * primes[2];
+						vertex->Hash = std::hash<float>{}((vertex->Position.x)* primes[0]) ^ (std::hash<float>{}(vertex->Position.y)* primes[1]) ^ std::hash<float>{}(vertex->Position.z) * primes[2];
 						vertex->Bone = 0;
 
 						lastVertex++;
@@ -811,7 +811,7 @@ namespace TEN::Renderer
 					vertex.OriginalIndex = v;
 
 					vertex.Effects = Vector4(meshPtr->effects[v].x, meshPtr->effects[v].y, meshPtr->effects[v].z, poly->shineStrength);
-					vertex.hash = std::hash<float>{}(vertex.Position.x) ^ std::hash<float>{}(vertex.Position.y) ^ std::hash<float>{}(vertex.Position.z);
+					vertex.Hash = std::hash<float>{}(vertex.Position.x) ^ std::hash<float>{}(vertex.Position.y) ^ std::hash<float>{}(vertex.Position.z);
 
 					if (obj->Type == 0)
 						moveablesVertices[*lastVertex] = vertex;
