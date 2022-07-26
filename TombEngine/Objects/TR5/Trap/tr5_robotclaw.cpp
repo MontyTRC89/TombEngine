27d65d6343377df5b4919314a4154db3b01b86fc
Stranger1992 <danieljsmith1992@gmail.com>
2022-07-26 19:45:28 +0100

    Updated #includes

--- TombEngine/Objects/TR5/Trap/tr5_robotclaw.cpp
+++ TombEngine/Objects/TR5/Trap/tr5_robotclaw.cpp
@@ -1,5 +1,5 @@
 #include "framework.h"
-#include "tr5_ventilator.h"
+#include "tr5_robotclaw.h"
 #include "Game/animation.h"
 #include "Specific/level.h"
 #include "Game/control/control.h"
@@ -7,8 +7,19 @@
 #include "Game/camera.h"
 #include "Game/effects/effects.h"
 #include "Game/items.h"
+#include "Game/control/box.h"
+#include <Game/collision/collide_room.h>
+
+
 
 void InitialiseRobotClaw(short itemNumber)
 {
 	auto* item = &g_Level.Items[itemNumber];
+}
+
+
+void RobotClawControl(short itemNumber)
+{
+	short roomNumber = item->RoomNumber;
+	GetFloor(item->Pose.Position.x, item->Pose.Position.y, item->Pose.Position.z, &roomNumber);
 }
--- TombEngine/Objects/TR5/Trap/tr5_robotclaw.h
+++ TombEngine/Objects/TR5/Trap/tr5_robotclaw.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void InitialiseRobotClaw(short itemNumber);
+void RobotClawControl(short itemNumber);
