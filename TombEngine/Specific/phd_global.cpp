a248ccec3abe1a2e1d829c53a289cc9de50a4c77
Sezz <sezzary@outlook.com>
2022-07-30 19:08:32 +1000

    Move definitions

--- TombEngine/Specific/phd_global.cpp
+++ TombEngine/Specific/phd_global.cpp
@@ -1,6 +1,134 @@
 #include "framework.h"
 #include "phd_global.h"
 
+#include "Specific/trmath.h"
+
+float Angle::Normalize(float angle)
+{
+	if (angle < -PI || angle > PI)
+		return atan2(sin(angle), cos(angle));
+
+	return angle;
+
+	// Alternative (faster?) method:
+	/*return (angle > 0) ?
+		fmod(angle + PI, PI * 2) - PI :
+		fmod(angle - PI, PI * 2) + PI;*/
+}
+
+bool Angle::Compare(float angle0, float angle1, float epsilon)
+{
+	epsilon = ClampEpsilon(epsilon);
+
+	float difference = ShortestAngularDistance(angle0, angle1);
+	if (abs(difference) <= epsilon)
+		return true;
+
+	return false;
+}
+
+float Angle::ShortestAngularDistance(float angleFrom, float angleTo)
+{
+	return Normalize(angleTo - angleFrom);
+}
+
+float Angle::InterpolateLinear(float angleFrom, float angleTo, float alpha, float epsilon)
+{
+	alpha = ClampAlpha(alpha);
+	epsilon = ClampEpsilon(epsilon);
+
+	if (!Compare(angleFrom, angleTo, epsilon))
+	{
+		float difference = ShortestAngularDistance(angleFrom, angleTo);
+		return (angleFrom + (difference * alpha));
+	}
+
+	return angleTo;
+}
+
+float Angle::InterpolateConstant(float angleFrom, float angleTo, float rate)
+{
+	rate = ClampEpsilon(rate);
+
+	if (!Compare(angleFrom, angleTo, rate))
+	{
+		int sign = copysign(1, ShortestAngularDistance(angleFrom, angleTo));
+		return (angleFrom + (rate * sign));
+	}
+
+	return angleTo;
+}
+
+float Angle::InterpolateConstantEaseOut(float angleFrom, float angleTo, float rate, float alpha, float epsilon)
+{
+	rate = ClampEpsilon(rate);
+	alpha = ClampAlpha(alpha);
+	epsilon = ClampEpsilon(epsilon);
+
+	if (!Compare(angleFrom, angleTo, epsilon))
+	{
+		if (!Compare(angleFrom, angleTo, rate))
+		{
+			int sign = copysign(1, ShortestAngularDistance(angleFrom, angleTo));
+			return (angleFrom + (rate * sign));
+		}
+		else
+		{
+			float difference = ShortestAngularDistance(angleFrom, angleTo);
+			return (angleFrom + (difference * alpha));
+		}
+	}
+
+	return angleTo;
+}
+
+float Angle::OrientBetweenPoints(Vector3 point0, Vector3 point1)
+{
+	auto direction = point1 - point0;
+	return atan2(direction.x, direction.z);
+}
+
+float Angle::DeltaHeading(Vector3 origin, Vector3 target, float heading)
+{
+	float difference = OrientBetweenPoints(origin, target);
+	return ShortestAngularDistance(heading, difference + DegToRad(90.0f));
+}
+
+float Angle::DegToRad(float degrees)
+{
+	return Normalize(degrees * (PI / 180.0f));
+}
+
+float Angle::RadToDeg(float radians)
+{
+	return fmod((Normalize(radians) * (180.0f / PI)) + 360.0f, 360.0f);
+}
+
+float Angle::ShrtToRad(short shortForm)
+{
+	return Normalize(shortForm * ((360.0f / (USHRT_MAX + 1)) * (PI / 180.0f)));
+}
+
+short Angle::DegToShrt(float degrees)
+{
+	return (short)round(degrees * ((USHRT_MAX + 1) / 360.0f));
+}
+
+short Angle::RadToShrt(float radians)
+{
+	return (short)round((radians / (PI / 180.0f)) * ((USHRT_MAX + 1) / 360.0f));
+}
+
+float Angle::ClampAlpha(float value)
+{
+	return ((abs(value) > 1.0f) ? 1.0f : abs(value));
+}
+
+float Angle::ClampEpsilon(float value)
+{
+	return abs(Normalize(value));
+}
+
 BOUNDING_BOX operator+(BOUNDING_BOX const& box, PHD_3DPOS const& vec)
 {
 	BOUNDING_BOX box2 = box;
