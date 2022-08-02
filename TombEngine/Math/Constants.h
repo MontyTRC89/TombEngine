cd43d88f21a8ad1317b34a8d9b81f2e05830effe
Sezz <sezzary@outlook.com>
2022-08-03 01:50:33 +1000

    Experimental constants

--- TombEngine/Math/Constants.h
+++ TombEngine/Math/Constants.h
@@ -8,4 +8,11 @@
 	constexpr inline auto PI_DIV_4	= PI / 4;
 	constexpr inline auto RADIAN	= PI / 180;
 	constexpr inline auto SQRT_2	= 1.41421356237309504880168872420969807856967187537694L;
+
+	constexpr inline auto BLOCK_UNIT	 = 1024;
+	constexpr inline auto HLF_BLOCK_UNIT = BLOCK_UNIT / 2;
+	constexpr inline auto QTR_BLOCK_UNIT = BLOCK_UNIT / 4;
+	constexpr inline auto BLOCK			 = [](auto x) { return int(BLOCK_UNIT * x); };
+	constexpr inline auto HLF_BLOCK		 = [](auto x) { return int(HLF_BLOCK_UNIT * x); };
+	constexpr inline auto QTR_BLOCK		 = [](auto x) { return int(QTR_BLOCK_UNIT * x); };
 //}
