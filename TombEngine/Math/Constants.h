f6b502a9e753b1e9c2a0c171e128012088c525bc
Sezz <sezzary@outlook.com>
2022-08-06 00:36:35 +1000

    Update Constatns.h

--- TombEngine/Math/Constants.h
+++ TombEngine/Math/Constants.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <climits>
 
 //namespace TEN::Math
 //{
+	// Standard constants
 	constexpr inline auto PI		= 3.14159265358979323846264338327950288419716939937510L;
 	constexpr inline auto PI_MULT_2 = PI * 2;
 	constexpr inline auto PI_DIV_2	= PI / 2;
@@ -9,9 +11,13 @@
 	constexpr inline auto RADIAN	= PI / 180;
 	constexpr inline auto SQRT_2	= 1.41421356237309504880168872420969807856967187537694L;
 
-	constexpr inline auto BLOCK_UNIT	 = 1024;
-	constexpr inline auto QTR_BLOCK_UNIT = BLOCK_UNIT / 4;
+	constexpr inline auto SQUARE = [](auto x) { return int(x * x); };
+
+	// TEN constants
+	constexpr inline auto BLOCK_UNIT = 1024;
+	constexpr inline auto NO_HEIGHT	 = INT_MIN + UCHAR_MAX;
+	constexpr inline auto MAX_HEIGHT = INT_MIN + 1;
 
 	constexpr inline auto BLOCK		= [](auto x) { return int(BLOCK_UNIT * x); };
-	constexpr inline auto QTR_BLOCK	= [](auto x) { return int(QTR_BLOCK_UNIT * x); };
+	constexpr inline auto QTR_BLOCK	= [](auto x) { return int((BLOCK_UNIT / 4) * x); };
 //}
