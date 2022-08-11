9686f80b293213358fbc3ff69840df0d1729984e
hispidence <squidshirehimself@gmail.com>
2022-08-11 22:49:31 +0100

    Add documentation for EventSequence and update autogenerated docs.

--- Scripts/EventSequence.lua
+++ Scripts/EventSequence.lua
@@ -1,3 +1,47 @@
+-----
+--- Event sequence - a chain of functions to call at specified times, modeled after TRNG's organizers.
+-- Example usage:
+--	local EventSequence = require("EventSequence")
+--
+--	-- These will be called by the sequence
+--	LevelFuncs.HealLara = function()
+--		Lara:SetHP(Lara:GetHP()+10)
+--	end
+--	
+--	local nSpawned = 0
+--	LevelFuncs.SpawnBaddy = function(baddy, name, pos)
+--		local myBaddy = TEN.Objects.Moveable(baddy, name..nSpawned, pos, nil, 0)
+--		myBaddy:Enable()
+--		nSpawned = nSpawned + 1
+--	end
+--
+--	-- This function triggers the sequence
+--	LevelFuncs.TriggerSequence = function(obj) 
+--		local posSteve = TEN.Objects.GetMoveableByName("stevePosNullmesh"):GetPosition()
+--		local posChris = TEN.Objects.GetMoveableByName("chrisPosNullmesh"):GetPosition()
+--		local mySeq = EventSequence.Create("my_seq",
+--			false, -- does not loop
+--			{seconds = true, deciseconds = true}, -- timer format, see Timer for details
+--			6, -- seconds until call the function specified in next arg 
+--			"HealLara", -- first function to call. If we don't need to pass any arguments, we can just give the func name as a string
+--			2.1, -- seconds until the next function, after the previous one has been called
+--			{"SpawnBaddy", TEN.Objects.ObjID.BADDY1, "steve", posSteve}, -- if we DO want to pass arguments to the function to be called, we give a table with the name of the function ("SpawnBaddy" in this case) followed by the args to pass to it
+--			0.5,
+--			{"SpawnBaddy", TEN.Objects.ObjID.SAS_CAIRO, "chris", posChris},
+--			1,
+--			"HealLara")
+--
+--		-- event sequences are inactive to begin with and so need to be started
+--		mySeq:Start()
+--	end
+--
+--	-- EventSequence runs on Timer, so this call is required
+--	LevelFuncs.OnControlPhase = function(dt)
+--		Timer.UpdateAll(dt)
+--	end
+--
+-- @luautil EventSequence
+
 local Timer = require("Timer")
 
 local EventSequence
@@ -18,11 +62,20 @@ LevelFuncs.__TEN_eventSequence_callNext = function(sequenceName, nextTimerName, 
 		theTimer:SetRemainingTime(theTimer:GetTotalTime())
 		theTimer:Start()
 		thisES.currentTimer = 1
+	else
+		thisES.currentTimer = 1
 	end
 end
 
 EventSequence = {
-	Create = function(name, loop, showString, ...)
+	--- Create (but do not start) a new event sequence.
+	--
+	-- @string name A label to give the sequence; used to retrieve the timer later as well as internally by TEN.
+	-- @bool loop if true, the sequence will start again from its first timer once its final function has been called
+	-- @tparam ?table|bool timerFormat same as in Timer. This is mainly for debugging. __This will not work properly if another sequence or timer is showing a countdown.__
+	-- @param[opt] ... a variable number of pairs of arguments - a time in seconds, followed by the function (must be defined in the LevelFuncs table) to call once the time has elapsed, followed by another duration in seconds, another function name, etc. You can specify a function either by its name as a string, or by a table with the function name as the first member, followed by its arguments (see above example).
+	-- @return The inactive sequence.
+	Create = function(name, loop, timerFormat, ...)
 		local obj = {}
 		local mt = {}
 		mt.__index = EventSequence
@@ -69,7 +122,7 @@ EventSequence = {
 			local thisTimer = Timer.Create(timerName,
 						tfa[i], -- time
 						false,
-						showString,
+						timerFormat,
 						"__TEN_eventSequence_callNext",
 						name,
 						nextTimerName,
@@ -83,6 +136,9 @@ EventSequence = {
 		return obj
 	end;
 
+	--- Get an event sequence by its name.
+	-- @string name The label that was given to the sequence when it was created
+	-- @return The sequence
 	Get = function(name)
 		if LevelVars.__TEN_eventSequence.sequences[name] then
 			local obj = {}
@@ -93,21 +149,44 @@ EventSequence = {
 			return obj
 		end
 		return nil
-	end,
+	end;
 
-	Pause = function(t)
+	--- Pause or unpause the sequence. If showing the remaining time on-screen, its color will be set to yellow (paused) or white (unpaused).
+	-- @function mySequence:SetPaused
+	-- @bool p if true, the sequence will be paused; if false, it will be unpaused 
+	SetPaused = function(t, p)
 		local thisES = LevelVars.__TEN_eventSequence.sequences[t.name]
-		Timer.Get(thisES.timers[thisES.currentTimer]):Pause()
-	end,
+		Timer.Get(thisES.timers[thisES.currentTimer]):SetPaused(p)
+	end;
 
-	Stop = function(t)
+	--- Get whether or not the sequence is paused
+	-- @function mySequence:IsPaused
+	-- @return true if the timer is paused, false if otherwise
+	IsPaused = function(t)
 		local thisES = LevelVars.__TEN_eventSequence.sequences[t.name]
-		Timer.Get(thisES.timers[thisES.currentTimer]):Stop()
-	end,
-
+		return Timer.Get(thisES.timers[thisES.currentTimer]):IsPaused()
+	end;
+	
+	--- Begin or unpause a sequence. If showing the remaining time on-screen, its color will be set to white.
+	-- @function mySequence:Start
 	Start = function(t)
 		local thisES = LevelVars.__TEN_eventSequence.sequences[t.name]
 		Timer.Get(thisES.timers[thisES.currentTimer]):Start()
+	end;
+
+	--- Stop the sequence.
+	--@function mySequence:Stop
+	Stop = function(t)
+		local thisES = LevelVars.__TEN_eventSequence.sequences[t.name]
+		Timer.Get(thisES.timers[thisES.currentTimer]):Stop()
+	end;
+
+	--- Get whether or not the sequence is active
+	-- @function mySequence:IsActive
+	-- @return true if the sequence is active, false if otherwise
+	IsActive = function(t)
+		local thisES = LevelVars.__TEN_eventSequence.sequences[t.name]
+		return Timer.Get(thisES.timers[thisES.currentTimer]):IsActive()
 	end;
 }
 
