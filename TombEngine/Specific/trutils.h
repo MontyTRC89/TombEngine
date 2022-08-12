e3b5703f0e5894f9ce21a6734df560e4c051b196
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-08-12 17:27:52 +0300

    Simplify creating wchar strings, fix intro screen, add warning if no room geometry is present

--- TombEngine/Specific/trutils.h
+++ TombEngine/Specific/trutils.h
@@ -4,7 +4,8 @@
 namespace TEN::Utils
 {
 	std::string ToLower(std::string source);
-	std::string FromWchar(wchar_t* source); 
+	std::string FromWchar(const wchar_t* source);
+	std::wstring FromChar(const char* source);
 	std::vector<std::string> SplitString(const std::string& source);
 
 	std::vector<unsigned short> GetProductOrFileVersion(bool productVersion);
