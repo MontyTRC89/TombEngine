bbf1dfbe4fe208ac3706ac1d982bcaabeb6516d5
Sezz <sezzary@outlook.com>
2022-08-19 14:41:12 +1000

    Use float math constants instead of double

--- TombEngine/Math/Constants.h
+++ TombEngine/Math/Constants.h
@@ -4,12 +4,12 @@
 //namespace TEN::Math
 //{
 	// Math constants
-	constexpr inline auto PI	   = 3.14159265358979323846264338327950288419716939937510L;
+	constexpr inline auto PI	   = 3.14159265358979323846264338327950288419716939937510f;
 	constexpr inline auto PI_MUL_2 = PI * 2;
 	constexpr inline auto PI_DIV_2 = PI / 2;
 	constexpr inline auto PI_DIV_4 = PI / 4;
 	constexpr inline auto RADIAN   = PI / 180;
-	constexpr inline auto SQRT_2   = 1.41421356237309504880168872420969807856967187537694L;
+	constexpr inline auto SQRT_2   = 1.41421356237309504880168872420969807856967187537694f;
 
 	constexpr inline auto SQUARE = [](auto x) { return (x * x); };
 	constexpr inline auto CUBE	 = [](auto x) { return (x * x * x); };
