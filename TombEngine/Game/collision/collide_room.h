3b1aee4962f79635fe8d3d7cdeb10218d12544a5
Sezz <sezzary@outlook.com>
2022-08-20 20:57:47 +1000

    Revamp other surface alignment function

--- TombEngine/Game/collision/collide_room.h
+++ TombEngine/Game/collision/collide_room.h
@@ -152,7 +152,7 @@ void SnapItemToLedge(ItemInfo* item, CollisionInfo* coll, float offsetMultiplier
 void SnapItemToLedge(ItemInfo* item, CollisionInfo* coll, short angle, float offsetMultiplier = 0.0f);
 void SnapItemToGrid(ItemInfo* item, CollisionInfo* coll);
 
-void CalculateItemRotationToSurface(ItemInfo* item, float zRadiusDivisor = 1.0f, float maxAngle = 45.0f, short xOffset = 0, short zOffset = 0);
+void AlignEntityToSurface(ItemInfo* item, Vector2 radius, float tiltConstraintAngle = 45.0f, Vector3Shrt orientOffset = Vector3Shrt::Zero);
 
 short GetSurfaceAspectAngle(Vector2 tilt);
 short GetSurfaceSteepnessAngle(Vector2 tilt);
