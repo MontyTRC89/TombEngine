e1199299bf4d69e6c4a405059376b0523029737a
Sezz <seb.zych@outlook.com>
2022-09-06 23:29:07 +1000

    Tabulation

--- TombEngine/Math/Geometry.h
+++ TombEngine/Math/Geometry.h
@@ -7,11 +7,11 @@ class EulerAngles;
 //namespace TEN::Math::Geometry
 //{
 	Vector3i TranslatePoint(const Vector3i& point, short headingAngle, float forward, float down = 0.0f, float right = 0.0f);
-	Vector3	   TranslatePoint(const Vector3& point, short headingAngle, float forward, float down = 0.0f, float right = 0.0f);
+	Vector3	 TranslatePoint(const Vector3& point, short headingAngle, float forward, float down = 0.0f, float right = 0.0f);
 	Vector3i TranslatePoint(const Vector3i& point, const EulerAngles& orient, float distance);
-	Vector3	   TranslatePoint(const Vector3& point, const EulerAngles& orient, float distance);
+	Vector3	 TranslatePoint(const Vector3& point, const EulerAngles& orient, float distance);
 	Vector3i TranslatePoint(const Vector3i& point, const Vector3& direction, float distance);
-	Vector3	   TranslatePoint(const Vector3& point, const Vector3& direction, float distance);
+	Vector3	 TranslatePoint(const Vector3& point, const Vector3& direction, float distance);
 
 	short GetShortestAngularDistance(short angleFrom, short angleTo);
 	short GetSurfaceSteepnessAngle(Vector2 tilt);
