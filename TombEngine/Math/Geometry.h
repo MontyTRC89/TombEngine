a5aac75faeac7d80e8e0dd3e5e0ac44c38afdfd5
Sezz <seb.zych@outlook.com>
2022-09-11 19:01:12 +1000

    Update comment

--- TombEngine/Math/Geometry.h
+++ TombEngine/Math/Geometry.h
@@ -6,6 +6,9 @@ struct Vector3i;
 
 namespace TEN::Math::Geometry
 {
+	// Since Y is assumed as the vertical axis, only the Y Euler component needs to be considered and
+	// 2D vector operations can be done in the XZ plane. Maybe revise geometry functions to each take an "up" vector argument someday.
+
 	Vector3i TranslatePoint(const Vector3i& point, short headingAngle, float forward, float down = 0.0f, float right = 0.0f);
 	Vector3	 TranslatePoint(const Vector3& point, short headingAngle, float forward, float down = 0.0f, float right = 0.0f);
 	Vector3i TranslatePoint(const Vector3i& point, const EulerAngles& orient, float distance);
@@ -17,9 +20,6 @@ namespace TEN::Math::Geometry
 	short GetSurfaceSteepnessAngle(Vector2 tilt);
 	short GetSurfaceAspectAngle(Vector2 tilt);
 	EulerAngles GetOrientTowardPoint(const Vector3& origin, const Vector3& target);
-
-	// Since Y is assumed as the vertical axis, only the Y Euler component needs to be considered and
-	// 2D vector operations can be done in the XZ plane. Maybe revise these to each take an up vector argument someday.
 
 	bool IsPointInFront(const PoseData& pose, const Vector3& target);
 	bool IsPointInFront(const Vector3& origin, const Vector3& target, const EulerAngles& orient);
