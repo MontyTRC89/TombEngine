3f27dc5a90baed6791be5569360a9d3d32335fb1
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-09-15 13:39:40 +0300

    Refactor animcommand parser

--- TombEngine/Game/Lara/lara.h
+++ TombEngine/Game/Lara/lara.h
@@ -95,3 +95,4 @@ void LaraWaterSurface(ItemInfo* item, CollisionInfo* coll);
 void LaraUnderwater(ItemInfo* item, CollisionInfo* coll);
 void LaraCheat(ItemInfo* item, CollisionInfo* coll);
 void AnimateLara(ItemInfo* item);
+void PerformAnimCommands(ItemInfo* item, bool frameBased);
