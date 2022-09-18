79bb1100a6cc688193f2bb4fe76bc6ce8ba358b2
Sezz <seb.zych@outlook.com>
2022-09-18 13:12:38 +1000

    Use Vector3i constructor

--- TombEngine/Objects/TR2/Entity/tr2_sword_guardian.cpp
+++ TombEngine/Objects/TR2/Entity/tr2_sword_guardian.cpp
@@ -28,11 +28,11 @@ namespace TEN::Entities::Creatures::TR2
 
 	void SwordGuardianFly(ItemInfo* item)
 	{
-		Vector3i pos;
-		pos.x = (GetRandomControl() * 256 / 32768) + item->Pose.Position.x - 128;
-		pos.y = (GetRandomControl() * 256 / 32768) + item->Pose.Position.y - 256;
-		pos.z = (GetRandomControl() * 256 / 32768) + item->Pose.Position.z - 128;
-
+		auto pos = Vector3i(
+			(GetRandomControl() * 256 / 32768) + item->Pose.Position.x - 128,
+			(GetRandomControl() * 256 / 32768) + item->Pose.Position.y - 256,
+			(GetRandomControl() * 256 / 32768) + item->Pose.Position.z - 128
+		);
 		TriggerGunSmoke(pos.x, pos.y, pos.z, 1, 1, 1, 1, LaraWeaponType::GrenadeLauncher, 32);
 		SoundEffect(SFX_TR2_WARRIOR_HOVER, &item->Pose);
 	}
@@ -67,8 +67,8 @@ namespace TEN::Entities::Creatures::TR2
 		}
 		else
 		{
-			creature->LOT.Step = STEP_SIZE;
-			creature->LOT.Drop = -STEP_SIZE;
+			creature->LOT.Step = CLICK(1);
+			creature->LOT.Drop = -CLICK(1);
 			creature->LOT.Fly = NO_FLYING;
 			creature->LOT.Zone = ZoneType::Basic;
 
@@ -79,9 +79,9 @@ namespace TEN::Entities::Creatures::TR2
 			{
 				if (AI.zoneNumber != AI.enemyZone)
 				{
-					creature->LOT.Step = WALL_SIZE * 20;
-					creature->LOT.Drop = -WALL_SIZE * 20;
-					creature->LOT.Fly = STEP_SIZE / 4;
+					creature->LOT.Step = SECTOR(20);
+					creature->LOT.Drop = -SECTOR(20);
+					creature->LOT.Fly = CLICK(0.25f);
 					creature->LOT.Zone = ZoneType::Flyer;
 					CreatureAIInfo(item, &AI);
 				}
