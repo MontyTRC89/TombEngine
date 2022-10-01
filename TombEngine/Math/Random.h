5ad9b18c6ca4d434b32e408a1937fdf35902eb7e
Sezz <seb.zych@outlook.com>
2022-10-01 21:56:46 +1000

    Simplify

--- TombEngine/Math/Random.h
+++ TombEngine/Math/Random.h
@@ -1,11 +1,10 @@
 #pragma once
-#include <limits>
 
 namespace TEN::Math::Random
 {
-	int32_t GenerateInt(int32_t low = 0, int32_t high = std::numeric_limits<int16_t>::max());
+	int32_t GenerateInt(int32_t low = 0, int32_t high = SHRT_MAX);
 	float	GenerateFloat(float low = 0.0f, float high = 1.0f);
-	short	GenerateAngle(short low = std::numeric_limits<int16_t>::min(), short high = std::numeric_limits<int16_t>::max());
+	short	GenerateAngle(short low = SHRT_MIN, short high = SHRT_MAX);
 
 	bool TestProbability(float probability);
 }
