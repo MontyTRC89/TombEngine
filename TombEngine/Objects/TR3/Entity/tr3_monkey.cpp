5be41f663abc7a60961aace6d4140772ef3a0ef6
Leif Melles <75839472+goldlife2904@users.noreply.github.com>
2022-10-09 19:20:33 +0200

    Fix Monkey Crash when Monkey is triggered - review change

--- TombEngine/Objects/TR3/Entity/tr3_monkey.cpp
+++ TombEngine/Objects/TR3/Entity/tr3_monkey.cpp
@@ -168,8 +168,11 @@ namespace TEN::Entities::Creatures::TR3
 			AI_INFO AI;
 			CreatureAIInfo(item, &AI);
 
-			if (!creature->HurtByLara && creature->Enemy != nullptr && creature->Enemy->IsLara())
-				creature->Enemy = nullptr;
+			if (creature->Enemy != nullptr)
+			{
+				if (!creature->HurtByLara && creature->Enemy->IsLara())
+					creature->Enemy = nullptr;
+			}
 
 			AI_INFO laraAI;
 			if (creature->Enemy != nullptr && creature->Enemy->IsLara())
