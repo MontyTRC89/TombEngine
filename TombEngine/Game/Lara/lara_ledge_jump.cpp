db52598c4fc58c90bdcbc048f079e8bea9d341ef
Leif Melles <75839472+goldlife2904@users.noreply.github.com>
2022-10-12 22:56:46 +0200

    Update lara_ledge_jump.cpp
    Revised Code

--- TombEngine/Game/Lara/lara_ledge_jump.cpp
+++ TombEngine/Game/Lara/lara_ledge_jump.cpp
@@ -1,7 +1,6 @@
 #include "framework.h"
 #include "lara_ledge_jump.h"
 #include "Game/Lara/lara.h"
-#include "Game/Lara/lara_helpers.h"
 
 void lara_ledge_jump_up(ItemInfo* item, CollisionInfo* coll)
 {
