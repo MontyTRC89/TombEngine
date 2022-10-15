202dcdb6eb18fed62ee51c0f18246dc86045c885
Sezz <sezzary@outlook.com>
2022-10-15 13:18:38 +1100

    Add operators to GameVector

--- TombEngine/Math/Containers/GameVector.cpp
+++ TombEngine/Math/Containers/GameVector.cpp
@@ -5,7 +5,7 @@
 
 //namespace TEN::Math
 //{
-	const GameVector GameVector::Zero = GameVector(0, 0, 0, 0, 0);
+	const GameVector GameVector::Zero = GameVector(0, 0, 0, 0);
 
 	GameVector::GameVector()
 	{
@@ -26,15 +26,6 @@
 		this->RoomNumber = roomNumber;
 	}
 	
-	GameVector::GameVector(const Vector3i& pos, short roomNumber, short boxNumber)
-	{
-		this->x = pos.x;
-		this->y = pos.y;
-		this->z = pos.z;
-		this->RoomNumber = roomNumber;
-		this->BoxNumber = boxNumber;
-	}
-
 	GameVector::GameVector(int xPos, int yPos, int zPos)
 	{
 		this->x = xPos;
@@ -50,15 +41,6 @@
 		this->RoomNumber = roomNumber;
 	}
 
-	GameVector::GameVector(int xPos, int yPos, int zPos, short roomNumber, short boxNumber)
-	{
-		this->x = xPos;
-		this->y = yPos;
-		this->z = zPos;
-		this->RoomNumber = roomNumber;
-		this->BoxNumber = boxNumber;
-	}
-
 	Vector3 GameVector::ToVector3() const
 	{
 		return Vector3(x, y, z);
@@ -67,5 +49,88 @@
 	Vector3i GameVector::ToVector3i() const
 	{
 		return Vector3i(x, y, z);
+	}
+
+	bool GameVector::operator ==(const GameVector& vector) const
+	{
+		return ((x == vector.x) && (y == vector.y) && (z == vector.z));
+	}
+
+	bool GameVector::operator !=(const GameVector& vector) const
+	{
+		return ((x != vector.x) || (y != vector.y) || (z != vector.z));
+	}
+
+	GameVector& GameVector::operator =(const GameVector& vector)
+	{
+		this->x = vector.x;
+		this->y = vector.y;
+		this->z = vector.z;
+		return *this;
+	}
+
+	GameVector& GameVector::operator +=(const GameVector& vector)
+	{
+		this->x += vector.x;
+		this->y += vector.y;
+		this->z += vector.z;
+		return *this;
+	}
+
+	GameVector& GameVector::operator -=(const GameVector& vector)
+	{
+		this->x -= vector.x;
+		this->y -= vector.y;
+		this->z -= vector.z;
+		return *this;
+	}
+
+	GameVector& GameVector::operator *=(const GameVector& vector)
+	{
+		this->x *= vector.x;
+		this->y *= vector.y;
+		this->z *= vector.z;
+		return *this;
+	}
+
+	GameVector& GameVector::operator *=(float scale)
+	{
+		this->x *= scale;
+		this->y *= scale;
+		this->z *= scale;
+		return *this;
+	}
+
+	GameVector& GameVector::operator /=(float scale)
+	{
+		this->x /= scale;
+		this->y /= scale;
+		this->z /= scale;
+		return *this;
+	}
+
+	GameVector GameVector::operator +(const GameVector& vector) const
+	{
+		return GameVector(x + vector.x, y + vector.y, z + vector.z);
+	}
+
+	GameVector GameVector::operator -(const GameVector& vector) const
+	{
+		return GameVector(x - vector.x, y - vector.y, z - vector.z);
+	}
+
+	GameVector GameVector::operator *(const GameVector& vector) const
+	{
+		return GameVector(x * vector.x, y * vector.y, z * vector.z);
+	}
+
+	GameVector GameVector::operator *(float scale) const
+	{
+		return GameVector((int)round(x * scale), (int)round(y * scale), (int)round(z * scale));
+	}
+
+	GameVector GameVector::operator /(float scale) const
+	{
+		return GameVector((int)round(x / scale), (int)round(y / scale), (int)round(z / scale));
 	}
 //}
--- TombEngine/Math/Containers/GameVector.h
+++ TombEngine/Math/Containers/GameVector.h
@@ -21,13 +21,26 @@ class Vector3i;
 		GameVector();
 		GameVector(const Vector3i& pos);
 		GameVector(const Vector3i& pos, short roomNumber);
-		GameVector(const Vector3i& pos, short roomNumber, short boxNumber);
 		GameVector(int xPos, int yPos, int zPos);
 		GameVector(int xPos, int yPos, int zPos, short roomNumber);
-		GameVector(int xPos, int yPos, int zPos, short roomNumber, short boxNumber);
 
 		// Converters
 		Vector3	 ToVector3() const;
 		Vector3i ToVector3i() const;
+
+		// Operators
+		bool		operator ==(const GameVector& vector) const;
+		bool		operator !=(const GameVector& vector) const;
+		GameVector& operator =(const GameVector& vector);
+		GameVector& operator +=(const GameVector& vector);
+		GameVector& operator -=(const GameVector& vector);
+		GameVector& operator *=(const GameVector& vector);
+		GameVector& operator *=(float scale);
+		GameVector& operator /=(float scale);
+		GameVector	operator +(const GameVector& vector) const;
+		GameVector	operator -(const GameVector& vector) const;
+		GameVector	operator *(const GameVector& vector) const;
+		GameVector	operator *(float scale) const;
+		GameVector	operator /(float scale) const;
 	};
 //}
