7d5f854ef99c88cd64560505db09837aa72e14e3
Sezz <sezzary@outlook.com>
2022-10-24 16:54:01 +1100

    Add Lerp() method to EulerAngles

--- TombEngine/Math/Containers/EulerAngles.h
+++ TombEngine/Math/Containers/EulerAngles.h
@@ -20,6 +20,13 @@
 		EulerAngles(const Quaternion& quat);
 		EulerAngles(const Matrix& rotMatrix);
 
+		// Inquirers
+		bool EulerAngles::Compare(const EulerAngles& eulers0, const EulerAngles& eulers1, short epsilon = 2);
+
+		// Utilities
+		void		Lerp(const EulerAngles& eulersTo, float alpha = 1.0f, short epsilon = 2);
+		EulerAngles Lerp(const EulerAngles& eulersFrom, const EulerAngles& eulersTo, float alpha = 1.0f, short epsilon = 2) const;
+
 		// Converters
 		Vector3	   ToDirection() const;
 		Quaternion ToQuaternion() const;
@@ -39,5 +46,11 @@
 		EulerAngles	 operator *(const EulerAngles& eulers) const;
 		EulerAngles	 operator *(float scale) const;
 		EulerAngles	 operator /(float scale) const;
+	
+	private:
+		// Temporary. Will be integrated into eventual Angle class.
+		float ClampAlpha(float alpha) const;
+		bool Compare(short angle0, short angle1, short epsilon = 2) const;
+		short InterpolateLinear(short angleFrom, short angleTo, float alpha = 1.0f, short epsilon = 2) const;
 	};
 //}
