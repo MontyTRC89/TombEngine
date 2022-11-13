ad719fdf301507122f9c0b79ebf4ccbd057f65eb
Anatoly <3331699+Lwmte@users.noreply.github.com>
2022-11-13 02:46:12 +0200

    Simple Lua callback refactor (#829)

--- TombEngine/Game/items.h
+++ TombEngine/Game/items.h
@@ -75,10 +75,19 @@ struct EntityModelData
 	std::vector<BoneMutator> Mutator = {};
 };
 
+struct EntityCallbackData
+{
+	std::string OnKilled;
+	std::string OnHit;
+	std::string OnObjectCollided;
+	std::string OnRoomCollided;
+};
+
 //todo we need to find good "default states" for a lot of these - squidshire 25/05/2022
 struct ItemInfo
 {
 	GAME_OBJECT_ID ObjectNumber;
+	std::string Name;
 
 	int Status;	// ItemStatus enum.
 	bool Active;
@@ -89,7 +98,9 @@ struct ItemInfo
 
 	ITEM_DATA Data;
 	EntityAnimationData Animation;
+	EntityCallbackData Callbacks;
 	EntityModelData Model;
+	
 	Pose StartPose;
 	Pose Pose;
 	ROOM_VECTOR Location;
@@ -117,13 +128,6 @@ struct ItemInfo
 	uint8_t AIBits; // AIObjectType enum.
 	short AfterDeath;
 	short CarriedItem;
-
-	// Lua
-	std::string LuaName;
-	std::string LuaCallbackOnKilledName;
-	std::string LuaCallbackOnHitName;
-	std::string LuaCallbackOnCollidedWithObjectName;
-	std::string LuaCallbackOnCollidedWithRoomName;
 
 	bool TestOcb(short ocbFlags);
 	void RemoveOcb(short ocbFlags);
--- TombEngine/Scripting/Internal/TEN/Objects/Moveable/MoveableObject.cpp
+++ TombEngine/Scripting/Internal/TEN/Objects/Moveable/MoveableObject.cpp
@@ -447,7 +447,7 @@ void Moveable::Init()
 {
 	bool cond = IsPointInRoom(m_item->Pose.Position, m_item->RoomNumber);
 	std::string err{ "Position of item \"{}\" does not match its room ID." };
-	if (!ScriptAssertF(cond, err, m_item->LuaName))
+	if (!ScriptAssertF(cond, err, m_item->Name))
 	{
 		ScriptWarn("Resetting to the center of the room.");
 		auto center = GetRoomCenter(m_item->RoomNumber);
@@ -482,7 +482,7 @@ void SetLevelFuncCallback(TypeOrNil<LevelFunc> const & cb, std::string const & c
 	}
 	else
 	{
-		ScriptAssert(false, "Tried giving " + mov.m_item->LuaName
+		ScriptAssert(false, "Tried giving " + mov.m_item->Name
 			+ " a non-LevelFunc object as an arg to "
 			+ callerName);
 	}
@@ -491,27 +491,27 @@ void SetLevelFuncCallback(TypeOrNil<LevelFunc> const & cb, std::string const & c
 
 void Moveable::SetOnHit(TypeOrNil<LevelFunc> const & cb)
 {
-	SetLevelFuncCallback(cb, ScriptReserved_SetOnHit, *this, m_item->LuaCallbackOnHitName);
+	SetLevelFuncCallback(cb, ScriptReserved_SetOnHit, *this, m_item->Callbacks.OnHit);
 }
 
 void Moveable::SetOnKilled(TypeOrNil<LevelFunc> const & cb)
 {
-	SetLevelFuncCallback(cb, ScriptReserved_SetOnKilled, *this, m_item->LuaCallbackOnKilledName);
+	SetLevelFuncCallback(cb, ScriptReserved_SetOnKilled, *this, m_item->Callbacks.OnKilled);
 }
 
 void Moveable::SetOnCollidedWithObject(TypeOrNil<LevelFunc> const & cb)
 {
-	SetLevelFuncCallback(cb, ScriptReserved_SetOnCollidedWithObject, *this, m_item->LuaCallbackOnCollidedWithObjectName);
+	SetLevelFuncCallback(cb, ScriptReserved_SetOnCollidedWithObject, *this, m_item->Callbacks.OnObjectCollided);
 }
 
 void Moveable::SetOnCollidedWithRoom(TypeOrNil<LevelFunc> const & cb)
 {
-	SetLevelFuncCallback(cb, ScriptReserved_SetOnCollidedWithRoom, *this, m_item->LuaCallbackOnCollidedWithRoomName);
+	SetLevelFuncCallback(cb, ScriptReserved_SetOnCollidedWithRoom, *this, m_item->Callbacks.OnRoomCollided);
 }
 
 std::string Moveable::GetName() const
 {
-	return m_item->LuaName;
+	return m_item->Name;
 }
 
 bool Moveable::SetName(std::string const & id) 
@@ -524,11 +524,11 @@ bool Moveable::SetName(std::string const & id)
 	if (s_callbackSetName(id, m_num))
 	{
 		// remove the old name if we have one
-		if (id != m_item->LuaName)
+		if (id != m_item->Name)
 		{
-			if(!m_item->LuaName.empty())
-				s_callbackRemoveName(m_item->LuaName);
-			m_item->LuaName = id;
+			if(!m_item->Name.empty())
+				s_callbackRemoveName(m_item->Name);
+			m_item->Name = id;
 		}
 	}
 	else
@@ -980,7 +980,7 @@ void Moveable::Destroy()
 	if (m_num > NO_ITEM) 
 	{
 		dynamic_cast<ObjectsHandler*>(g_GameScriptEntities)->RemoveMoveableFromMap(m_item, this);
-		s_callbackRemoveName(m_item->LuaName);
+		s_callbackRemoveName(m_item->Name);
 		KillItem(m_num);
 	}
 
@@ -991,7 +991,7 @@ bool Moveable::MeshExists(int index) const
 {
 	if (index < 0 || index >= Objects[m_item->ObjectNumber].nmeshes)
 	{
-		ScriptAssertF(false, "Mesh index {} does not exist in moveable '{}'", index, m_item->LuaName);
+		ScriptAssertF(false, "Mesh index {} does not exist in moveable '{}'", index, m_item->Name);
 		return false;
 	}
 
