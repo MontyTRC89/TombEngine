1d90bbc6f69b5779c04e4f04b13fecb5b3e4e38b
Sezz <sezzary@outlook.com>
2022-12-12 13:55:45 +1100

    Organise

--- TombEngine/Objects/TR5/Entity/tr5_autoguns.h
+++ TombEngine/Objects/TR5/Entity/tr5_autoguns.h
@@ -1,7 +1,10 @@
 #pragma once
 
+class Vector3i;
+
 namespace TEN::Entities::Creatures::TR5
 {
 	void InitialiseAutoGuns(short itemNumber);
 	void AutoGunsControl(short itemNumber);
+	void TriggerAutoGunSmoke(const Vector3i& pos, char shade);
 }
