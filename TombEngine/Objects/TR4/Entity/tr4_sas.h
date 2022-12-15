1ac996ec2f19e676cebce2ffa894643ba8c609bf
Sezz <sezzary@outlook.com>
2022-12-15 23:36:39 +1100

    Tidy up SAS

--- TombEngine/Objects/TR4/Entity/tr4_sas.h
+++ TombEngine/Objects/TR4/Entity/tr4_sas.h
@@ -1,12 +1,16 @@
 #pragma once
-#include "Game/collision/collide_room.h"
+
+struct CollisionInfo;
+struct ItemInfo;
 
 namespace TEN::Entities::TR4
 {
 	void InitialiseSas(short itemNumber);
-	void SasControl(short itemNumber);
 	void InitialiseInjuredSas(short itemNumber);
+
+	void SasControl(short itemNumber);
 	void InjuredSasControl(short itemNumber);
+
 	void SasDragBlokeCollision(short itemNumber, ItemInfo* laraItem, CollisionInfo* coll);
-	void SasFireGrenade(ItemInfo* item, short angle1, short angle2);
+	void SasFireGrenade(ItemInfo& item, short angle1, short angle2);
 }
