52691a5e87b6e5d85d95f43145c9c76ebbaceb1d
Sezz <sezzary@outlook.com>
2022-12-21 21:46:46 +1100

    Revert changes for PR

--- TombEngine/Objects/TR5/Object/tr5_pushableblock.h
+++ TombEngine/Objects/TR5/Object/tr5_pushableblock.h
@@ -4,35 +4,33 @@ class Vector3i;
 struct CollisionInfo;
 struct ItemInfo;
 
-namespace TEN::Entities::Generic
-{
-	struct PushableInfo;
+// TODO: After wrapping into namespace, uncomment these two lines.
+//struct PushableInfo;
 
-	PushableInfo& GetPushableInfo(ItemInfo& item);
-	void InitialisePushableBlock(short itemNumber);
+//PushableInfo* GetPushableInfo(ItemInfo* item);
+void InitialisePushableBlock(short itemNumber);
 
-	void ClearMovableBlockSplitters(const Vector3i& pos, short roomNumber);
-	void PushableBlockControl(short itemNumber);
-	void PushableBlockCollision(short itemNumber, ItemInfo* laraItem, CollisionInfo* coll);
-	bool TestBlockMovable(ItemInfo* item, int blockHeight);
-	bool TestBlockPush(ItemInfo* item, int blockHeight, int quadrant);
-	bool TestBlockPull(ItemInfo* item, int blockHeight, int quadrant);
+void ClearMovableBlockSplitters(const Vector3i& pos, short roomNumber);
+void PushableBlockControl(short itemNumber);
+void PushableBlockCollision(short itemNumber, ItemInfo* laraItem, CollisionInfo* coll);
+bool TestBlockMovable(ItemInfo* item, int blockHeight);
+bool TestBlockPush(ItemInfo* item, int blockHeight, int quadrant);
+bool TestBlockPull(ItemInfo* item, int blockHeight, int quadrant);
 
-	// Pushable stack utilities
-	void MoveStackXZ(short itemNumber);
-	void MoveStackY(short itemNumber, int y);
-	void RemoveBridgeStack(short itemNumber);
-	void AddBridgeStack(short itemNumber);
-	void RemoveFromStack(short itemNumber);
-	int FindStack(short itemNumber);
-	int GetStackHeight(ItemInfo& item);
-	bool CheckStackLimit(ItemInfo& item);
+// Pushable stack utilities
+void MoveStackXZ(short itemNumber);
+void MoveStackY(short itemNumber, int y);
+void RemoveBridgeStack(short itemNumber);
+void AddBridgeStack(short itemNumber);
+void RemoveFromStack(short itemNumber);
+int FindStack(short itemNumber);
+int GetStackHeight(ItemInfo* item);
+bool CheckStackLimit(ItemInfo* item);
 
-	void PushLoop(ItemInfo& item);
-	void PushEnd(ItemInfo& item);
+void PushLoop(ItemInfo* item);
+void PushEnd(ItemInfo* item);
 
-	std::optional<int> PushableBlockFloor(short itemNumber, int x, int y, int z);
-	std::optional<int> PushableBlockCeiling(short itemNumber, int x, int y, int z);
-	int PushableBlockFloorBorder(short itemNumber);
-	int PushableBlockCeilingBorder(short itemNumber);
-}
+std::optional<int> PushableBlockFloor(short itemNumber, int x, int y, int z);
+std::optional<int> PushableBlockCeiling(short itemNumber, int x, int y, int z);
+int PushableBlockFloorBorder(short itemNumber);
+int PushableBlockCeilingBorder(short itemNumber);
