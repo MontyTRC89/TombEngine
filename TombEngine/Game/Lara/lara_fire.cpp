de2e7c7ca17531b2f5cc7c55a0f68b2280257e87
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2023-01-13 10:01:00 +0100

    Added range customize for hitRoutine
    - use item->ItemFlags[7] to specify the joint detection range.

--- TombEngine/Game/Lara/lara_fire.cpp
+++ TombEngine/Game/Lara/lara_fire.cpp
@@ -1063,7 +1063,8 @@ void HitTarget(ItemInfo* laraItem, ItemInfo* targetEntity, GameVector* hitPos, i
 		for (int jointID = 0; jointID < object.nmeshes; jointID++)
 		{
 			auto pos = GetJointPosition(targetEntity, jointID);
-			if (Vector3i::Distance(hitPos->ToVector3i(), pos) < BLOCK(1 / 16.0f))
+			float distance = targetEntity->ItemFlags[7] != 0 ? (float)targetEntity->ItemFlags[7] : BLOCK(1 / 16.0f);
+			if (Vector3i::Distance(hitPos->ToVector3i(), pos) < distance)
 			{
 				foundJointID = jointID;
 				break;
