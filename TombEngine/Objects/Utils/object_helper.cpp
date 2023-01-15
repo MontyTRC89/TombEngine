38d5a6acf5bdaccdd4bf669ddb401b2d7aabea68
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2023-01-15 14:06:27 +0100

    Added animation check to required obj

--- TombEngine/Objects/Utils/object_helper.cpp
+++ TombEngine/Objects/Utils/object_helper.cpp
@@ -27,6 +27,10 @@ bool AssignObjectAnimations(ObjectInfo& object, int requiredObject, const std::s
 	// Use slot if loaded.
 	if (Objects[requiredObject].loaded)
 	{
+		// Check if the required object has at least 1 animation with more than 1 frame.
+		const auto& anim = g_Level.Anims[Objects[requiredObject].animIndex];
+		if ((anim.frameEnd - anim.frameBase) > 1)
+			return true;
 		object.animIndex = Objects[requiredObject].animIndex;
 		object.frameBase = Objects[requiredObject].frameBase;
 		return true;
