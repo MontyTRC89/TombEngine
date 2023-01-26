680b6fb6ef3082e2b7ba30498e1503a2c0bce43c
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2023-01-26 21:45:26 +0100

    Started normal mode
    - Changed OCB to include lua mode.
    - If ocb is 0 then normal mode is set by default.

--- TombEngine/Objects/TR3/Entity/SophiaLeigh.cpp
+++ TombEngine/Objects/TR3/Entity/SophiaLeigh.cpp
@@ -23,6 +23,8 @@ namespace TEN::Entities::Creatures::TR3
 	constexpr auto SOPHIALEIGH_VAULT_SHIFT = 96;
 	constexpr auto SOPHIALEIGH_WALK_TURN_RATE_MAX = ANGLE(4);
 	constexpr auto SOPHIALEIGH_RUN_TURN_RATE_MAX = ANGLE(7);
+	constexpr auto SOPHIALEIGH_LASER_DECREASE_XANGLE_IF_LARA_CROUCH = ANGLE(0.25f);
+	constexpr auto SOPHIALEIGH_LASER_DISPERSION_ANGLE = ANGLE(1.5f);
 	constexpr auto SOPHIALEIGH_WALK_RANGE = SQUARE(BLOCK(1));
 	constexpr auto SOPHIALEIGH_Y_DISTANCE_RANGE = BLOCK(1.5f);
 	constexpr auto SOPHIALEIGH_REACHED_GOAL_RANGE = CLICK(2);
@@ -81,10 +83,11 @@ namespace TEN::Entities::Creatures::TR3
 		SOPHIALEIGH_ANIM_SCEPTER_SMALL_SHOOT = 25
 	};
 
-	enum class SophiaMode : int
+	enum class SophiaOCB : int
 	{
-		Normal = 0, // Like other entity, move/climb/attack and chase lara.
-		Tower = 1, // TR3 one, which only climb, can't be killed unless a trigger say otherwise (electrical box for example).
+		OCB_Normal = 1, // Like other entity, move/climb/attack and chase lara.
+		OCB_Tower = 2, // TR3 one, which only climb, can't be killed unless a trigger say otherwise (electrical box for example).
+		OCB_LuaToMoveUpDown = 4, // TR3 one but use volume to move her instead of height check, they need to increase or decrease creature->LocationAI for her to go up/down.
 	};
 
 	struct SophiaData
@@ -100,6 +103,8 @@ namespace TEN::Entities::Creatures::TR3
 	{
 		auto* item = &g_Level.Items[itemNumber];
 		InitialiseCreature(itemNumber);
+		if (item->TriggerFlags == 0) // if no mode is set then set it to normal by default.
+			item->TriggerFlags = (short)SophiaOCB::OCB_Normal;
 		item->SetFlagField((int)BossItemFlags::ImmortalState, TRUE); // Immortal state.
 		item->SetFlagField((int)BossItemFlags::ChargedState, FALSE); // Charged state. 1 = full charged.
 		item->SetFlagField((int)BossItemFlags::DeathCount, 0);
@@ -127,7 +132,7 @@ namespace TEN::Entities::Creatures::TR3
 			item->Pose.Orientation.y += angleRate;
 	}
 
-	static void TriggerLaserBolt(ItemInfo* item, ItemInfo* enemy, const BiteInfo& bite, bool isBigLaser, short angleAdd)
+	static void TriggerLaserBolt(ItemInfo* item, ItemInfo* enemy, const BiteInfo& bite, SophiaData* data, bool isBigLaser, short angleAdd)
 	{
 		short fxNumber = CreateNewEffect(item->RoomNumber);
 		if (fxNumber == -1)
@@ -139,26 +144,21 @@ namespace TEN::Entities::Creatures::TR3
 		fx.pos.Position.x = pos.x;
 		fx.pos.Position.y = pos.y;
 		fx.pos.Position.z = pos.z;
+		fx.pos.Orientation.x = item->Pose.Orientation.x + data->torsoXAngle;
 
 		if (enemy->IsLara())
 		{
 			auto lara = GetLaraInfo(enemy);
 			if (lara->Control.IsLow)
-				fx.pos.Orientation.x = item->Pose.Orientation.x - STEPUP_HEIGHT;
-			else
-				fx.pos.Orientation.x = item->Pose.Orientation.x;
-		}
-		else
-		{
-			fx.pos.Orientation.x = item->Pose.Orientation.x;
+				fx.pos.Orientation.x -= SOPHIALEIGH_LASER_DECREASE_XANGLE_IF_LARA_CROUCH;
 		}
 
-		fx.pos.Orientation.y = item->Pose.Orientation.y + angleAdd;
-		fx.pos.Orientation.z = item->Pose.Orientation.z;
+		fx.pos.Orientation.y = item->Pose.Orientation.y + data->torsoYAngle + angleAdd;
+		fx.pos.Orientation.z = 0;
 		fx.roomNumber = item->RoomNumber;
 		fx.counter = 0;
 		fx.flag1 = laserType;
-		fx.flag2 = isBigLaser ? 20 : 1; // damage
+		fx.flag2 = isBigLaser ? 10 : 2; // damage
 		fx.speed = Random::GenerateInt(120, 160);
 		fx.objectNumber = ID_ENERGY_BUBBLES;
 		fx.frameNumber = Objects[fx.objectNumber].meshIndex + (laserType - 1);
@@ -205,10 +205,13 @@ namespace TEN::Entities::Creatures::TR3
 		{
 			creature->ReachedGoal = TRUE;
 			creature->Enemy = LaraItem; // TODO: deal with LaraItem global.
-			if (isEnemyAhead)
-				creature->LocationAI++;
-			else if (isSophiaAhead)
-				creature->LocationAI--;
+			if (!(item->TriggerFlags & (short)SophiaOCB::OCB_LuaToMoveUpDown))
+			{
+				if (isEnemyAhead)
+					creature->LocationAI++;
+				else if (isSophiaAhead)
+					creature->LocationAI--;
+			}
 		}
 		else
 		{
@@ -246,15 +249,15 @@ namespace TEN::Entities::Creatures::TR3
 			{
 				TENLog("AI_GUARD found.");
 			}
-			/*else if (enemy->HitPoints <= 0)
+			else if (creature->Enemy->IsLara() && creature->Enemy->HitPoints <= 0)
 			{
 				item->Animation.TargetState = SOPHIALEIGH_STATE_LAUGH;
-			}*/
+			}
 			else if (creature->ReachedGoal)
 			{
-				if (item->TestFlagField(4, 1))
+				if (item->TestFlagField((int)BossItemFlags::ChargedState, TRUE))
 					item->Animation.TargetState = SOPHIALEIGH_STATE_BIG_SHOOT;
-				else if (!item->Timer)
+				else if (item->Timer <= 0)
 					item->Animation.TargetState = SOPHIALEIGH_STATE_SUMMON;
 				else
 					item->Animation.TargetState = SOPHIALEIGH_STATE_SMALL_SHOOT;
@@ -299,7 +302,7 @@ namespace TEN::Entities::Creatures::TR3
 			}
 			else if (item->Animation.AnimNumber == (Objects[item->ObjectNumber].animIndex + SOPHIALEIGH_ANIM_SUMMON) && item->Animation.FrameNumber >= GetFrameCount(item->Animation.AnimNumber) - 1)
 			{
-				item->SetFlagField(4, 1);
+				item->SetFlagField((int)BossItemFlags::ChargedState, TRUE);
 			}
 
 			//TriggerSophiaShockwave(item, SOPHIALEIGH_Staff); // SHITTY EFFECT
@@ -314,13 +317,13 @@ namespace TEN::Entities::Creatures::TR3
 				data->torsoXAngle = ai.xAngle;
 			}
 
-			item->SetFlagField(4, 0);
+			item->SetFlagField((int)BossItemFlags::ChargedState, FALSE);
 
 			if (item->Animation.FrameNumber == GetFrameNumber(item, 36))
 			{
-				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Right, false, ANGLE(1.5f));
-				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Staff, true, 0);
-				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Left, false, -ANGLE(1.5f));
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Right, data, false, SOPHIALEIGH_LASER_DISPERSION_ANGLE);
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Staff, data, true, 0);
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Left, data, false, -SOPHIALEIGH_LASER_DISPERSION_ANGLE);
 			}
 			
 			break;
@@ -335,8 +338,8 @@ namespace TEN::Entities::Creatures::TR3
 
 			if (item->Animation.FrameNumber == GetFrameNumber(item, 14))
 			{
-				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Right, false, ANGLE(1.5f));
-				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Left, false, -ANGLE(1.5f));
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Right, data, false, SOPHIALEIGH_LASER_DISPERSION_ANGLE);
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Left, data, false, -SOPHIALEIGH_LASER_DISPERSION_ANGLE);
 			}
 
 			break;
@@ -345,7 +348,94 @@ namespace TEN::Entities::Creatures::TR3
 
 	static void LondonBossNormalControl(ItemInfo* item, CreatureInfo* creature, SophiaData* data)
 	{
+		AI_INFO ai;
 
+		CreatureAIInfo(item, &ai);
+		GetCreatureMood(item, &ai, true);
+		CreatureMood(item, &ai, true);
+
+		data->angle = CreatureTurn(item, creature->MaxTurn);
+		switch (item->Animation.ActiveState)
+		{
+		case SOPHIALEIGH_STATE_LAUGH:
+			creature->MaxTurn = 0;
+
+			break;
+		case SOPHIALEIGH_STATE_STAND:
+			creature->MaxTurn = 0;
+			creature->Flags = 0;
+
+			break;
+		case SOPHIALEIGH_STATE_WALK:
+			creature->MaxTurn = SOPHIALEIGH_WALK_TURN_RATE_MAX;
+
+			break;
+		case SOPHIALEIGH_STATE_RUN:
+			creature->MaxTurn = SOPHIALEIGH_RUN_TURN_RATE_MAX;
+			data->tilt = data->angle / 2;
+
+
+			break;
+		case SOPHIALEIGH_STATE_SUMMON:
+			creature->MaxTurn = 0;
+
+			if (item->Animation.AnimNumber == Objects[item->ObjectNumber].animIndex + SOPHIALEIGH_ANIM_SUMMON_START)
+			{
+				if (item->Animation.FrameNumber == GetFrameNumber(item, 0))
+				{
+					item->Timer = SOPHIALEIGH_CHARGE_TIMER_DURATION;
+				}
+				else if (item->HitStatus && item->Animation.TargetState != SOPHIALEIGH_STATE_STAND && Random::TestProbability(1.0f / 50.0f)) // Avoid her being canceled every time.
+				{
+					StopSoundEffect(SFX_TR3_SOFIALEE_SUMMON);
+					SoundEffect(SFX_TR3_SOFIALEE_SUMMON_FAIL, &item->Pose);
+					SoundEffect(SFX_TR3_SOFIALEE_TAKE_HIT, &item->Pose);
+					item->Animation.TargetState = SOPHIALEIGH_STATE_STAND;
+				}
+			}
+			else if (item->Animation.AnimNumber == (Objects[item->ObjectNumber].animIndex + SOPHIALEIGH_ANIM_SUMMON) && item->Animation.FrameNumber >= GetFrameCount(item->Animation.AnimNumber) - 1)
+			{
+				item->SetFlagField((int)BossItemFlags::ChargedState, TRUE);
+			}
+
+			//TriggerSophiaShockwave(item, SOPHIALEIGH_Staff); // SHITTY EFFECT
+
+			break;
+		case SOPHIALEIGH_STATE_BIG_SHOOT:
+			creature->MaxTurn = 0;
+			if (ai.ahead)
+			{
+				data->torsoYAngle = ai.angle;
+				data->torsoXAngle = ai.xAngle;
+			}
+
+			item->SetFlagField((int)BossItemFlags::ChargedState, FALSE);
+
+			if (item->Animation.FrameNumber == GetFrameNumber(item, 36))
+			{
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Right, data, false, SOPHIALEIGH_LASER_DISPERSION_ANGLE);
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Staff, data, true, 0);
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Left, data, false, -SOPHIALEIGH_LASER_DISPERSION_ANGLE);
+			}
+
+			break;
+		case SOPHIALEIGH_STATE_SMALL_SHOOT:
+			creature->MaxTurn = 0;
+			RotateToTarget(item, &ai, SOPHIALEIGH_WALK_TURN_RATE_MAX);
+			if (ai.ahead)
+			{
+				data->torsoYAngle = ai.angle;
+				data->torsoXAngle = ai.xAngle;
+			}
+
+			if (item->Animation.FrameNumber == GetFrameNumber(item, 14))
+			{
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Right, data, false, SOPHIALEIGH_LASER_DISPERSION_ANGLE);
+				TriggerLaserBolt(item, creature->Enemy, SOPHIALEIGH_Left, data, false, -SOPHIALEIGH_LASER_DISPERSION_ANGLE);
+			}
+
+			break;
+		}
 	}
 
 	void LondonBossControl(short itemNumber)
@@ -359,7 +449,7 @@ namespace TEN::Entities::Creatures::TR3
 		SophiaData data{};
 
 		// She is immortal in tower mode !
-		if (item->TestFlagField((int)BossItemFlags::ImmortalState, TRUE) && item->TestOcb((int)SophiaMode::Tower))
+		if (item->TestFlagField((int)BossItemFlags::ImmortalState, TRUE) && item->TestOcb((int)SophiaOCB::OCB_Tower))
 			item->HitPoints = object.HitPoints;
 
 		if (item->HitPoints <= 0)
@@ -384,7 +474,7 @@ namespace TEN::Entities::Creatures::TR3
 		}
 		else
 		{
-			if (item->TestOcb((int)SophiaMode::Tower))
+			if (item->TestOcb((int)SophiaOCB::OCB_Tower))
 				LondonBossTowerControl(item, creature, &data);
 			else
 				LondonBossNormalControl(item, creature, &data);
