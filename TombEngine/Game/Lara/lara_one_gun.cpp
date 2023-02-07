d5f7d42dfcce8e3437380d7af1b519589c757b90
Sezz <sezzary@outlook.com>
2023-02-08 00:01:30 +1100

    Update bubble spawning; update SinkInfo struct

--- TombEngine/Game/Lara/lara_one_gun.cpp
+++ TombEngine/Game/Lara/lara_one_gun.cpp
@@ -635,12 +635,8 @@ void HarpoonBoltControl(short itemNumber)
 	}
 	else
 	{
-		// Spawn bubbles.
 		if (Wibble & 4)
-		{
-			for (int i = 0; i < 3; i++)
-				SpawnBubble(item.Pose.Position.ToVector3(), item.RoomNumber, (int)BubbleFlags::HighAmplitude);
-		}
+			SpawnBubble(item.Pose.Position.ToVector3(), item.RoomNumber, (int)BubbleFlags::HighAmplitude);
 			
 		item.Animation.Velocity.y = -HARPOON_VELOCITY * phd_sin(item.Pose.Orientation.x) / 2;
 		item.Animation.Velocity.z = HARPOON_VELOCITY * phd_cos(item.Pose.Orientation.x) / 2;
