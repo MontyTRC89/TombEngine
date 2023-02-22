fe472f66c606a6ab34a2d5bf09cdd0adfb951b56
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2023-02-22 11:25:19 +0100

    Fix tr2 statue (#1004)
    * Added tr2_spear transition from jade to alive
    
    - added a smoke when the transition is done for a mesh, it need some adjust through.
    
    * Updated sword guardian too.
    
    * Fix spear and sword taking damage when doing transition
    
    * Fix sword and spear joint rotation
    
    * Fix project toolset
    
    * Update Changes.txt
    
    * Update Changes.txt
    
    * Fixed sword guardian using wrong effect during transition.
    
    - Remove TENLog in spear and sword code.
    - Improved sword fly effect randomness (now in sphere).
    
    * Fix project toolset again
    
    * Changed sector to block
    
    * Grammar, formatting
    
    * Reformat enum
    
    * Formatting
    
    ---------
    
    Co-authored-by: Sezz <sezzary@outlook.com>
    Co-authored-by: Kubsy <80340234+Kubsy@users.noreply.github.com>

--- TombEngine/Objects/TR2/Entity/tr2_spear_guardian.h
+++ TombEngine/Objects/TR2/Entity/tr2_spear_guardian.h
@@ -1,7 +1,14 @@
 #pragma once
 
+class GameVector;
+class Vector3i;
+struct ItemInfo;
+
 namespace TEN::Entities::Creatures::TR2
 {
 	void InitialiseSpearGuardian(short itemNumber);
 	void SpearGuardianControl(short itemNumber);
+	void SpearGuardianHit(ItemInfo& target, ItemInfo& source, std::optional<GameVector> pos, int damage, bool isExplosive, int jointIndex);
+	
+	void SpawnSpearGuardianParticle(const Vector3& pos, int roomNumber);
 }
