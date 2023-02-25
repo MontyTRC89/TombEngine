9860a39b9d1321b6c7e66be3021559f9da98bfa6
Kubsy <kubadd475@gmail.com>
2023-02-25 18:58:03 +0000

    start implementing teeth spike door

---
+++ TombEngine/Objects/TR1/Trap/TeethSpikeDoor.cpp
@@ -0,0 +1,48 @@
+#include "framework.h"
+#include "Objects/TR1/Trap/TeethSpikeDoor.h"
+
+#include "Game/camera.h"
+#include "Game/collision/collide_item.h"
+#include "Game/collision/collide_room.h"
+#include "Game/effects/effects.h"
+#include "Game/Lara/lara.h"
+#include "Specific/level.h"
+#include "Specific/setup.h"
+
+using namespace TEN::Math;
+
+namespace TEN::Entities::Traps::TR1
+{
+	constexpr auto TEETH_SPIKE_DOOR_DAMAGE = 400;
+
+	enum TeethSpikeDoorState
+	{
+		TEETHSPIKEDOOR_DISABLED = 0,
+		TEETHSPIKEDOOR_ENABLED = 1
+	};
+
+	void InitialiseTeethSpikeDoor(short itemNumber)
+	{
+		auto* item = &g_Level.Items[itemNumber];
+	}
+
+	void ControlTeethSpikeDoor(short itemNumber)
+	{
+		auto* item = &g_Level.Items[itemNumber];
+
+		if (TriggerActive(item))
+		{
+			item->Animation.ActiveState = item->Animation.TargetState = TEETHSPIKEDOOR_ENABLED;
+			if (item->TouchBits.TestAny())
+			{
+				DoDamage(LaraItem, TEETH_SPIKE_DOOR_DAMAGE);
+				//TODO Add blood and check for individual spike in door(?)
+			}
+		}
+		else
+		{
+			item->Animation.ActiveState = item->Animation.TargetState = TEETHSPIKEDOOR_DISABLED;
+		}
+	}
+
+}
---
+++ TombEngine/Objects/TR1/Trap/TeethSpikeDoor.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct CollisionInfo;
+struct ItemInfo;
+struct ObjectInfo;
+
+namespace TEN::Entities::Traps::TR1
+{
+	void InitialiseTeethSpikeDoor(short itemNumber);
+
+	void ControlTeethSpikeDoor(short itemNumber);
+}
