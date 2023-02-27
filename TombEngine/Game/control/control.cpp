b4e475a5c801590fca97103ea779fd5915847f2a
Nemoel-Tomo <tomo_669@hotmail.com>
2023-02-27 17:03:34 +0100

    clear camera at levelend

--- TombEngine/Game/control/control.cpp
+++ TombEngine/Game/control/control.cpp
@@ -429,6 +429,9 @@ void CleanUp()
 
 	// Clear all remaining renderer data.
 	g_Renderer.ClearScene();
+
+	// Reset Itemcamera
+	ClearObjCamera();
 }
 
 void InitialiseScripting(int levelIndex, bool loadGame)
