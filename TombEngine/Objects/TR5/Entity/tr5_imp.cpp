a48b644c9d26bb65e8e6cba4ab8d2b7db2b65b81
Sezz <sezzary@outlook.com>
2023-03-04 03:06:16 +1100

    Fix imp formatting

--- TombEngine/Objects/TR5/Entity/tr5_imp.cpp
+++ TombEngine/Objects/TR5/Entity/tr5_imp.cpp
@@ -8,34 +8,34 @@
 #include "Game/itemdata/creature_info.h"
 #include "Game/items.h"
 #include "Game/Lara/lara.h"
+#include "Game/Lara/lara_helpers.h"
 #include "Game/misc.h"
 #include "Math/Math.h"
 #include "Objects/Generic/Object/burning_torch.h"
 #include "Specific/level.h"
 #include "Specific/setup.h"
-#include "Game/Lara/lara_helpers.h"
 
 using namespace TEN::Entities::Generic;
 using namespace TEN::Math;
 
 namespace TEN::Entities::Creatures::TR5
 {
-	constexpr auto IMP_SCARED_TURN_RATE_MAX = ANGLE(7.0f);
-	constexpr auto IMP_WALK_TURN_RATE_MAX = ANGLE(7.0f);
-	constexpr auto IMP_RUN_TURN_RATE_MAX = ANGLE(7.0f);
-	constexpr auto IMP_ATTACKJUMP_OR_STONE_TURN_RATE_MAX = ANGLE(2.0f);
-	constexpr auto IMP_SHOOT_DEATH_ANGLE_BACKWARD = ANGLE(67.5f);
-
-	constexpr auto IMP_ATTACK_RANGE = SQUARE(BLOCK(0.25f));
-	constexpr auto IMP_WALK_RANGE = SQUARE(BLOCK(2));
-	constexpr auto IMP_TORCH_LIT_SCARED_RANGE = BLOCK(2);
 	constexpr auto IMP_ATTACK_DAMAGE = 3;
 
-	constexpr auto MESHSWAP_RATE = 16;
+	constexpr auto IMP_WALK_RANGE			  = SQUARE(BLOCK(2));
+	constexpr auto IMP_ATTACK_RANGE			  = SQUARE(BLOCK(0.25f));
+	constexpr auto IMP_LIT_TORCH_SCARED_RANGE = BLOCK(2);
 
-	const auto ImpLeftHandBite = BiteInfo(Vector3(0.0f, 100.0f, 0.0f), 7);
+	constexpr auto IMP_WALK_TURN_RATE_MAX	= ANGLE(7.0f);
+	constexpr auto IMP_RUN_TURN_RATE_MAX	= ANGLE(7.0f);
+	constexpr auto IMP_SCARED_TURN_RATE_MAX = ANGLE(7.0f);
+	constexpr auto IMP_ATTACK_TURN_RATE_MAX = ANGLE(2.0f);
+
+	constexpr auto IMP_HEAD_MESH_SWAP_INTERVAL = 16;
+
+	const auto ImpLeftHandBite	= BiteInfo(Vector3(0.0f, 100.0f, 0.0f), 7);
 	const auto ImpRightHandBite = BiteInfo(Vector3(0.0f, 100.0f, 0.0f), 9);
-	const auto ImpHeadSwapJoints = std::vector <unsigned int> { 10 };
+	const auto ImpHeadMeshSwapJoints = std::vector<unsigned int>{ 10 };
 	
 	enum ImpState
 	{
@@ -45,40 +45,39 @@ namespace TEN::Entities::Creatures::TR5
 		IMP_STATE_ATTACK_1 = 3,
 		IMP_STATE_JUMP_ATTACK = 5,
 		IMP_STATE_SCARED = 6,
-		IMP_STATE_START_CLIMB = 7,
-		IMP_STATE_START_ROLL = 8,
+		IMP_STATE_VAULT_UP_1_STEP = 7,
+		IMP_STATE_ROLL = 8,
 		IMP_STATE_DEATH = 9,
-		IMP_STATE_THROW_STONES = 11
+		IMP_STATE_STONE_ATTACK = 11
 	};
 
 	enum ImpAnim
 	{
 		IMP_ANIM_WALK = 0,
-		IMP_ANIM_WAIT = 1,
+		IMP_ANIM_IDLE = 1,
 		IMP_ANIM_RUN = 2,
 		IMP_ANIM_ATTACK = 3,
 		IMP_ANIM_CRY = 4,
 		IMP_ANIM_JUMP_ATTACK = 5,  
 		IMP_ANIM_SCARED_RUN_AWAY = 6,
-		IMP_ANIM_CLIMB_UP = 7,
-		IMP_ANIM_BARREL_ROLL = 8,
-		IMP_ANIM_DEATH_BACKWARDS = 9,
-		IMP_ANIM_DEATH_FORWARD = 10,
-		IMP_ANIM_LEFT_FOOT_START_WALK = 11,
-		IMP_ANIM_RIGHT_FOOT_START_WALK = 12,
+		IMP_ANIM_VAULT_UP_1_STEP = 7,
+		IMP_ANIM_ROLL = 8,
+		IMP_ANIM_BACKWARD_DEATH = 9,
+		IMP_ANIM_FORWARD_DEATH = 10,
+		IMP_ANIM_IDLE_TO_WALK_LEFT = 11,
+		IMP_ANIM_IDLE_TO_WALK_RIGHT = 12,
 		IMP_ANIM_SLOW_TO_WAIT_LEFT = 13,
 		IMP_ANIM_SLOW_TO_WAIT_RIGHT = 14,
 		IMP_ANIM_SLOW_TO_RUN = 15,
 		IMP_ANIM_SLOW_TO_WALK = 16,
-		IMP_ANIM_THROW_ROCK = 17
+		IMP_ANIM_STONE_ATTACK = 17
 	};
 
-	enum ImpOCB
+	enum ImpOcb
 	{
-		IMP_OCB_CLIMB_UP_START = 1,
-		IMP_OCB_BARREL_ROLL_START = 2,
-		IMP_OCB_THROW_STONES = 3
-
+		IMP_OCB_CLIMB_UP = 1,
+		IMP_OCB_ROLL = 2,
+		IMP_OCB_STONE_ATTACK = 3
 	};
 
 	static void RotateTowardTarget(ItemInfo& item, const AI_INFO& ai, short turnRate)
@@ -97,125 +96,137 @@ namespace TEN::Entities::Creatures::TR5
 		}
 	}
 
-	void InitialiseImp(short itemNumber)
-	{
-		auto* item = &g_Level.Items[itemNumber];
-		InitialiseCreature(itemNumber);
-
-		switch (item->TriggerFlags)
-		{
-		case IMP_OCB_BARREL_ROLL_START:
-			SetAnimation(item, IMP_ANIM_BARREL_ROLL);
-			break;
-		case IMP_OCB_CLIMB_UP_START:
-			SetAnimation(item, IMP_ANIM_CLIMB_UP);
-			break;
-		default:
-			SetAnimation(item, IMP_ANIM_WAIT);
-			break;
-		}
-	}
-
-	void ImpThrowStones(ItemInfo* item)
+	static void DoImpStoneAttack(ItemInfo* item)
 	{
 		auto pos1 = GetJointPosition(item, ImpRightHandBite.meshNum);
 		auto pos2 = GetJointPosition(LaraItem, LM_HEAD);
 		auto orient = Geometry::GetOrientToPoint(pos1.ToVector3(), pos2.ToVector3());
-		
+
 		int distance = Vector3i::Distance(pos1, pos2);
 		if (distance < 8)
 			distance = 8;
+
 		orient.x += short(GetRandomControl() % (distance / 2) - (distance / 4));
 		orient.y += short(GetRandomControl() % (distance / 4) - (distance / 8));
 
-		short fxNumber = CreateNewEffect(item->RoomNumber);
-		if (fxNumber != NO_ITEM)
-		{
-			auto* fx = &EffectList[fxNumber];
+		int fxNumber = CreateNewEffect(item->RoomNumber);
+		if (fxNumber == NO_ITEM)
+			return;
 
-			fx->pos.Position = pos1;
-			fx->roomNumber = item->RoomNumber;
-			fx->speed = 4 * sqrt(distance);
+		auto& fx = EffectList[fxNumber];
 
-			fx->pos.Orientation = EulerAngles(orient.x + (distance / 2), orient.y, 0);
+		fx.objectNumber = ID_IMP_ROCK;
+		fx.frameNumber = Objects[ID_IMP_ROCK].meshIndex;
 
-			if (fx->speed < 256)
-				fx->speed = 256;
+		fx.pos.Position = pos1;
+		fx.roomNumber = item->RoomNumber;
+		fx.speed = sqrt(distance) * 4;
 
-			fx->fallspeed = 0;
-			fx->objectNumber = ID_IMP_ROCK;
-			fx->frameNumber = Objects[ID_IMP_ROCK].meshIndex;
-			fx->color = Vector4::One;
-			fx->counter = 0;
-			fx->flag1 = 2;
-			fx->flag2 = 0x2000;
-		}
+		fx.pos.Orientation = EulerAngles(orient.x + (distance / 2), orient.y, 0);
+
+		if (fx.speed < BLOCK(0.25f))
+			fx.speed = BLOCK(0.25f);
+
+		fx.fallspeed = 0;
+		fx.color = Vector4::One;
+		fx.counter = 0;
+		fx.flag1 = 2;
+		fx.flag2 = 0x2000;
 	}
 
-	static bool IsTorchLitNearby(ItemInfo* item, CreatureInfo* creature)
+	static bool IsTorchLitNearby(ItemInfo& item)
 	{
-		if (creature->Enemy->IsLara())
+		const auto& creature = *GetCreatureInfo(&item);
+
+		if (creature.Enemy->IsLara())
 		{
-			auto* lara = GetLaraInfo(creature->Enemy);
-			float distance = Vector3::Distance(item->Pose.Position.ToVector3(), creature->Enemy->Pose.Position.ToVector3());
-			if (lara->Torch.IsLit && distance <= IMP_TORCH_LIT_SCARED_RANGE)
+			const auto& player = *GetLaraInfo(creature.Enemy);
+
+			float distance = Vector3i::Distance(item.Pose.Position, creature.Enemy->Pose.Position);
+			if (player.Torch.IsLit && distance <= IMP_LIT_TORCH_SCARED_RANGE)
 				return true;
 		}
 
-		auto foundTorchList = FoundCreatedItems(ID_BURNING_TORCH_ITEM);
-		for (auto& torchItemNumber : foundTorchList)
+		auto torchItems = FoundCreatedItems(ID_BURNING_TORCH_ITEM);
+		for (auto& itemNumber : torchItems)
 		{
-			if (torchItemNumber == NO_ITEM)
+			if (itemNumber == NO_ITEM)
 				continue;
-			auto& torch = g_Level.Items[torchItemNumber];
-			float distance = Vector3::Distance(item->Pose.Position.ToVector3(), torch.Pose.Position.ToVector3());
-			if (torch.ItemFlags[3] != 0 && distance <= IMP_TORCH_LIT_SCARED_RANGE)
+
+			const auto& torchItem = g_Level.Items[itemNumber];
+
+			float distance = Vector3i::Distance(item.Pose.Position, torchItem.Pose.Position);
+			if (torchItem.ItemFlags[3] != 0 && distance <= IMP_LIT_TORCH_SCARED_RANGE)
 				return true;
 		}
 
 		return false;
 	}
 
+	void InitialiseImp(short itemNumber)
+	{
+		auto& item = g_Level.Items[itemNumber];
+
+		InitialiseCreature(itemNumber);
+
+		switch (item.TriggerFlags)
+		{
+		case IMP_OCB_ROLL:
+			SetAnimation(&item, IMP_ANIM_ROLL);
+			break;
+
+		case IMP_OCB_CLIMB_UP:
+			SetAnimation(&item, IMP_ANIM_VAULT_UP_1_STEP);
+			break;
+
+		default:
+			SetAnimation(&item, IMP_ANIM_IDLE);
+			break;
+		}
+	}
+
 	void ImpControl(short itemNumber)
 	{
 		if (!CreatureActive(itemNumber))
 			return;
-		
-		AI_INFO AI;
-		short angle = 0;
-		short otherAngle = 0;
-		short torso_y = 0;
-		short torso_x = 0;
-		short head_y = 0;
-		short head_x = 0;
 
 		auto* item = &g_Level.Items[itemNumber];
 		auto* creature = GetCreatureInfo(item);
 
+		short headingAngle = 0;
+
+		auto extraHeadRot = EulerAngles::Zero;
+		auto extraTorsoRot = EulerAngles::Zero;
+
+		AI_INFO ai;
 		if (item->HitPoints <= 0)
 		{
 			if (item->Animation.ActiveState != IMP_STATE_DEATH)
 			{
-				CreatureAIInfo(item, &AI);
-				if (AI.angle > -IMP_SHOOT_DEATH_ANGLE_BACKWARD && AI.angle < IMP_SHOOT_DEATH_ANGLE_BACKWARD)
-					SetAnimation(item, IMP_ANIM_DEATH_BACKWARDS);
+				CreatureAIInfo(item, &ai);
+
+				if (ai.angle > -ANGLE(67.5f) && ai.angle < ANGLE(67.5f))
+					SetAnimation(item, IMP_ANIM_BACKWARD_DEATH);
 				else
-					SetAnimation(item, IMP_ANIM_DEATH_FORWARD);
+					SetAnimation(item, IMP_ANIM_FORWARD_DEATH);
 			}
 		}
 		else
 		{
 			if (item->AIBits)
+			{
 				GetAITarget(creature);
+			}
 			else if (creature->HurtByLara)
+			{
 				creature->Enemy = LaraItem;
+			}
 
-			CreatureAIInfo(item, &AI);
+			CreatureAIInfo(item, &ai);
 
-			int elevation = item->Pose.Position.y - creature->Enemy->Pose.Position.y + CLICK(1.5f);
+			int elevation = (item->Pose.Position.y - creature->Enemy->Pose.Position.y) + CLICK(1.5f);
 			if (creature->Enemy->IsLara())
 			{
-				otherAngle = AI.angle;
 				if (creature->Enemy->Animation.ActiveState == LS_CROUCH_IDLE ||
 					creature->Enemy->Animation.ActiveState == LS_CROUCH_ROLL ||
 					creature->Enemy->Animation.ActiveState > LS_MONKEY_TURN_180 &&
@@ -226,32 +237,32 @@ namespace TEN::Entities::Creatures::TR5
 					elevation = item->Pose.Position.y - creature->Enemy->Pose.Position.y;
 				}
 			}
-			else
-				otherAngle = phd_atan(creature->Enemy->Pose.Position.z - item->Pose.Position.z, creature->Enemy->Pose.Position.x - item->Pose.Position.x) - item->Pose.Orientation.y;
 			
-			AI.xAngle = phd_atan(sqrt(AI.distance), elevation);
+			ai.xAngle = phd_atan(sqrt(ai.distance), elevation);
 
-			GetCreatureMood(item, &AI, true);
+			GetCreatureMood(item, &ai, true);
 			if (item->Animation.ActiveState == IMP_STATE_SCARED)
 				creature->Mood = MoodType::Escape;
-			CreatureMood(item, &AI, true);
 
-			angle = CreatureTurn(item, creature->MaxTurn);
+			CreatureMood(item, &ai, true);
 
-			if (Wibble & MESHSWAP_RATE)
-				item->SetMeshSwapFlags (ImpHeadSwapJoints);
+			headingAngle = CreatureTurn(item, creature->MaxTurn);
+
+			if (Wibble & IMP_HEAD_MESH_SWAP_INTERVAL)
+				item->SetMeshSwapFlags (ImpHeadMeshSwapJoints);
 			else
 				item->SetMeshSwapFlags(NO_JOINT_BITS);
 
-			if (AI.ahead)
+			if (ai.ahead)
 			{
-				torso_x = AI.xAngle / 2;
-				torso_y = AI.angle / 2;
-				head_x = AI.xAngle / 2;
-				head_y = AI.angle / 2;
+				extraTorsoRot.x = ai.xAngle / 2;
+				extraTorsoRot.y = ai.angle / 2;
+				extraHeadRot.x = ai.xAngle / 2;
+				extraHeadRot.y = ai.angle / 2;
 			}
 
-			bool isScared = IsTorchLitNearby(item, creature);
+			bool isScared = IsTorchLitNearby(*item);
+
 			switch (item->Animation.ActiveState)
 			{
 			case IMP_STATE_WALK:
@@ -263,10 +274,14 @@ namespace TEN::Entities::Creatures::TR5
 					break;
 				}
 
-				if (AI.distance > IMP_WALK_RANGE)
+				if (ai.distance > IMP_WALK_RANGE)
+				{
 					item->Animation.TargetState = IMP_STATE_RUN;
-				else if (AI.bite && AI.distance < IMP_ATTACK_RANGE)
+				}
+				else if (ai.bite && ai.distance < IMP_ATTACK_RANGE)
+				{
 					item->Animation.TargetState = IMP_STATE_IDLE;
+				}
 
 				break;
 
@@ -280,9 +295,9 @@ namespace TEN::Entities::Creatures::TR5
 					break;
 				}
 
-				if (AI.bite && AI.distance < IMP_ATTACK_RANGE)
+				if (ai.bite && ai.distance < IMP_ATTACK_RANGE)
 				{
-					if (GetRandomControl() & 1)
+					if (Random::TestProbability(1 / 2.0f))
 						item->Animation.TargetState = IMP_STATE_ATTACK_1;
 					else
 						item->Animation.TargetState = IMP_STATE_JUMP_ATTACK;
@@ -291,15 +306,15 @@ namespace TEN::Entities::Creatures::TR5
 				{
 					item->Animation.TargetState = IMP_STATE_WALK;
 				}
-				else if(item->TriggerFlags == IMP_OCB_THROW_STONES)
+				else if(item->TriggerFlags == IMP_OCB_STONE_ATTACK)
 				{
-					item->Animation.TargetState = IMP_STATE_THROW_STONES;
+					item->Animation.TargetState = IMP_STATE_STONE_ATTACK;
 				}
-				else if (AI.distance > IMP_WALK_RANGE)
+				else if (ai.distance > IMP_WALK_RANGE)
 				{
 					item->Animation.TargetState = IMP_STATE_RUN;
 				}
-				else if (AI.distance > IMP_ATTACK_RANGE)
+				else if (ai.distance > IMP_ATTACK_RANGE)
 				{
 					item->Animation.TargetState = IMP_STATE_WALK;
 				}
@@ -315,10 +330,14 @@ namespace TEN::Entities::Creatures::TR5
 					break;
 				}
 
-				if (AI.distance < IMP_ATTACK_RANGE)
+				if (ai.distance < IMP_ATTACK_RANGE)
+				{
 					item->Animation.TargetState = IMP_STATE_IDLE;
-				else if (AI.distance < IMP_WALK_RANGE)
+				}
+				else if (ai.distance < IMP_WALK_RANGE)
+				{
 					item->Animation.TargetState = IMP_STATE_WALK;
+				}
 
 				break;
 
@@ -333,8 +352,9 @@ namespace TEN::Entities::Creatures::TR5
 				}
 				
 				break;
+
 			case IMP_STATE_JUMP_ATTACK:
-				RotateTowardTarget(*item, AI, IMP_ATTACKJUMP_OR_STONE_TURN_RATE_MAX);
+				RotateTowardTarget(*item, ai, IMP_ATTACK_TURN_RATE_MAX);
 
 				if (!(creature->Flags & 1) && item->TouchBits.Test(ImpRightHandBite.meshNum))
 				{
@@ -354,34 +374,36 @@ namespace TEN::Entities::Creatures::TR5
 
 			case IMP_STATE_SCARED:
 				creature->MaxTurn = IMP_SCARED_TURN_RATE_MAX;
+
 				if (!isScared)
 				{
-					if (AI.distance > IMP_WALK_RANGE)
+					if (ai.distance > IMP_WALK_RANGE)
 						item->Animation.TargetState = IMP_STATE_RUN;
 					else
 						item->Animation.TargetState = IMP_STATE_WALK;
 				}
+
 				break;
 
-			case IMP_STATE_START_CLIMB:
-			case IMP_STATE_START_ROLL:
+			case IMP_STATE_VAULT_UP_1_STEP:
+			case IMP_STATE_ROLL:
 				creature->MaxTurn = 0;
 				break;
 
-			case IMP_STATE_THROW_STONES:
-				RotateTowardTarget(*item, AI, IMP_ATTACKJUMP_OR_STONE_TURN_RATE_MAX);
+			case IMP_STATE_STONE_ATTACK:
+				RotateTowardTarget(*item, ai, IMP_ATTACK_TURN_RATE_MAX);
 				
 				if (item->Animation.FrameNumber == GetFrameNumber(item, 40))
-					ImpThrowStones(item);
+					DoImpStoneAttack(item);
 
 				break;
 			}
 		}
 
-		CreatureJoint(item, 0, torso_x);
-		CreatureJoint(item, 1, torso_y);
-		CreatureJoint(item, 2, head_x);
-		CreatureJoint(item, 3, head_y);
-		CreatureAnimation(itemNumber, angle, 0);
+		CreatureJoint(item, 0, extraTorsoRot.x);
+		CreatureJoint(item, 1, extraTorsoRot.y);
+		CreatureJoint(item, 2, extraHeadRot.x);
+		CreatureJoint(item, 3, extraHeadRot.y);
+		CreatureAnimation(itemNumber, headingAngle, 0);
 	}
 }
