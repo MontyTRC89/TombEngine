1199292ab6bce37de40f71bd3f0b861ef62c7653
Sezz <sezzary@outlook.com>
2023-03-21 00:27:18 +1100

    Tabulation

--- TombEngine/Game/animation.h
+++ TombEngine/Game/animation.h
@@ -124,10 +124,10 @@ int		  GetNextAnimState(int objectID, int animNumber);
 bool	  GetStateDispatch(ItemInfo* item, const AnimData& anim);
 
 AnimFrameInterpData GetFrameInterpData(const ItemInfo& item);
-AnimFrame* GetFrame(GAME_OBJECT_ID objectID, int animNumber, int frameNumber);
-AnimFrame* GetFirstFrame(GAME_OBJECT_ID objectID, int animNumber);
-AnimFrame* GetLastFrame(GAME_OBJECT_ID objectID, int animNumber);
-AnimFrame* GetBestFrame(ItemInfo* item);
+AnimFrame*			GetFrame(GAME_OBJECT_ID objectID, int animNumber, int frameNumber);
+AnimFrame*			GetFirstFrame(GAME_OBJECT_ID objectID, int animNumber);
+AnimFrame*			GetLastFrame(GAME_OBJECT_ID objectID, int animNumber);
+AnimFrame*			GetBestFrame(ItemInfo* item);
 
 void ClampRotation(Pose& outPose, short angle, short rotation); 
 void DrawAnimatingItem(ItemInfo* item);
