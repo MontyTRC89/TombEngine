345ff5bbe8472a652aa4196fb975b8af762a1fed
davidmarr <116632612+davidmarr@users.noreply.github.com>
2023-04-03 10:40:57 +0200

    Added the LUA SetFlags() function (#1036)
    * Added the LUA SetFlags()
    
    Added the LUA SetFlags() function. It allows you to change the flags options of a string
    
    * Update DisplayString.cpp
    
    * Update DisplayString.cpp
    
    added usage example
    
    * Added possible parameters in the example
    
    Full example

--- TombEngine/Scripting/Internal/TEN/Strings/DisplayString/DisplayString.cpp
+++ TombEngine/Scripting/Internal/TEN/Strings/DisplayString/DisplayString.cpp
@@ -132,7 +132,18 @@ void DisplayString::Register(sol::table & parent)
 		// @function DisplayString:GetPosition
 		// @treturn int x x-coordinate of the string
 		// @treturn int y y-coordinate of the string
-		ScriptReserved_GetPosition, &DisplayString::GetPos
+		ScriptReserved_GetPosition, &DisplayString::GetPos,
+
+		/// Set the display string's flags 
+		// @function DisplayString:SetFlags
+		// @tparam Table table the new table with display flags options
+		// Example with possible values:
+		// local varDisplayString = DisplayString('example string', 0, 0, Color(255, 255, 255), false)
+		// varDisplayString:SetFlags({})
+		// varDisplayString:SetFlags({ TEN.Strings.DisplayStringOption.SHADOW })
+		// varDisplayString:SetFlags({ TEN.Strings.DisplayStringOption.CENTER })
+		// varDisplayString:SetFlags({ TEN.Strings.DisplayStringOption.SHADOW, TEN.Strings.DisplayStringOption.CENTER })
+		ScriptReserved_SetFlags, & DisplayString::SetFlags
 	);
 }
 
@@ -179,6 +190,17 @@ std::string DisplayString::GetKey() const
 {
 	UserDisplayString& s = s_getItemCallback(m_id).value();
 	return s.m_key;
+}
+
+void DisplayString::SetFlags(sol::table const& flags) 
+{
+	UserDisplayString& s = s_getItemCallback(m_id).value();
+	FlagArray f{};
+	for (const auto& val : flags) {
+		auto i = val.second.as<size_t>();
+		f[i] = true;
+	}
+	s.m_flags = f;
 }
 
 SetItemCallback DisplayString::s_setItemCallback = [](DisplayStringIDType, UserDisplayString)
