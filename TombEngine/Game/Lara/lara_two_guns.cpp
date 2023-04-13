89f2811b69660ac4e3e75601a4aff9f8e0dbe839
Sezz <sezzary@outlook.com>
2023-04-13 23:15:43 +1000

    Add GetWeaponAnimData()

--- TombEngine/Game/Lara/lara_two_guns.cpp
+++ TombEngine/Game/Lara/lara_two_guns.cpp
@@ -12,6 +12,7 @@
 #include "Game/Lara/lara_struct.h"
 #include "Game/savegame.h"
 #include "Math/Math.h"
+#include "Objects/game_object_ids.h"
 #include "Sound/sound.h"
 #include "Specific/Input/Input.h"
 #include "Specific/level.h"
@@ -20,22 +21,30 @@
 using namespace TEN::Input;
 using namespace TEN::Math;
 
-struct WeaponDef
+struct WeaponAnimData
 {
-	int ObjectNumber = 0;
-	int Draw1Anim2	 = 0;
-	int Draw1Anim	 = 0;
-	int Draw2Anim	 = 0;
-	int RecoilAnim	 = 0;
+	GAME_OBJECT_ID ObjectID = GAME_OBJECT_ID::ID_NO_OBJECT;
+
+	int Draw1Anim2 = 0;
+	int Draw1Anim  = 0;
+	int Draw2Anim  = 0;
+	int RecoilAnim = 0;
 };
 
-const WeaponDef WeaponDefs[4] =
+static WeaponAnimData GetWeaponAnimData(LaraWeaponType weaponType)
 {
-	{ ID_LARA, 0, 0, 0, 0 },
-	{ ID_PISTOLS_ANIM, 4, 5, 13, 24 },
-	{ ID_REVOLVER_ANIM , 7, 8, 15, 29 },
-	{ ID_UZI_ANIM, 4, 5, 13, 24 }
-};
+	static const auto DEFAULT_WEAPON_ANIM_DATA = WeaponAnimData{ ID_LARA, 0, 0, 0, 0 };
+	static const auto ANIM_DATA_MAP			   = std::unordered_map<LaraWeaponType, WeaponAnimData>
+	{
+		{ LaraWeaponType::None, WeaponAnimData{ ID_LARA, 0, 0, 0, 0 } },
+		{ LaraWeaponType::Pistol, WeaponAnimData{ ID_PISTOLS_ANIM, 4, 5, 13, 24 } },
+		{ LaraWeaponType::Revolver, WeaponAnimData{ ID_REVOLVER_ANIM , 7, 8, 15, 29 } },
+		{ LaraWeaponType::Uzi, WeaponAnimData{ ID_UZI_ANIM, 4, 5, 13, 24 } }
+	};
+
+	auto it = ANIM_DATA_MAP.find(weaponType);
+	return ((it != ANIM_DATA_MAP.end()) ? it->second : DEFAULT_WEAPON_ANIM_DATA);
+}
 
 static Vector3i GetWeaponSmokeRelOffset(LaraWeaponType weaponType, bool isRightWeapon)
 {
@@ -58,19 +67,19 @@ static Vector3i GetWeaponSmokeRelOffset(LaraWeaponType weaponType, bool isRightW
 static void SetArmInfo(const ItemInfo& laraItem, ArmInfo& arm, int frame)
 {
 	const auto& player = GetLaraInfo(laraItem);
-	const auto& weaponDef = WeaponDefs[(int)player.Control.Weapon.GunType];
+	const auto& weaponAnimData = GetWeaponAnimData(player.Control.Weapon.GunType);
 
-	int animBase = Objects[(int)weaponDef.ObjectNumber].animIndex;
+	int animBase = Objects[(int)weaponAnimData.ObjectID].animIndex;
 
-	if (frame < weaponDef.Draw1Anim)
+	if (frame < weaponAnimData.Draw1Anim)
 	{
 		arm.AnimNumber = animBase;
 	}
-	else if (frame < weaponDef.Draw2Anim)
+	else if (frame < weaponAnimData.Draw2Anim)
 	{
 		arm.AnimNumber = animBase + 1;
 	}
-	else if (frame < weaponDef.RecoilAnim)
+	else if (frame < weaponAnimData.RecoilAnim)
 	{
 		arm.AnimNumber = animBase + 2;
 	}
@@ -105,7 +114,7 @@ static void AnimateWeapon(ItemInfo& laraItem, LaraWeaponType weaponType, bool& h
 	auto& arm = isRightWeapon ? player.RightArm : player.LeftArm;
 	auto& uziBool = isRightWeapon ? player.Control.Weapon.UziRight : player.Control.Weapon.UziLeft;
 	const auto& weapon = Weapons[(int)weaponType];
-	const auto& weaponDef = WeaponDefs[(int)player.Control.Weapon.GunType];
+	const auto& weaponAnimData = GetWeaponAnimData(player.Control.Weapon.GunType);
 
 	// Spawn weapon smoke.
 	if (laraItem.MeshBits.TestAny() && arm.GunSmoke)
@@ -121,12 +130,12 @@ static void AnimateWeapon(ItemInfo& laraItem, LaraWeaponType weaponType, bool& h
 	if ((IsHeld(In::Action) && !player.TargetEntity) || arm.Locked)
 	{
 		// At or beyond DRAW_END (2) start frame AND before SHOOT_START (0) end frame; increment toward SHOOT_START (0) end frame.
-		if (frame >= 0 && frame < weaponDef.Draw1Anim2)
+		if (frame >= 0 && frame < weaponAnimData.Draw1Anim2)
 		{
 			frame++;
 		}
 		// At SHOOT_START (0) end frame.
-		else if (frame == weaponDef.Draw1Anim2)
+		else if (frame == weaponAnimData.Draw1Anim2)
 		{
 			// Shoot weapon.
 			if (IsHeld(In::Action))
@@ -162,7 +171,7 @@ static void AnimateWeapon(ItemInfo& laraItem, LaraWeaponType weaponType, bool& h
 				}
 
 				// Go to SHOOT_CONTINUE (3) start frame.
-				frame = weaponDef.RecoilAnim;
+				frame = weaponAnimData.RecoilAnim;
 			}
 			else
 			{
@@ -174,7 +183,7 @@ static void AnimateWeapon(ItemInfo& laraItem, LaraWeaponType weaponType, bool& h
 			}
 		}
 		// At or beyond SHOOT_CONTINUE (3) start frame; increment toward SHOOT_CONTINUE (3) end frame to finish recoil before allowing to shoot again.
-		else if (frame >= weaponDef.RecoilAnim)
+		else if (frame >= weaponAnimData.RecoilAnim)
 		{
 			if (weaponType == LaraWeaponType::Uzi)
 			{
@@ -185,24 +194,24 @@ static void AnimateWeapon(ItemInfo& laraItem, LaraWeaponType weaponType, bool& h
 			frame++;
 
 			// At SHOOT_CONTINUE (3) end frame; go to START_SHOOT (0) end frame.
-			if (frame == (weaponDef.RecoilAnim + weapon.RecoilFrame))
-				frame = weaponDef.Draw1Anim2;
+			if (frame == (weaponAnimData.RecoilAnim + weapon.RecoilFrame))
+				frame = weaponAnimData.Draw1Anim2;
 		}
 	}
 	// Wind animation backward.
 	else
 	{
 		// Let SHOOT_CONTINUE (3) finish.
-		if (frame >= weaponDef.RecoilAnim && frame < (weaponDef.RecoilAnim + weapon.RecoilFrame))
+		if (frame >= weaponAnimData.RecoilAnim && frame < (weaponAnimData.RecoilAnim + weapon.RecoilFrame))
 			frame++;
 
 		// At SHOOT_CONTINUE (3) end frame; go to START_SHOOT (0) end frame.
-		if (frame == (weaponDef.RecoilAnim + weapon.RecoilFrame))
+		if (frame == (weaponAnimData.RecoilAnim + weapon.RecoilFrame))
 		{
-			frame = weaponDef.Draw1Anim2;
+			frame = weaponAnimData.Draw1Anim2;
 		}
 		// Go back to "ready" stance.
-		else if ((frame > 0) && (frame <= weaponDef.Draw1Anim2))
+		else if ((frame > 0) && (frame <= weaponAnimData.Draw1Anim2))
 		{
 			frame--;
 		}
@@ -222,19 +231,19 @@ static int AnimateWeaponUndraw(ItemInfo& laraItem, LaraWeaponType weaponType, bo
 	auto& player = GetLaraInfo(laraItem);
 	auto& arm = isRightWeapon ? player.RightArm : player.LeftArm;
 	const auto& weapon = Weapons[(int)weaponType];
-	const auto& weaponDef = WeaponDefs[(int)player.Control.Weapon.GunType];
+	const auto& weaponAnimData = GetWeaponAnimData(player.Control.Weapon.GunType);
 
 	int frame = arm.FrameNumber;
 
 	// Finish recoil anim before reholstering weapon.
-	if (frame >= weaponDef.RecoilAnim && frame < (weaponDef.RecoilAnim + weapon.RecoilFrame))
+	if (frame >= weaponAnimData.RecoilAnim && frame < (weaponAnimData.RecoilAnim + weapon.RecoilFrame))
 		frame++;
 
-	if (frame == (weaponDef.RecoilAnim + weapon.RecoilFrame))
+	if (frame == (weaponAnimData.RecoilAnim + weapon.RecoilFrame))
 	{
-		frame = weaponDef.Draw1Anim2;
+		frame = weaponAnimData.Draw1Anim2;
 	}
-	else if (frame > 0 && frame < weaponDef.Draw1Anim)
+	else if (frame > 0 && frame < weaponAnimData.Draw1Anim)
 	{
 		arm.Orientation -= arm.Orientation / frame;
 		frame--;
@@ -242,13 +251,13 @@ static int AnimateWeaponUndraw(ItemInfo& laraItem, LaraWeaponType weaponType, bo
 	else if (frame == 0)
 	{
 		arm.Orientation = EulerAngles::Zero;
-		frame = weaponDef.RecoilAnim - 1;
+		frame = weaponAnimData.RecoilAnim - 1;
 	}
-	else if (frame > weaponDef.Draw1Anim && frame < weaponDef.RecoilAnim)
+	else if (frame > weaponAnimData.Draw1Anim && frame < weaponAnimData.RecoilAnim)
 	{
 		frame--;
 
-		if (frame == (weaponDef.Draw2Anim - 1))
+		if (frame == (weaponAnimData.Draw2Anim - 1))
 		{
 			UndrawPistolMesh(laraItem, weaponType, isRightWeapon);
 			SoundEffect(SFX_TR4_LARA_HOLSTER, &laraItem.Pose);
@@ -325,20 +334,20 @@ void AnimatePistols(ItemInfo& laraItem, LaraWeaponType weaponType)
 void DrawPistols(ItemInfo& laraItem, LaraWeaponType weaponType)
 {
 	auto& player = GetLaraInfo(laraItem);
-	const auto& weaponDef = WeaponDefs[(int)player.Control.Weapon.GunType];
+	const auto& weaponAnimData = GetWeaponAnimData(player.Control.Weapon.GunType);
 
 	int frame = player.LeftArm.FrameNumber + 1;
 
-	if (frame < weaponDef.Draw1Anim || frame > (weaponDef.RecoilAnim - 1))
+	if (frame < weaponAnimData.Draw1Anim || frame > (weaponAnimData.RecoilAnim - 1))
 	{
-		frame = weaponDef.Draw1Anim;
+		frame = weaponAnimData.Draw1Anim;
 	}
-	else if (frame == weaponDef.Draw2Anim)
+	else if (frame == weaponAnimData.Draw2Anim)
 	{
 		DrawPistolMeshes(laraItem, weaponType);
 		SoundEffect(SFX_TR4_LARA_DRAW, &laraItem.Pose);
 	}
-	else if (frame == (weaponDef.RecoilAnim - 1))
+	else if (frame == (weaponAnimData.RecoilAnim - 1))
 	{
 		ReadyPistols(laraItem, weaponType);
 		frame = 0;
@@ -351,13 +360,13 @@ void DrawPistols(ItemInfo& laraItem, LaraWeaponType weaponType)
 void UndrawPistols(ItemInfo& laraItem, LaraWeaponType weaponType)
 {
 	auto& player = GetLaraInfo(laraItem);
-	const auto& weaponDef = WeaponDefs[(int)player.Control.Weapon.GunType];
+	const auto& weaponAnimData = GetWeaponAnimData(player.Control.Weapon.GunType);
 
 	int frameLeft = AnimateWeaponUndraw(laraItem, weaponType, false);
 	int frameRight = AnimateWeaponUndraw(laraItem, weaponType, true);
 
-	if (frameLeft == weaponDef.Draw1Anim &&
-		frameRight == weaponDef.Draw1Anim)
+	if (frameLeft == weaponAnimData.Draw1Anim &&
+		frameRight == weaponAnimData.Draw1Anim)
 	{
 		player.Control.HandStatus = HandStatus::Free;
 		player.TargetEntity = nullptr;
