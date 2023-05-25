8db97e3b8be6c0199094c48b8e031948c5c12c3e
Sezz <sezzary@outlook.com>
2023-05-25 14:37:59 +1000

    Adjust fake epsilon default in EulerAngles class

--- TombEngine/Math/Objects/EulerAngles.h
+++ TombEngine/Math/Objects/EulerAngles.h
@@ -23,9 +23,9 @@
 				  EulerAngles(const Matrix& rotMatrix);
 
 		// Utilities
-		static bool		   Compare(const EulerAngles& eulers0, const EulerAngles& eulers1, short epsilon = 2);
-		void			   Lerp(const EulerAngles& eulersTo, float alpha = 1.0f, short epsilon = 2);
-		static EulerAngles Lerp(const EulerAngles& eulersFrom, const EulerAngles& eulersTo, float alpha = 1.0f, short epsilon = 2);
+		static bool		   Compare(const EulerAngles& eulers0, const EulerAngles& eulers1, short epsilon = 3);
+		void			   Lerp(const EulerAngles& eulersTo, float alpha = 1.0f, short epsilon = 3);
+		static EulerAngles Lerp(const EulerAngles& eulersFrom, const EulerAngles& eulersTo, float alpha = 1.0f, short epsilon = 3);
 		void			   Slerp(const EulerAngles& eulersTo, float alpha = 1.0f);
 		static EulerAngles Slerp(const EulerAngles& eulersFrom, const EulerAngles& eulersTo, float alpha = 1.0f);
 		void			   InterpolateConstant(const EulerAngles& eulersTo, short angularVel);
@@ -55,8 +55,8 @@
 	private:
 		// Temporary. Will be integrated into eventual Angle class.
 		static float ClampAlpha(float alpha);
-		static bool	 Compare(short angle0, short angle1, short epsilon = 2);
-		static short Lerp(short angleFrom, short angleTo, float alpha = 1.0f, short epsilon = 2);
+		static bool	 Compare(short angle0, short angle1, short epsilon = 3);
+		static short Lerp(short angleFrom, short angleTo, float alpha = 1.0f, short epsilon = 3);
 		static short InterpolateConstant(short angleFrom, short angleTo, short angularVel);
 	};
 //}
