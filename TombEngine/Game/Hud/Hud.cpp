a480d04cf21d9b1b7cfa3a69b7af15cb054dcc14
Sezz <sezzary@outlook.com>
2023-05-27 01:30:08 +1000

    Remove debug code

--- TombEngine/Game/Hud/Hud.cpp
+++ TombEngine/Game/Hud/Hud.cpp
@@ -7,11 +7,6 @@
 
 using namespace TEN::Math;
 
-
-// DEBUG
-#include "Renderer11.h"
-using TEN::Renderer::g_Renderer;
-
 namespace TEN::Hud
 {
 	HudController g_Hud = {};
@@ -24,17 +19,6 @@ namespace TEN::Hud
 
 	void HudController::Draw(const ItemInfo& item) const
 	{
-		// ----------------Debug
-		static short orient2D = 0;
-		orient2D += ANGLE(1.0f);
-
-		auto pos = SCREEN_SPACE_RES / 3;
-		g_Renderer.DrawSpriteIn2DSpace(
-			ID_MISC_SPRITES, 1,
-			pos, 0, Vector4(1.0f, 1.0f, 1.0f, 0.5f), Vector2(300.0f));
-
-		//------------------
-
 		PickupSummary.Draw();
 		StatusBars.Draw(item);
 	}
