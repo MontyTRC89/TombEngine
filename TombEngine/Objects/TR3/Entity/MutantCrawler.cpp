2617a365bbb1f1796eb95d9f25747368f74bf26e
Kubsy <kubadd475@gmail.com>
2023-06-08 19:25:51 +0100

    progress.

--- TombEngine/Objects/TR3/Entity/MutantCrawler.cpp
+++ TombEngine/Objects/TR3/Entity/MutantCrawler.cpp
@@ -41,17 +41,20 @@ namespace TEN::Entities::Creatures::TR3
 
 		auto* item = &g_Level.Items[itemNumber];
 		auto& creature = *GetCreatureInfo(item);
+		const auto& anim = GetAnimData(item);
+
+		long c;
+
+		Vector3i pos;
 
 		if (item->ItemFlags[0] > 80)
 			item->HitPoints = 0;
 
 		if (item->HitPoints <= 0)
 		{
-			if (item->Animation.ActiveState != 3)
+			if (item->Animation.ActiveState != MUTANT_CRAWLER_STATE_DEATH)
 			{
-				item->Animation.AnimNumber = Objects[item->ObjectNumber].animIndex + 5;
-				item->Animation.FrameNumber = GetAnimData(item).frameBase;
-				item->Animation.ActiveState = 3;
+				SetAnimation(item, MUTANT_CRAWLER_ANIM_DEATH);
 				creature.Flags = 0;
 			}
 
@@ -59,12 +62,33 @@ namespace TEN::Entities::Creatures::TR3
 			{
 				for (int i = 9; i < 17; i++)
 				{
-					getJoi
+					pos.x = 0;
+					pos.y = 0;
+					pos.z = 0;
+					GetJointPosition(item, i, pos);
+					//TriggerFireFlame(pos.x, pos.y, pos.z, -1, 255); not sure how to translate this
 				}
+
+				c = anim.frameEnd - GetAnimData(item).frameBase;
+
+				if (c > 16)
+				{
+					c = anim.frameEnd - item->Animation.FrameNumber;
+
+					if (c > 16)
+						c = 16;
+				}
+
+				/* No idea how to translate this.
+				b = GetRandomControl();
+				r = (c * (255 - ((b >> 4) & 0x1F))) >> 4;
+				g = (c * (192 - ((b >> 6) & 0x3F))) >> 4;
+				b = (c * (b & 0x3F)) >> 4;
+				TriggerDynamic(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos, 12, r, g, b);
+				*/ 
 			}
 
+			else if (item->Animation.FrameNumber >= )
 		}
-
 	}
-
 }
