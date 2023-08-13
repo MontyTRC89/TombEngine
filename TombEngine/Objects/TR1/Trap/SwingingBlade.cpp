d4ed3c90d6057199c80425151e45642d7651b8f5
Sezz <sezzary@outlook.com>
2023-08-13 15:46:19 +1000

    Update

--- TombEngine/Objects/TR1/Trap/SwingingBlade.cpp
+++ TombEngine/Objects/TR1/Trap/SwingingBlade.cpp
@@ -5,18 +5,20 @@
 
 namespace TEN::Entities::Traps::TR1
 {
-	constexpr auto SWINGING_BLADE_DAMAGE = 100;
+	constexpr auto SWINGING_BLADE_HARM_DAMAGE = 100;
 
-	enum SlammingDoorsState
+	const std::vector<unsigned int> SwingingBladeHarmJoints = { 1, 2 };
+
+	enum SwingingBladeState
 	{
-		SWINGING_BLADE_DISABLED = 0,
-		SWINGING_BLADE_ENABLED = 2
+		SWINGING_BLADE_STATE_DISABLED = 0,
+		SWINGING_BLADE_STATE_ENABLED = 2
 	};
 
-	enum SlammingDoorsAnim
+	enum SwingingBladeAnim
 	{
 		SWINGING_BLADE_ANIM_DISABLED = 0,
-		SWINGING_BLADE_ANIM_DEACTIVATION = 1,
+		SWINGING_BLADE_ANIM_DEACTIVATING = 1,
 		SWINGING_BLADE_ANIM_ENABLED = 2
 	};
 
@@ -25,13 +27,9 @@ namespace TEN::Entities::Traps::TR1
 		auto& item = g_Level.Items[itemNumber];
 
 		SetAnimation(&item, SWINGING_BLADE_ANIM_DISABLED);
-
-		// Used by GenericSphereBoxCollision. Bits correspond to joint damage index.
-		// 6 = 000000000 000000110, so damage joints are bit 1 (pole) and bit 2 (blade)
-		item.ItemFlags[0] = 0;
-
-		// Used by GenericSphereBoxCollision for trap damage value.
-		item.ItemFlags[3] = SWINGING_BLADE_DAMAGE;
+		
+		// Used for damage in GenericSphereBoxCollision().
+		item.ItemFlags[3] = SWINGING_BLADE_HARM_DAMAGE;
 	}
 
 	void ControlSwingingBlade(short itemNumber)
@@ -40,17 +38,23 @@ namespace TEN::Entities::Traps::TR1
 
 		if (TriggerActive(&item))
 		{
-			if (item.Animation.TargetState != SWINGING_BLADE_ANIM_ENABLED)
+			if (item.Animation.TargetState != SWINGING_BLADE_STATE_ENABLED)
 			{
-				item.Animation.TargetState = SWINGING_BLADE_ANIM_ENABLED;
-				item.ItemFlags[0] = 6;
+				item.Animation.TargetState = SWINGING_BLADE_STATE_ENABLED;
+
+				// Set harm joints.
+				auto bitField = BitField::Default;
+				bitField.Set(SwingingBladeHarmJoints);
+				item.ItemFlags[0] = bitField.ToPackedBits();
 			}
 		}
 		else
 		{
-			if (item.Animation.TargetState != SWINGING_BLADE_ANIM_DISABLED)
+			if (item.Animation.TargetState != SWINGING_BLADE_STATE_DISABLED)
 			{
-				item.Animation.TargetState = SWINGING_BLADE_ANIM_DISABLED;
+				item.Animation.TargetState = SWINGING_BLADE_STATE_DISABLED;
+
+				// Unset harm joints.
 				item.ItemFlags[0] = 0;
 			}
 		}
