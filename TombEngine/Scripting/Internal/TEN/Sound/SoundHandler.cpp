56c1e9d11e9609337f20061562694eec0dead688
davidmarr <116632612+davidmarr@users.noreply.github.com>
2023-10-05 22:41:03 +0200

    Update SoundHandler.cpp
    tableMisc variable renamed to tableSound

--- TombEngine/Scripting/Internal/TEN/Sound/SoundHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/Sound/SoundHandler.cpp
@@ -75,59 +75,59 @@ namespace Sound
 
 	void Register(sol::state* state, sol::table& parent)
 	{
-		sol::table tableMisc{ state->lua_state(), sol::create };
-		parent.set(ScriptReserved_Sound, tableMisc);
+		sol::table tableSound{ state->lua_state(), sol::create };
+		parent.set(ScriptReserved_Sound, tableSound);
 
 		/// Play an audio track
 		//@function PlayAudioTrack
 		//@tparam string name of track (without file extension) to play
 		//@tparam Sound.SoundTrackType type of the audio track to play
-		tableMisc.set_function(ScriptReserved_PlayAudioTrack, &PlayAudioTrack);
+		tableSound.set_function(ScriptReserved_PlayAudioTrack, &PlayAudioTrack);
 
 		///Set and play an ambient track
 		//@function SetAmbientTrack
 		//@tparam string name of track (without file extension) to play
-		tableMisc.set_function(ScriptReserved_SetAmbientTrack, &SetAmbientTrack);
+		tableSound.set_function(ScriptReserved_SetAmbientTrack, &SetAmbientTrack);
 
 		///Stop any audio tracks currently playing
 		//@function StopAudioTracks
-		tableMisc.set_function(ScriptReserved_StopAudioTracks, &StopAudioTracks);
+		tableSound.set_function(ScriptReserved_StopAudioTracks, &StopAudioTracks);
 
 		///Stop audio track that is currently playing
 		//@function StopAudioTrack
 		//@tparam Misc.SoundTrackType type of the audio track
-		tableMisc.set_function(ScriptReserved_StopAudioTrack, &StopAudioTrack);
+		tableSound.set_function(ScriptReserved_StopAudioTrack, &StopAudioTrack);
 
 		///Get current loudness level for specified track type
 		//@function GetAudioTrackLoudness
 		//@tparam Misc.SoundTrackType type of the audio track
 		//@treturn float current loudness of a specified audio track
-		tableMisc.set_function(ScriptReserved_GetAudioTrackLoudness, &GetAudioTrackLoudness);
+		tableSound.set_function(ScriptReserved_GetAudioTrackLoudness, &GetAudioTrackLoudness);
 
 		/// Play sound effect
 		//@function PlaySound
 		//@tparam int sound ID to play. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
 		////@tparam[opt] Vec3 position The 3D position of the sound, i.e. where the sound "comes from". If not given, the sound will not be positional.
-		tableMisc.set_function(ScriptReserved_PlaySound, &PlaySoundEffect);
+		tableSound.set_function(ScriptReserved_PlaySound, &PlaySoundEffect);
 
 		/// Check if the sound effect is playing
 		//@function IsSoundPlaying
 		//@tparam int Sound ID to check. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
-		tableMisc.set_function(ScriptReserved_IsSoundPlaying, &IsSoundPlaying);
+		tableSound.set_function(ScriptReserved_IsSoundPlaying, &IsSoundPlaying);
 
 		/// Check if the audio track is playing
 		//@function IsAudioTrackPlaying
 		//@tparam string Track filename to check. Should be without extension and without full directory path.
-		tableMisc.set_function(ScriptReserved_IsAudioTrackPlaying, &IsAudioTrackPlaying);
+		tableSound.set_function(ScriptReserved_IsAudioTrackPlaying, &IsAudioTrackPlaying);
 
 		///Get current subtitle string for a voice track currently playing.
 		//Subtitle file must be in .srt format, have same filename as voice track, and be placed in same directory as voice track.
 		//Returns nil if no voice track is playing or no subtitle present.
 		//@function GetCurrentSubtitle
 		//@treturn string current subtitle string
-		tableMisc.set_function(ScriptReserved_GetCurrentSubtitle, &GetCurrentVoiceTrackSubtitle);
+		tableSound.set_function(ScriptReserved_GetCurrentSubtitle, &GetCurrentVoiceTrackSubtitle);
 
 		LuaHandler handler{ state };
-		handler.MakeReadOnlyTable(tableMisc, ScriptReserved_SoundTrackType, SOUNDTRACK_TYPE);
+		handler.MakeReadOnlyTable(tableSound, ScriptReserved_SoundTrackType, SOUNDTRACK_TYPE);
 	}
 };
