da2eed4b186a4fc0b7b7948bccd868793d62fdc4
Sezz <sezzary@outlook.com>
2023-10-06 14:16:54 +1100

    Formatting

--- TombEngine/Scripting/Internal/ScriptInterfaceState.cpp
+++ TombEngine/Scripting/Internal/ScriptInterfaceState.cpp
@@ -13,8 +13,8 @@
 
 using namespace TEN::Scripting::DisplaySprite;
 
-static sol::state s_solState;
-static sol::table s_rootTable;
+static sol::state SolState;
+static sol::table RootTable;
 
 int lua_exception_handler(lua_State* luaStatePtr, sol::optional<const std::exception&> exception, sol::string_view description)
 {
@@ -23,40 +23,40 @@ int lua_exception_handler(lua_State* luaStatePtr, sol::optional<const std::excep
 
 ScriptInterfaceGame* ScriptInterfaceState::CreateGame()
 {
-	return new LogicHandler(&s_solState, s_rootTable);
+	return new LogicHandler(&SolState, RootTable);
 }
 
 ScriptInterfaceFlowHandler* ScriptInterfaceState::CreateFlow()
 {
-	return new FlowHandler(&s_solState, s_rootTable);
+	return new FlowHandler(&SolState, RootTable);
 }
 
 ScriptInterfaceObjectsHandler* ScriptInterfaceState::CreateObjectsHandler()
 {
-	return new ObjectsHandler(&s_solState, s_rootTable);
+	return new ObjectsHandler(&SolState, RootTable);
 }
 
 ScriptInterfaceStringsHandler* ScriptInterfaceState::CreateStringsHandler()
 {
-	return new StringsHandler(&s_solState, s_rootTable);
+	return new StringsHandler(&SolState, RootTable);
 }
 
 void ScriptInterfaceState::Init(const std::string& assetsDir)
 {
-	s_solState.open_libraries(
+	SolState.open_libraries(
 		sol::lib::base, sol::lib::math, sol::lib::package, sol::lib::coroutine,
 		sol::lib::table, sol::lib::string, sol::lib::debug);
 
-	s_solState.script("package.path=\"" + assetsDir + "Scripts/?.lua\"");
-	s_solState.set_exception_handler(lua_exception_handler);
+	SolState.script("package.path=\"" + assetsDir + "Scripts/?.lua\"");
+	SolState.set_exception_handler(lua_exception_handler);
 
-	s_rootTable = sol::table{ s_solState.lua_state(), sol::create };
-	s_solState.set(ScriptReserved_TEN, s_rootTable);
+	RootTable = sol::table(SolState.lua_state(), sol::create);
+	SolState.set(ScriptReserved_TEN, RootTable);
 
 	// Misc. handlers not assigned above.
-	InventoryHandler::Register(&s_solState, s_rootTable);
-	Misc::Register(&s_solState, s_rootTable);
-	Effects::Register(&s_solState, s_rootTable);
+	InventoryHandler::Register(&SolState, RootTable);
+	Misc::Register(&SolState, RootTable);
+	Effects::Register(&SolState, RootTable);
 
-	ScriptDisplaySprite::RegisterTables(&s_solState, s_rootTable);
+	ScriptDisplaySprite::RegisterTables(&SolState, RootTable);
 }
