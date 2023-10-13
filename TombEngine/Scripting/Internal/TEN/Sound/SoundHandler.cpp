5ca937d337cfdea44283e893e2d26da052de6c3c
davidmarr <116632612+davidmarr@users.noreply.github.com>
2023-10-13 17:13:16 +0200

    moved LDoc comments above the respective functions
    moved LDoc comments above the respective functions

--- TombEngine/Scripting/Internal/TEN/Sound/SoundHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/Sound/SoundHandler.cpp
@@ -16,54 +16,88 @@ Functions to manage sounds.
 
 namespace Sound
 {
+	/// Play an audio track
+	//@function PlayAudioTrack
+	//@tparam string name of track (without file extension) to play
+	//@tparam Sound.SoundTrackType type of the audio track to play
 	static void PlayAudioTrack(const std::string& trackName, TypeOrNil<SoundTrackType> mode)
 	{
 		auto playMode = USE_IF_HAVE(SoundTrackType, mode, SoundTrackType::OneShot);
 		PlaySoundTrack(trackName, playMode);
 	}
 
+	///Set and play an ambient track
+	//@function SetAmbientTrack
+	//@tparam string name of track (without file extension) to play
 	static void SetAmbientTrack(const std::string& trackName)
 	{
 		PlaySoundTrack(trackName, SoundTrackType::BGM);
 	}
 
+	///Stop any audio tracks currently playing
+	//@function StopAudioTracks
 	static void StopAudioTracks()
 	{
 		StopSoundTracks();
 	}
 
+	///Stop audio track that is currently playing
+	//@function StopAudioTrack
+	//@tparam Misc.SoundTrackType type of the audio track
 	static void StopAudioTrack(TypeOrNil<SoundTrackType> mode)
 	{
 		auto playMode = USE_IF_HAVE(SoundTrackType, mode, SoundTrackType::OneShot);
 		StopSoundTrack(playMode, SOUND_XFADETIME_ONESHOT);
 	}
 
+	///Get current loudness level for specified track type
+	//@function GetAudioTrackLoudness
+	//@tparam Misc.SoundTrackType type of the audio track
+	//@treturn float current loudness of a specified audio track
 	static float GetAudioTrackLoudness(TypeOrNil<SoundTrackType> mode)
 	{
 		auto playMode = USE_IF_HAVE(SoundTrackType, mode, SoundTrackType::OneShot);
 		return GetSoundTrackLoudness(playMode);
 	}
 
+	/// Play sound effect
+	//@function PlaySound
+	//@tparam int sound ID to play. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
+	////@tparam[opt] Vec3 position The 3D position of the sound, i.e. where the sound "comes from". If not given, the sound will not be positional.
 	static void PlaySoundEffect(int id, sol::optional<Vec3> p)
 	{
 		SoundEffect(id, p.has_value() ? &Pose(p.value().x, p.value().y, p.value().z) : nullptr, SoundEnvironment::Always);
 	}
 
+	/// Stop sound effect
+	//@function StopSound
+	//@tparam int sound ID to play. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
 	static void StopSound(int id)
 	{
 		StopSoundEffect(id);
 	}
 
+	/// Check if the sound effect is playing
+	//@function IsSoundPlaying
+	//@tparam int Sound ID to check. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
 	static bool IsSoundPlaying(int effectID)
 	{
 		return (Sound_EffectIsPlaying(effectID, nullptr) != SOUND_NO_CHANNEL);
 	}
 
+	/// Check if the audio track is playing
+	//@function IsAudioTrackPlaying
+	//@tparam string Track filename to check. Should be without extension and without full directory path.
 	static bool IsAudioTrackPlaying(const std::string& trackName)
 	{
 		return Sound_TrackIsPlaying(trackName);
 	}
 
+	///Get current subtitle string for a voice track currently playing.
+	//Subtitle file must be in .srt format, have same filename as voice track, and be placed in same directory as voice track.
+	//Returns nil if no voice track is playing or no subtitle present.
+	//@function GetCurrentSubtitle
+	//@treturn string current subtitle string
 	static TypeOrNil<std::string> GetCurrentVoiceTrackSubtitle()
 	{
 		auto& result = GetCurrentSubtitle();
@@ -83,58 +117,15 @@ namespace Sound
 		sol::table tableSound{ state->lua_state(), sol::create };
 		parent.set(ScriptReserved_Sound, tableSound);
 
-		/// Play an audio track
-		//@function PlayAudioTrack
-		//@tparam string name of track (without file extension) to play
-		//@tparam Sound.SoundTrackType type of the audio track to play
 		tableSound.set_function(ScriptReserved_PlayAudioTrack, &PlayAudioTrack);
-
-		///Set and play an ambient track
-		//@function SetAmbientTrack
-		//@tparam string name of track (without file extension) to play
 		tableSound.set_function(ScriptReserved_SetAmbientTrack, &SetAmbientTrack);
-
-		///Stop any audio tracks currently playing
-		//@function StopAudioTracks
 		tableSound.set_function(ScriptReserved_StopAudioTracks, &StopAudioTracks);
-
-		///Stop audio track that is currently playing
-		//@function StopAudioTrack
-		//@tparam Misc.SoundTrackType type of the audio track
 		tableSound.set_function(ScriptReserved_StopAudioTrack, &StopAudioTrack);
-
-		///Get current loudness level for specified track type
-		//@function GetAudioTrackLoudness
-		//@tparam Misc.SoundTrackType type of the audio track
-		//@treturn float current loudness of a specified audio track
 		tableSound.set_function(ScriptReserved_GetAudioTrackLoudness, &GetAudioTrackLoudness);
-
-		/// Play sound effect
-		//@function PlaySound
-		//@tparam int sound ID to play. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
-		////@tparam[opt] Vec3 position The 3D position of the sound, i.e. where the sound "comes from". If not given, the sound will not be positional.
 		tableSound.set_function(ScriptReserved_PlaySound, &PlaySoundEffect);
-
-		/// Stop sound effect
-		//@function StopSound
-		//@tparam int sound ID to play. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
 		tableSound.set_function(ScriptReserved_StopSound, &StopSound);
-
-		/// Check if the sound effect is playing
-		//@function IsSoundPlaying
-		//@tparam int Sound ID to check. Corresponds to the value in the sound XML file or Tomb Editor's "Sound Infos" window.
 		tableSound.set_function(ScriptReserved_IsSoundPlaying, &IsSoundPlaying);
-
-		/// Check if the audio track is playing
-		//@function IsAudioTrackPlaying
-		//@tparam string Track filename to check. Should be without extension and without full directory path.
 		tableSound.set_function(ScriptReserved_IsAudioTrackPlaying, &IsAudioTrackPlaying);
-
-		///Get current subtitle string for a voice track currently playing.
-		//Subtitle file must be in .srt format, have same filename as voice track, and be placed in same directory as voice track.
-		//Returns nil if no voice track is playing or no subtitle present.
-		//@function GetCurrentSubtitle
-		//@treturn string current subtitle string
 		tableSound.set_function(ScriptReserved_GetCurrentSubtitle, &GetCurrentVoiceTrackSubtitle);
 
 		LuaHandler handler{ state };
