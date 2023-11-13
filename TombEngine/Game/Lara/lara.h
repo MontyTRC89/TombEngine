bd26ea05a7c6569899b11942d757718a7423e6a9
Sezz <sezzary@outlook.com>
2023-11-13 12:27:29 +1100

    Update lara.h

--- TombEngine/Game/Lara/lara.h
+++ TombEngine/Game/Lara/lara.h
@@ -56,7 +56,7 @@ constexpr auto LARA_ALIGN_VELOCITY	  = 12; // TODO: Float.
 constexpr auto LARA_FREEFALL_VELOCITY	= 126.0f;
 constexpr auto LARA_DAMAGE_VELOCITY		= 141.0f;
 constexpr auto LARA_DEATH_VELOCITY		= 155.0f;
-constexpr auto LARA_DIVE_DEATH_VELOCITY	= 134.0f;
+constexpr auto LARA_DIVE_DEATH_VELOCITY = 134.0f;
 constexpr auto LARA_TERMINAL_VELOCITY	= CLICK(10);
 
 constexpr auto LARA_SWIM_VELOCITY_ACCEL		   = 2.0f;
