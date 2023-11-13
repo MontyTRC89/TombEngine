3d01a2ce5a9bd2f6e27161e42bf7b5931906e488
Sezz <sezzary@outlook.com>
2023-11-13 12:31:14 +1100

    Fix comment and math

--- TombEngine/Game/camera.cpp
+++ TombEngine/Game/camera.cpp
@@ -531,9 +531,9 @@ void ChaseCamera(ItemInfo* item)
 
 	auto probe = GetCollision(Camera.target.x, Camera.target.y + CLICK(1), Camera.target.z, Camera.target.RoomNumber);
 
+	// HACK: Prevent camera from shaking in swamps.
 	if (TestEnvironment(ENV_FLAG_SWAMP, probe.RoomNumber))
-		//HACK: I included an offset to avoid the camera shacking during the swamp.
-		Camera.target.y = g_Level.Rooms[probe.RoomNumber].y - CLICK(1) + BLOCK (1.4f);
+		Camera.target.y = g_Level.Rooms[probe.RoomNumber].y - CLICK(1.15f);
 
 	int y = Camera.target.y;
 	probe = GetCollision(Camera.target.x, y, Camera.target.z, Camera.target.RoomNumber);
