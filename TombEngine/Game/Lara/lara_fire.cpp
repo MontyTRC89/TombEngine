c03310c12d84d909a711a5645b732a8f34990aa7
Adngel <adngel@hotmail.com>
2023-11-14 18:47:03 +0100

    Update lara_fire.cpp

--- TombEngine/Game/Lara/lara_fire.cpp
+++ TombEngine/Game/Lara/lara_fire.cpp
@@ -793,7 +793,6 @@ void HandleWeapon(ItemInfo& laraItem)
 				player.Flare.ControlLeft = (player.Context.Vehicle != NO_ITEM || TestState(laraItem.Animation.ActiveState, FlarePoseStates));
 
 				//Test Animations Exceptions for flare pose
-
 				const auto currentLaraAnim = laraItem.Animation.AnimNumber;
 				for (const auto& currentExceptionAnim : FlarePoseAnimationExceptions)
 				{
