556c06332fc53a54ef70660d73f3139585fd26f9
Sezz <sezzary@outlook.com>
2023-12-18 19:16:40 +1100

    Fix AFK posing

--- TombEngine/Game/Lara/lara_basic.cpp
+++ TombEngine/Game/Lara/lara_basic.cpp
@@ -688,6 +688,7 @@ void lara_as_pose(ItemInfo* item, CollisionInfo* coll)
 		if (IsWakeActionHeld())
 		{
 			item->Animation.TargetState = LS_IDLE;
+			player.Control.Count.Pose = 0;
 			return;
 		}
 
@@ -696,6 +697,7 @@ void lara_as_pose(ItemInfo* item, CollisionInfo* coll)
 	}
 
 	item->Animation.TargetState = LS_IDLE;
+	player.Control.Count.Pose = 0;
 }
 
 // State:	  LS_RUN_BACK (5)
