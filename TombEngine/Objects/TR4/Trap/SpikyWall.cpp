d70d2acc1ea4082ad062012c81a9d596e01dd38f
Nemoel-Tomo <tomo_669@hotmail.com>
2024-02-05 22:16:34 +0100

    update spiky wall
    it explodes shatterable statics, kill enemies, stops at another spiky wall

--- TombEngine/Objects/TR4/Trap/SpikyWall.cpp
+++ TombEngine/Objects/TR4/Trap/SpikyWall.cpp
@@ -5,9 +5,11 @@
 #include "Game/collision/collide_room.h"
 #include "Game/collision/sphere.h"
 #include "Game/control/control.h"
+#include "Game/effects/debris.h"
 #include "Game/effects/effects.h"
 #include "Game/items.h"
 #include "Game/Lara/lara.h"
+#include "Game/Setup.h"
 #include "Math/Math.h"
 #include "Sound/sound.h"
 #include "Specific/level.h"
@@ -23,6 +25,7 @@ namespace TEN::Entities::Traps
 		auto& item = g_Level.Items[itemNumber];
 
 		item.ItemFlags[0] = item.TriggerFlags;
+		item.ItemFlags[1] = 0;
 	}
 
 	void ControlSpikyWall(short itemNumber)
@@ -33,16 +36,70 @@ namespace TEN::Entities::Traps
 			return;
 
 		item.ItemFlags[0] = item.TriggerFlags;
+		
+		if (item.ItemFlags[1] && 
+			item.ItemFlags[0])
+		{
+			item.ItemFlags[1] -= 1;
+		}
 
 		int forwardVel = item.ItemFlags[0];
 		auto bounds = GameBoundingBox(&item);
 
-		// Get point collision.
-		auto pointColl = GetCollision(&item, item.Pose.Orientation.y, (forwardVel >= 0) ? bounds.Z2 : bounds.Z1);
+		auto pointColl = GetCollision(&item, item.Pose.Orientation.y, (forwardVel >= 0) ? bounds.Z2 : bounds.Z1, bounds.Y2);
+		auto pointColl2 = GetCollision(&item, item.Pose.Orientation.y, (forwardVel >= 0) ? bounds.Z2 : bounds.Z1, bounds.Y2, (bounds.X2 - bounds.X1) /2);
+
+		if (GetCollidedObjects(&item, CLICK(1), true, CollidedItems, CollidedMeshes, true))
+		{
+			int lp = 0;
+			while (CollidedItems[lp] != nullptr)
+			{
+				if (Objects[CollidedItems[lp]->ObjectNumber].intelligent)
+				{
+					CollidedItems[lp]->HitPoints = 0;
+					
+				}
+				else if (CollidedItems[lp]->ObjectNumber == ID_SPIKY_WALL && !item.ItemFlags[1])
+				{
+					CollidedItems[lp]->TriggerFlags = 0;
+					item.TriggerFlags = 0;
+					item.Status = ITEM_DEACTIVATED;
+					StopSoundEffect(SFX_TR4_ROLLING_BALL);
+					item.ItemFlags[1] = 10;
+				}
+
+				lp++;
+			}
+		}
 
 		// Stop moving.
-		if (!item.TriggerFlags)
+		if (!item.TriggerFlags ||
+			pointColl.Block->IsWall(item.Pose.Position.x, item.Pose.Position.z) || 
+			pointColl.Block->Stopper ||
+			pointColl2.Block->IsWall(item.Pose.Position.x, item.Pose.Position.z) ||
+			pointColl2.Block->Stopper )
 		{
+			auto& room = g_Level.Rooms[item.RoomNumber];
+			auto pos = item.Pose.Position;
+
+			for (auto& mesh : room.mesh)
+			{
+				if ((abs(pointColl.Coordinates.x - mesh.pos.Position.x) < BLOCK(1) &&
+					abs(pointColl.Coordinates.z - mesh.pos.Position.z) < BLOCK(1)) ||
+					abs(pointColl2.Coordinates.x - mesh.pos.Position.x) < BLOCK(1) &&
+					abs(pointColl2.Coordinates.z - mesh.pos.Position.z) < BLOCK(1) &&
+					!(StaticObjects[mesh.staticNumber].shatterType == ShatterType::None))
+				{					
+					if (mesh.HitPoints == 0)
+					{
+						mesh.HitPoints -= 1;
+						ShatterObject(nullptr, &mesh, -64, LaraItem->RoomNumber, 0);
+						SoundEffect(SFX_TR4_SMASH_ROCK, &item.Pose);
+						TestTriggers(pos.x, pos.y, pos.z, item.RoomNumber, true);
+					}
+				}
+			}
+
 			item.Status = ITEM_DEACTIVATED;
 			StopSoundEffect(SFX_TR4_ROLLING_BALL);
 		}
@@ -92,7 +149,6 @@ namespace TEN::Entities::Traps
 			int velocity = playerItem->Animation.Velocity.z;
 
 			playerItem->Pose.Position = Geometry::TranslatePoint(playerItem->Pose.Position, item.Pose.Orientation.y, (item.ItemFlags[0] > 0) ? (item.ItemFlags[0] + velocity) : (item.ItemFlags[0] - velocity));
-
 		}
 	}
 }
