60a89741c6087ceb3f00ac374a9a30b8e1ac02c7
Lwmte <3331699+Lwmte@users.noreply.github.com>
2024-02-18 12:47:03 +0100

    Fixed #1303

--- TombEngine/Game/collision/collide_item.cpp
+++ TombEngine/Game/collision/collide_item.cpp
@@ -1798,8 +1798,6 @@ void DoProjectileDynamics(short itemNumber, int x, int y, int z, int xv, int yv,
 
 void DoObjectCollision(ItemInfo* item, CollisionInfo* coll)
 {
-	const auto& player = GetLaraInfo(*item);
-
 	item->HitStatus = false;
 	coll->HitStatic = false;
 
