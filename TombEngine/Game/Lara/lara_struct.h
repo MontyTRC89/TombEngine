cf4116c8eb393139ecf470c862664823cafd8821
Sezz <sezzary@outlook.com>
2024-02-20 14:17:25 +1100

    Update lara_struct.h

--- TombEngine/Game/Lara/lara_struct.h
+++ TombEngine/Game/Lara/lara_struct.h
@@ -222,7 +222,7 @@ enum LaraState
 	LS_TREAD_WATER_VAULT_0_STEPS_TO_CROUCH = 196,
 	LS_TREAD_WATER_VAULT_1_STEP_UP_TO_CROUCH = 197,
 
-	LS_RUN_START_CANCEL = 206,
+	LS_RUN_FORWARD_START_CANCEL = 206,
 
 	NUM_LARA_STATES
 };
@@ -522,7 +522,7 @@ enum LaraAnim
 	LA_CRAWL_BACK_TO_IDLE_RIGHT = 277,								// Crawl back > crawl idle, right foot first
 	LA_REMOVE_PUZZLE = 278,											// Remove puzzle item > idle
 	LA_CRAWL_BACK_TO_IDLE_LEFT = 279,								// Crawl back > crawl idle, left foot first
-	LA_CRAWL_BACK_TO_IDLE_LEFT_END = 280,							// Unused.
+	LA_RUN_FORWARD_START_CANCEL = 280,								// Run forward start > idle
 	LA_CRAWL_TURN_LEFT_TO_IDLE_EARLY = 281,							// Crawl rotate left > crawl idle, early opportunity
 	LA_CRAWL_TURN_RIGHT_TO_IDLE_EARLY = 282,						// Crawl rotate right > crawl idle, early opportunity
 	LA_MONKEY_TURN_LEFT_TO_IDLE_EARLY = 283,						// Turn left on monkey swing > monkey swing idle, 1st opportunity
@@ -830,7 +830,6 @@ enum LaraAnim
 	NUM_LARA_ANIMS
 
 	// TRASHED ANIMS (reuse slots before going any higher and remove entries from this list when you do):
-	// 280,
 	// 368, 370,
 	// 442
 };
