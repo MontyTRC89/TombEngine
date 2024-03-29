bd4a735c84cba309a468e4fcd26975b7d595c7bc
Sezz <sezzary@outlook.com>
2024-03-29 17:55:49 +1100

    Update SPOTCAM struct

--- TombEngine/Game/spotcam.h
+++ TombEngine/Game/spotcam.h
@@ -10,19 +10,21 @@ constexpr auto MAX_SPOTCAMS = 256;
 constexpr auto SPOTCAM_CINEMATIC_BARS_HEIGHT = 1.0f / 16;
 constexpr auto SPOTCAM_CINEMATIC_BARS_SPEED = 1.0f / FPS;
 
+// TODO: Game loads spot cameras in chunks, therefore member ordering and size must remain the same.
 struct SPOTCAM
 {
 	Vector3i Position		= Vector3i::Zero;
 	Vector3i PositionTarget = Vector3i::Zero;
-
-	int roomNumber;
-	int sequence;
-	int camera;
+	
+	unsigned char sequence;
+	unsigned char camera;
 	short fov;
 	short roll;
-	int timer;
-	int speed;
-	int flags;
+	short timer;
+	short speed;
+	short flags;
+	short roomNumber;
+	short pad;
 };
 
 enum SPOTCAM_FLAGS
