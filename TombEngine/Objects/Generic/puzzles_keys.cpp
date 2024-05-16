a39f7ccd2c3e9a9f2ea4266a5dc9a8436e6b3298
MontyTRC89 <montyhammet@hotmail.it>
2024-05-16 12:01:33 +0200

    Disabled interpolation on PuzzleDone

--- TombEngine/Objects/Generic/puzzles_keys.cpp
+++ TombEngine/Objects/Generic/puzzles_keys.cpp
@@ -339,6 +339,7 @@ void PuzzleDone(ItemInfo* item, short itemNumber)
 		item->ObjectNumber += GAME_OBJECT_ID{ ID_PUZZLE_DONE1 - ID_PUZZLE_HOLE1 };
 		item->ItemFlags[5] = (int)ReusableReceptacleState::Done;
 		SetAnimation(item, 0);
+		item->DisableInterpolation = true;
 		item->ResetModelToDefault();	
 	}
 	else
@@ -349,6 +350,7 @@ void PuzzleDone(ItemInfo* item, short itemNumber)
 		item->Animation.ActiveState = GetAnimData(item).ActiveState;
 		item->Animation.TargetState = GetAnimData(item).ActiveState;
 		item->Animation.RequiredState = NO_VALUE;
+		item->DisableInterpolation = true;
 		item->ResetModelToDefault();
 
 		AddActiveItem(itemNumber);
