88408628d54a092eb9ac49ae4d9e6d8b329d96b1
Sezz <sezzary@outlook.com>
2024-07-07 02:54:55 +1000

    Organise

--- TombEngine/Math/Utils.cpp
+++ TombEngine/Math/Utils.cpp
@@ -5,6 +5,27 @@
 
 namespace TEN::Math
 {
+	float FloorToStep(float value, float step)
+	{
+		return (floor(value / step) * step);
+	}
+
+	float CeilToStep(float value, float step)
+	{
+		return (ceil(value / step) * step);
+	}
+
+	float RoundToStep(float value, float step)
+	{
+		return (round(value / step) * step);
+	}
+
+	float Remap(float value, float min0, float max0, float min1, float max1)
+	{
+		float alpha = (value - min0) / (max0 - min0);
+		return Lerp(min1, max1, alpha);
+	}
+
 	float Lerp(float value0, float value1, float alpha)
 	{
 		alpha = std::clamp(alpha, 0.0f, 1.0f);
@@ -86,26 +107,5 @@ namespace TEN::Math
 	{
 		auto result = Screen(Vector3(ambient), Vector3(tint));
 		return Vector4(result.x, result.y, result.z, ambient.w * tint.w);
-	}
-
-	float FloorToStep(float value, float step)
-	{
-		return (floor(value / step) * step);
-	}
-
-	float CeilToStep(float value, float step)
-	{
-		return (ceil(value / step) * step);
-	}
-
-	float RoundToStep(float value, float step)
-	{
-		return (round(value / step) * step);
-	}
-
-	float Remap(float value, float min0, float max0, float min1, float max1)
-	{
-		float alpha = (value - min0) / (max0 - min0);
-		return Lerp(min1, max1, alpha);
 	}
 }
