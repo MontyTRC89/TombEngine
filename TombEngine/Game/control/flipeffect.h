0c1c9d49a5dcd41661ddb636e318314390142c96
Sezz <sezzary@outlook.com>
2024-07-18 00:09:36 +1000

    Comment flipeffects to be converted to anim commands; use NO_VALUE

--- TombEngine/Game/control/flipeffect.h
+++ TombEngine/Game/control/flipeffect.h
@@ -1,6 +1,7 @@
 #pragma once
-#include "Game/items.h"
+
 #include "Game/control/control.h"
+#include "Game/items.h"
 
 #define EffectFunction void(ItemInfo* item)
 
@@ -10,35 +11,35 @@ extern int FlipEffect;
 
 extern std::function<EffectFunction> effect_routines[];
 
-void AddLeftFootprint(ItemInfo* item);
-void AddRightFootprint(ItemInfo* item);
+void AddLeftFootprint(ItemInfo* item); // TODO: To anim command.
+void AddRightFootprint(ItemInfo* item); // TODO: To anim command.
 void VoidEffect(ItemInfo* item);
 void FinishLevel(ItemInfo* item);
-void Turn180(ItemInfo* item);
+void Turn180(ItemInfo* item); // TODO: To anim command.
 void FloorShake(ItemInfo* item);
 void PlaySoundEffect(ItemInfo* item);
 void RubbleFX(ItemInfo* item);
 void PoseidonSFX(ItemInfo* item);
 void ActivateCamera(ItemInfo* item);
 void ActivateKey(ItemInfo* item);
-void SwapCrowbar(ItemInfo* item);
+void SwapCrowbar(ItemInfo* item); // TODO: To anim command.
 void ExplosionFX(ItemInfo* item);
 void LaraLocation(ItemInfo* item);
 void LaraLocationPad(ItemInfo* item);
 void KillActiveBaddys(ItemInfo* item);
-void LaraHandsFree(ItemInfo* item);
-void ShootRightGun(ItemInfo* item);
-void ShootLeftGun(ItemInfo* item);
+void LaraHandsFree(ItemInfo* item); // TODO: To anim command.
+void ShootRightGun(ItemInfo* item); // TODO: To anim command.
+void ShootLeftGun(ItemInfo* item); // TODO: To anim command.
 void SetFog(ItemInfo* item);
 void InvisibilityOn(ItemInfo* item);
 void InvisibilityOff(ItemInfo* item);
 void ResetHair(ItemInfo* item);
-void Pickup(ItemInfo* item);
-void Puzzle(ItemInfo* item);
-void DrawRightPistol(ItemInfo* item);
-void DrawLeftPistol(ItemInfo* item);
-void MeshSwapToPour(ItemInfo* item);
-void MeshSwapFromPour(ItemInfo* item); 
+void Pickup(ItemInfo* item); // TODO: To anim command.
+void Puzzle(ItemInfo* item); // TODO: To anim command.
+void DrawRightPistol(ItemInfo* item); // TODO: To anim command.
+void DrawLeftPistol(ItemInfo* item); // TODO: To anim command.
+void MeshSwapToPour(ItemInfo* item); // TODO: To anim command.
+void MeshSwapFromPour(ItemInfo* item); // TODO: To anim command.
 void FlashOrange(ItemInfo* item);
 void ClearSwarmEnemies(ItemInfo* item);
 
