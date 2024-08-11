04e81359bb64c15788e16a19728f96982e0b6a6b
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2024-08-11 15:02:48 +0200

    Update effects.h

--- TombEngine/Game/effects/effects.h
+++ TombEngine/Game/effects/effects.h
@@ -137,7 +137,7 @@ struct Particle
 	float size;
 	unsigned char friction;
 	unsigned char scalar;
-	unsigned char spriteIndex;
+	int spriteIndex;
 	signed char rotAdd;
 	signed char maxYvel;
 	bool on;
