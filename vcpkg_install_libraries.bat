e6dcb1ff2953c0ddc08e543adf68a28794886768
Sezz <sezzary@outlook.com>
2024-08-15 14:17:28 +1000

    Organise batch files

--- vcpkg_install_libraries.bat
+++ vcpkg_install_libraries.bat
@@ -3,8 +3,8 @@ title VCpkg Library Installer
 color 0
 
 echo "========================================"
-echo "   This batch installs VCpkg libraries  "
-echo "             for TombEngine.            "
+echo "        This batch will install         "
+echo "     VCpkg libraries for TombEngine.    "
 echo "========================================"
 echo.
 timeout /t 1 /nobreak > NUL
@@ -18,25 +18,25 @@ echo Installing DirectX Tool Kit.
 timeout /t 1 /nobreak > NUL
 vcpkg install directxtk[core,tools]:x86-windows
 
-echo Installing OIS.
-timeout /t 1 /nobreak > NUL
-vcpkg install ois[core]:x86-windows
-
 echo Installing FlatBuffers.
 timeout /t 1 /nobreak > NUL
 vcpkg install flatbuffers[core]:x86-windows
 
-echo Installing spdlog.
+echo Installing Lua.
 timeout /t 1 /nobreak > NUL
-vcpkg install spdlog[core]:x86-windows
+vcpkg install lua[core,cpp,tools]:x86-windows
+
+echo Installing OIS.
+timeout /t 1 /nobreak > NUL
+vcpkg install ois[core]:x86-windows
 
 echo Installing Sol2 (Lua module).
 timeout /t 1 /nobreak > NUL
 vcpkg install sol2[core]:x86-windows
 
-echo Installing Lua.
+echo Installing spdlog.
 timeout /t 1 /nobreak > NUL
-vcpkg install lua[core,cpp,tools]:x86-windows
+vcpkg install spdlog[core]:x86-windows
 
 echo Installing zlib.
 timeout /t 1 /nobreak > NUL
@@ -51,25 +51,25 @@ echo Installing DirectX Tool Kit.
 timeout /t 1 /nobreak > NUL
 vcpkg install directxtk[core,tools]:x64-windows
 
-echo Installing OIS.
-timeout /t 1 /nobreak > NUL
-vcpkg install ois[core]:x64-windows
-
 echo Installing FlatBuffers.
 timeout /t 1 /nobreak > NUL
 vcpkg install flatbuffers[core]:x64-windows
 
-echo Installing spdlog.
-timeout /t 1 /nobreak > NUL
-vcpkg install spdlog[core]:x64-windows
-
-echo Installing Sol2 (Lua Module).
-timeout /t 1 /nobreak > NUL
-vcpkg install sol2[core]:x64-windows
-
 echo Installing Lua.
 timeout /t 1 /nobreak > NUL
 vcpkg install lua[core,cpp,tools]:x64-windows
+
+echo Installing OIS.
+timeout /t 1 /nobreak > NUL
+vcpkg install ois[core]:x64-windows
+
+echo Installing Sol2 (Lua module).
+timeout /t 1 /nobreak > NUL
+vcpkg install sol2[core]:x64-windows
+
+echo Installing spdlog.
+timeout /t 1 /nobreak > NUL
+vcpkg install spdlog[core]:x64-windows
 
 echo Installing zlib.
 timeout /t 1 /nobreak > NUL
@@ -81,7 +81,7 @@ echo "========================================"
 timeout /t 1 /nobreak > NUL
 vcpkg upgrade --no-dry-run
 
-echo Installing integration with visual studio.
+echo Installing integration with Visual Studio.
 vcpkg integrate install
 
 echo Creating the game directory.
--- vcpkg_remove_libraries.bat
+++ vcpkg_remove_libraries.bat
@@ -1,81 +1,81 @@
 @echo off
-title VCpkg Library Uninnstaller
+title VCpkg Library Remover
 color 0
 
 echo "========================================"
-echo "   This batch removes vcpkg libraries   "
-echo "             for TombEngine.            "
+echo "        This batch will remove       "
+echo "     VCpkg libraries for TombEngine.    "
 echo "========================================"
 echo.
 timeout /t 1 /nobreak > NUL
 
 echo "========================================"
-echo "       Uninstalling x86 libraries.      "
+echo "         Removing x86 libraries.        "
 echo "========================================"
 timeout /t 1 /nobreak > NUL
 
-echo Uninstalling DirectX Tool Kit.
+echo Removing DirectX Tool Kit.
 timeout /t 1 /nobreak > NUL
 vcpkg remove directxtk[core,tools]:x86-windows
 
-echo Uninstalling OIS.
-timeout /t 1 /nobreak > NUL
-vcpkg remove ois[core]:x86-windows
-
-echo Uninstalling FlatBuffers.
+echo Removing FlatBuffers.
 timeout /t 1 /nobreak > NUL
 vcpkg remove flatbuffers[core]:x86-windows
 
-echo Uninstalling spdlog.
-timeout /t 1 /nobreak > NUL
-vcpkg remove spdlog[core]:x86-windows
-
-echo Uninstalling Sol2 (Lua module).
-timeout /t 1 /nobreak > NUL
-vcpkg remove sol2[core]:x86-windows
-
-echo Uninstalling Lua.
+echo Removing Lua.
 timeout /t 1 /nobreak > NUL
 vcpkg remove lua[core,cpp,tools]:x86-windows
 
-echo Uninstalling zlib.
+echo Removing OIS.
+timeout /t 1 /nobreak > NUL
+vcpkg remove ois[core]:x86-windows
+
+echo Removing Sol2 (Lua module).
+timeout /t 1 /nobreak > NUL
+vcpkg remove sol2[core]:x86-windows
+
+echo Removing spdlog.
+timeout /t 1 /nobreak > NUL
+vcpkg remove spdlog[core]:x86-windows
+
+echo Removing zlib.
 timeout /t 1 /nobreak > NUL
 vcpkg remove zlib:x86-windows
 
 echo "========================================"
-echo "       Uninstalling x64 libraries.      "
+echo "         Removing x64 libraries.        "
 echo "========================================"
 timeout /t 1 /nobreak > NUL
 
-echo Uninstalling DirectX Tool Kit.
+echo Removing DirectX Tool Kit.
 timeout /t 1 /nobreak > NUL
 vcpkg remove directxtk[core,tools]:x64-windows
 
-echo Uninstalling OIS.
-timeout /t 1 /nobreak > NUL
-vcpkg remove ois[core]:x64-windows
-
-echo Uninstalling FlatBuffers.
+echo Removing FlatBuffers.
 timeout /t 1 /nobreak > NUL
 vcpkg remove flatbuffers[core]:x64-windows
 
-echo Uninstalling spdlog.
-timeout /t 1 /nobreak > NUL
-vcpkg remove spdlog[core]:x64-windows
-
-echo Uninstalling Sol2 (Lua module).
-timeout /t 1 /nobreak > NUL
-vcpkg remove sol2[core]:x64-windows
-
-echo Uninstalling Lua.
+echo Removing Lua.
 timeout /t 1 /nobreak > NUL
 vcpkg remove lua[core,cpp,tools]:x64-windows
 
-echo Uninstalling zlib.
+echo Removing OIS.
+timeout /t 1 /nobreak > NUL
+vcpkg remove ois[core]:x64-windows
+
+echo Removing Sol2 (Lua module).
+timeout /t 1 /nobreak > NUL
+vcpkg remove sol2[core]:x64-windows
+
+echo Removing spdlog.
+timeout /t 1 /nobreak > NUL
+vcpkg remove spdlog[core]:x64-windows
+
+echo Removing zlib.
 timeout /t 1 /nobreak > NUL
 vcpkg remove zlib:x64-windows
 
-echo Removing integration with visual studio.
+echo Removing integration with Visual Studio.
 vcpkg integrate remove
 
 pause
