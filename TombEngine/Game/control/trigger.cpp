47088ba978f7edffa8bb67a3925ec4f489f0c01f
Lwmte <3331699+Lwmte@users.noreply.github.com>
2024-10-27 09:53:25 +0100

    Another attempt at fixing item status bug

--- TombEngine/Game/control/trigger.cpp
+++ TombEngine/Game/control/trigger.cpp
@@ -344,19 +344,13 @@ void Antitrigger(short const value, short const flags)
 	if (flags & ONESHOT)
 		item->Flags |= ATONESHOT;
 
-	if (item->Active)
-	{
-		RemoveActiveItem(value, false);
+	item->Status = ITEM_DEACTIVATED;
 
-		if (Objects[item->ObjectNumber].intelligent)
-		{
-			DisableEntityAI(value);
-			item->Status = ITEM_INVISIBLE;
-		}
-		else
-		{
-			item->Status = ITEM_DEACTIVATED;
-		}
+	if (item->Active && Objects[item->ObjectNumber].intelligent)
+	{
+		DisableEntityAI(value);
+		RemoveActiveItem(value, false);
+		item->Status = ITEM_INVISIBLE;
 	}
 }
 
