f31ed3158a4ddcb0b95593ea2835b83cb5e925cb
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2025-03-09 10:20:34 -0400

    Fix soundID

--- TombEngine/Objects/TR5/Trap/tr5_movinglaser.cpp
+++ TombEngine/Objects/TR5/Trap/tr5_movinglaser.cpp
@@ -94,7 +94,7 @@ namespace TEN::Entities::Traps
 
         if (item.ItemFlags[MovingLaserFlags::PauseCounter] == 0)
         {
-            SoundEffect(SFX_TR3_LASER_LOOP, &item.Pose, SoundEnvironment::Always);
+            SoundEffect(SFX_TR4_BINOCULARS_ZOOM, &item.Pose, SoundEnvironment::Always);
         }
 
         // Update room if necessary.
