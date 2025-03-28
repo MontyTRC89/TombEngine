9469a0e63a311da5b96918dda46c33d7c0e58e48
Nemoel-Tomo <tomo_669@hotmail.com>
2025-03-28 06:44:07 +0100

    Tomo - fireflies (#1597)
    * tests only - nothing to see
    
    * update
    
    * update
    
    * update
    
    * update corpse
    
    * update savegame, formatting
    
    * import develop
    
    * formatting
    
    * fix broken streamer vertex
    
    * update firefly streamer spawn
    
    * update effect
    
    * Add in objects for fireflies
    
    * fixed all reviews, formatting, changing on-off cycle
    
    * formatting
    
    * reduced the emitted light to two lights per cluster
    
    * adding antitrigger
    
    * formatting
    
    * fixed corpse
    
    * fixed a bug with corpse
    
    * fixed corpse fall bug
    
    * formatting
    
    * Small refactors
    
    * Fix merge
    
    ---------
    
    Co-authored-by: Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
    Co-authored-by: Lwmte <3331699+Lwmte@users.noreply.github.com>

--- TombEngine/Objects/TR5/tr5_objects.cpp
+++ TombEngine/Objects/TR5/tr5_objects.cpp
@@ -42,6 +42,7 @@
 #include "Objects/TR5/Emitter/tr5_spider_emitter.h"
 #include "Objects/TR5/Emitter/tr5_smoke_emitter.h"
 #include "Objects/TR5/Emitter/Waterfall.h"
+#include "Objects/Effects/Fireflies.h"
 
 // Objects
 #include "Objects/TR5/Light/tr5_light.h"
@@ -81,6 +82,7 @@ using namespace TEN::Effects::WaterfallEmitter;
 using namespace TEN::Entities::Creatures::TR5;
 using namespace TEN::Entities::Switches;
 using namespace TEN::Entities::Traps;
+using namespace TEN::Effects::Fireflies;
 
 static void StartEntity(ObjectInfo *obj)
 {
@@ -794,6 +796,14 @@ static void StartObject(ObjectInfo *obj)
 	{
 		obj->drawRoutine = nullptr;
 		obj->control = ControlEmberEmitter;
+	}
+
+	obj = &Objects[ID_FIREFLY_EMITTER];
+	if (obj->loaded)
+	{
+		obj->Initialize = InitializeFireflySwarm;
+		obj->control = ControlFireflySwarm;
+		obj->drawRoutine = NULL;
 	}
 
 	obj = &Objects[ID_GEN_SLOT1];
