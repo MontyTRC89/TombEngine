0ecb0844ff67a4ac35620110d6dd03cca9792440
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-04-13 16:31:54 +0200

    Use MAX_BONE_WEIGHTS in C++ code too

--- TombEngine/Renderer/Graphics/Vertices/Vertex.h
+++ TombEngine/Renderer/Graphics/Vertices/Vertex.h
@@ -12,12 +12,12 @@ namespace TEN::Renderer::Graphics::Vertices
 		Vector3 Tangent	 = Vector3::Zero;
 		Vector3 Binormal = Vector3::Zero;
 
-		unsigned int AnimationFrameOffset = 0;
-		Vector4		 Effects			  = Vector4::Zero;
-		unsigned int BoneIndex[4]		  = { 0, 0, 0, 0 };
-		float		 BoneWeight[4]		  = { 1, 0, 0, 0 };
-		unsigned int IndexInPoly		  = 0;
-		unsigned int OriginalIndex		  = 0;
-		unsigned int Hash				  = 0;
+		unsigned int AnimationFrameOffset			= 0;
+		Vector4		 Effects						= Vector4::Zero;
+		unsigned int BoneIndex[MAX_BONE_WEIGHTS]	= { 0, 0, 0, 0 };
+		float		 BoneWeight[MAX_BONE_WEIGHTS]	= { 1, 0, 0, 0 };
+		unsigned int IndexInPoly					= 0;
+		unsigned int OriginalIndex					= 0;
+		unsigned int Hash							= 0;
 	};
 }
